a3c1abaecae88fdc14089cf7874661d922ab991f
Yoel <yoelsh@post.tau.ac.il>
2018-03-09 23:44:27 +0200

    Start aspirelib

--- programs/aspire_viewstack.py
+++ programs/aspire_viewstack.py
@@ -1,47 +1,41 @@
 #!/opt/anaconda2/bin/python
 
-import sys
 import argparse
-import logging
 import matplotlib.pyplot as plt
 import mrcfile
+import aspirelib
 
-verbosity_parser = argparse.ArgumentParser(add_help=False)
-verbosity_parser.add_argument("--verbose", choices=["none", "info", "debug"],
-                    default="off", help="Logging level")
-verbosity_parser.add_argument("--logfile", help="log filename")
+logger = aspirelib.default_logger
 
-logger=logging.getLogger()
 
 def viewstack(mrcname, numslices=16, startslice=1, nrows=4, ncols=4):
-
     logger.debug("Input parameters:stack=%s, numslices=%d, startslice=%d,"
-                  "(nrows,ncols)=(%d,%d)",
-                  mrcname, numslices, startslice, nrows, ncols)
+                 "(nrows,ncols)=(%d,%d)",
+                 mrcname, numslices, startslice, nrows, ncols)
 
-    if numslices > nrows*ncols:
-            raise ValueError("nrows*ncols should be at least numslices"
-                             " to accomodate all images.")
-    stack = mrcfile.mmap(mrcname,mode="r")
-    logger.debug("Shape of loaded map: "\
-          + str((stack.header.nx, stack.header.ny, stack.header.nz)))
+    if numslices > nrows * ncols:
+        raise ValueError("nrows*ncols should be at least numslices"
+                         " to accomodate all images.")
+    stack = mrcfile.mmap(mrcname, mode="r")
+    logger.debug("Shape of loaded map: "
+                 + str((stack.header.nx, stack.header.ny, stack.header.nz)))
 
     plt.figure(figsize=(4, 4))
 
     j = 0
-    dx = 0.95/ncols
-    dy = 0.95/nrows
+    dx = 0.95 / ncols
+    dy = 0.95 / nrows
     for col in range(ncols):
         for row in range(nrows):
             if j < numslices:
-                x0 = col*1.0/ncols
-                y0 = 1-row*1.0/nrows-dy
+                x0 = col * 1.0 / ncols
+                y0 = 1 - row * 1.0 / nrows - dy
                 # Flip y direction to fill image grid from top
                 ax = plt.axes([x0, y0, dx, dy])
-                plt.imshow(stack.data[j,...])
+                plt.imshow(stack.data[j, ...])
                 plt.gray()
                 plt.axis("off")
-                ax.text(0.1, 0.9, str(j+startslice), fontsize=10,
+                ax.text(0.1, 0.9, str(j + startslice), fontsize=10,
                         horizontalalignment='center',
                         verticalalignment='center',
                         transform=ax.transAxes, color='red',
@@ -56,7 +50,7 @@ if __name__ == "__main__":
         description="Display images from an MRCS stack.",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
         epilog="Example: {} stack.mrcs".format(__file__),
-        parents=[verbosity_parser])
+        parents=[aspirelib.verbosity_parser])
 
     parser.add_argument("stack", help="Filename of MRCS stack")
     parser.add_argument("--numslices", type=int,
@@ -73,24 +67,7 @@ if __name__ == "__main__":
                         default=4)
 
     args = parser.parse_args()
-
-    FORMAT = "[%(asctime)-15s][%(name)s:%(lineno)03d][%(levelname)-5s]%(message)s"
-    DATEFMT = "%y-%m-%d %H:%M:%S"
-
-    if args.verbose=="none":
-        logger.propagte=False
-    elif args.verbose=="info":
-        logger.setLevel(logging.INFO)
-    elif args.verbose=="debug":
-        logger.setLevel(logging.DEBUG)
-
-    if args.logfile is not None:
-        lh = logging.FileHandler(args.logfile)
-    else:
-        lh = logging.StreamHandler(sys.stdout)
-
-    lh.setFormatter(logging.Formatter(FORMAT,DATEFMT))
-    logger.addHandler(lh)
+    aspirelib.configure_logger(logger,args)
 
     viewstack(args.stack, args.numslices, args.startslice,
               args.nrows, args.ncols)
