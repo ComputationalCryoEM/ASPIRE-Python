861484938ff5d0e37e9f73820dc0d0721152aff6
rbrook <cs123k@gmail.com>
2018-07-18 11:11:45 +0200

    adjusted docstring; removed commented out code

--- preprocessor/cryo_downsample.py
+++ preprocessor/cryo_downsample.py
@@ -13,24 +13,20 @@ from helpers.helpers import f_flatten, TupleCompare
 def cryo_downsample(img, side, compute_fx=False, stack=False, mask=None):
     """ Use Fourier methods to change the sample interval and/or aspect ratio
         of any dimensions of the input image 'img'. If the optional argument
-        stack is set to True, then the last dimension of 'img' is interpreted as the index of each
-        image in the stack. The size argument szout is either a scalar or a
-        vector of the dimension of the output images.  Let the size of a stack
-        of 2D images 'img' be n1 x n1 x ni.  The size of the output (szout=n or
-        szout=[n n]) will be n x n x ni. The size argument szout can be chosen
-        to change the aspect ratio of the output; however the routine will not
-        allow one dimension to be scaled down and another scaled up.
+        stack is set to True, then the *first* dimension of 'img' is interpreted as the index of each
+        image in the stack. The size argument side is an integer, the size of the
+        output images.  Let the size of a stack
+        of 2D images 'img' be n1 x n1 x k.  The size of the output will be side x side x k.
 
         If the optional mask argument is given, this is used as the
-        zero-centered Fourier mask for the re-sampling.  The size of mask should
+        zero-centered Fourier mask for the re-sampling. The size of mask should
         be the same as the output image size. For example for downsampling an
         n0 x n0 image with a 0.9 x nyquist filter, do the following:
             msk = fuzzymask(n,2,.45*n,.05*n)
             out = cryo_downsample(img, n, 0, msk)
-            The size of the mask must be the size of out. The optional fx output
+            The size of the mask must be the size of output. The optional fx output
             argument is the padded or cropped, masked, FT of in, with zero
             frequency at the origin.
-
     """
 
     try:
@@ -65,16 +61,6 @@ def cryo_downsample(img, side, compute_fx=False, stack=False, mask=None):
     if TupleCompare.eq(szout, szin):  # no change in shape
         if not compute_fx:
             return img
-
-    # todo should we remove this? On MATLAB this is mandatory for scaling up/down. Here it seems to not be needed.
-    # if TupleCompare.lt(szout, szin, eq=True):
-    #     down = True  # scale down
-    #
-    # elif TupleCompare.gt(szout, szin, eq=True):
-    #     down = False  # scale up
-    #
-    # else:  # make sure we don't scale down and up at the same time
-    #     raise DimensionsIncompatible("Can't scale up and down at the same time!")
 
     # adjust mask to be the size of desired output
     mask = cryo_crop(mask, side) if mask else 1
