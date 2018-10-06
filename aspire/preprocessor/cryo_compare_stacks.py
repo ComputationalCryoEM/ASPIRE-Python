d31b5ba555d2dff08b2ead8b540b1cd497cc129b
rbrook <cs123k@gmail.com>
2018-10-06 12:40:45 +0200

    utrning cryo_compare_stacks into a utility of aspire.py
    Former-commit-id: f0439cae12b803d66bf552be1adb218077adcb4a

--- aspire/preprocessor/cryo_compare_stacks.py
+++ aspire/preprocessor/cryo_compare_stacks.py
@@ -1,6 +1,3 @@
-# converted from MATLAB func "cryo_compare_stacks.m"
-
-import argparse
 import logging
 import mrcfile
 import numpy as np
@@ -35,18 +32,18 @@ def cryo_compare_mrc_files(mrcname1, mrcname2, verbose=0, max_err=None):
         except (TypeError, ValueError):
             raise Exception("max_err must be either a float or an integer!")
 
-    mrc1 = mrcfile.open(mrcname1)
-    mrc2 = mrcfile.open(mrcname2)
+    stack1 = mrcfile.open(mrcname1)
+    stack2 = mrcfile.open(mrcname2)
 
     # check the dimensions of the stack are compatible
     dimension_map = {0: 'x', 1: 'y', 2: 'z'}
     for dimension in range(3):
-        if mrc1.data.shape[dimension] != mrc2.data.shape[dimension]:
+        if stack1.data.shape[dimension] != stack2.data.shape[dimension]:
             raise Exception(f'{dimension_map[dimension]} dimension in both stacks is not '
-                            f'compatible: {mrcname1} has {mrc1.data.shape[dimension]} pixels, '
-                            f'{mrcname2} has {mrc2.data.shape[dimension]} pixels')
+                            f'compatible: {mrcname1} has {stack1.data.shape[dimension]} pixels, '
+                            f'{mrcname2} has {stack2.data.shape[dimension]} pixels')
 
-    num_of_images = mrc1.data.shape[2]
+    num_of_images = stack1.data.shape[2]
     if num_of_images == 0:
         logger.warning('stacks are empty!')
 
@@ -58,7 +55,7 @@ def cryo_compare_mrc_files(mrcname1, mrcname2, verbose=0, max_err=None):
     accumulated_err = 0
     for i in range(num_of_images):
 
-        err = np.linalg.norm(mrc1.data[i] - mrc2.data[i])
+        err = np.linalg.norm(stack1.data[i] - stack2.data[i])
         accumulated_err += err
         relative_err = accumulated_err / (i+1)
 
@@ -72,7 +69,7 @@ def cryo_compare_mrc_files(mrcname1, mrcname2, verbose=0, max_err=None):
         elif verbose == 1:
             pb.print_progress_bar((i + 1) / num_of_images * 100)
 
-        elif verbose == 2 and (i+1) % 10 == 0:
+        elif verbose == 2 and (i+1) % 100 == 0:
             logger.info(f'Finished comparing {i+1}/{num_of_images} projections. '
                         f'Relative error so far: {relative_err}')
 
@@ -85,26 +82,3 @@ def cryo_compare_mrc_files(mrcname1, mrcname2, verbose=0, max_err=None):
                     f'Relative error: {relative_err}')
 
     return relative_err
-
-
-if __name__ == '__main__':
-
-    parser = argparse.ArgumentParser(description='Compare relative error between 2 mrcfile stacks',
-                                     formatter_class=argparse.RawTextHelpFormatter)
-
-    parser.add_argument("mrcfile1", help="first mrc file to compare")
-    parser.add_argument("mrcfile2", help="second mrc file to compare")
-    parser.add_argument("-v", "--verbose", type=int, help="increase output verbosity.\n"
-                                                          "0: silent\n"
-                                                          "1: show progress-bar\n"
-                                                          "2: print relative err every 1k images\n"
-                                                          "3: print relative err for each image")
-
-    parser.add_argument("--max-err", type=float, help="raise an error if relative error is "
-                                                      "bigger than max-err")
-    args = parser.parse_args()
-
-    err = cryo_compare_mrc_files(args.mrcfile1, args.mrcfile2, verbose=args.verbose,
-                                 max_err=args.max_err)
-
-    logger.info(f'relative err: {err}')
