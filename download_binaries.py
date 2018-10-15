ad0d45721297d89d57ab25afb92c895f40b51651
rbrook <cs123k@gmail.com>
2018-10-15 10:41:37 +0200

    change target names for Makefile; added default for 'make' command; added colors for prints when downloading binary files

--- download_binaries.py
+++ download_binaries.py
@@ -14,8 +14,12 @@ binary_files = [
 
 dest_folder = 'binaries'
 
+# output colors
+YELLOW = '\033[1;33m'
+NC = '\033[0m'
+
 if os.path.exists(dest_folder):
-    choice = input("binaries folder exist, override existing files [N/y]?")
+    choice = input("'binaries' folder exist, override existing files [N/y]?")
     if choice.lower() != 'y':
         sys.exit()
 
@@ -24,13 +28,13 @@ else:
 
 for bf in binary_files:
     full_url = base_url + bf
-    print(f'downloading resource from: {full_url}')
+    print(f'downloading resource: {full_url}')
     res = requests.get(full_url)
     assert res.status_code == 200, f"Failed to download resource {bf}"
 
     with open(dest_folder + bf, 'wb') as fh:
         fh.write(res.content)
 
-    print(f"saved {os.path.basename(bf)} to binaries.")
+    print(f"{YELLOW}{os.path.basename(bf)}{NC} is saved to 'binaries'.")
 
 print('done.')
