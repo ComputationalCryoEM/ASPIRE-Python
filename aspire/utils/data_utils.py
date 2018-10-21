a7f2d8454754f6b62d012eff181ca30fd9854c3d
rbrook <cs123k@gmail.com>
2018-10-21 11:36:42 +0200

    added 2 util funcs to handle conversion from MATLAB to Numpy indexing; added '.mat' to gitignore

--- aspire/utils/data_utils.py
+++ aspire/utils/data_utils.py
@@ -1,5 +1,8 @@
-from scipy.io import loadmat
 import os
+import mrcfile
+import numpy as np
+
+from scipy.io import loadmat
 
 
 def mat_to_npy(file_name, dir_path=None):
@@ -13,3 +16,22 @@ def mat_to_npy(file_name, dir_path=None):
 def mat_to_npy_vec(file_name, dir_path=None):
     a = mat_to_npy(file_name, dir_path)
     return a.reshape(a.shape[0] * a.shape[1])
+
+
+def read_mrc_like_matlab(mrc_file):
+
+    mrc_stack = mrcfile.open(mrc_file).data
+
+    if not np.isfortran(mrc_stack):
+        mrc_stack = mrc_stack.swapaxes(0, 2)
+
+    return mrc_stack
+
+
+def write_mrc_like_matlab(mrc_filename, stack):
+
+    if not np.isfortran(stack):
+        stack = stack.swapaxes(0, 2)
+
+    with mrcfile.new(mrc_filename) as fh:
+        fh.set_data(stack)
