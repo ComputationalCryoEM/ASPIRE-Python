0266cdbde5b34f16d973e35b48d482768608bb44
rbrook <cs123k@gmail.com>
2018-11-06 21:06:14 +0100

    change error to warning

--- aspire/utils/array_utils.py
+++ aspire/utils/array_utils.py
@@ -9,6 +9,8 @@ import pyfftw
 from numpy.fft import fftshift, ifftshift
 from pyfftw.interfaces.numpy_fft import fft2, ifft2
 
+from aspire.common.logger import logger
+
 
 def crop(images, out_size, is_stack, fillval=0.0):
     if is_stack:
@@ -309,14 +311,14 @@ def cryo_epsds(imstack, samples_idx, max_d, verbose=None):
             if d <= max_d ** 2:
                 idx, _ = bsearch(dsquare, d*(1-1e-13), d*(1+1e-13))
                 if idx is None:
-                    raise Warning('something went wrong in bsearch')
+                    logger.warning('something went wrong in bsearch')
                 r2[i+p-1, j+p-1] = r[idx-1]
 
     w = gwindow(p, max_d)
     p2 = cfft2(r2 * w)
     err = np.linalg.norm(p2.imag) / np.linalg.norm(p2)
     if err > 1e-12:
-        raise Warning('Large imaginary components in P2 = {}'.format(err))
+        logger.warning('Large imaginary components in P2 = {}'.format(err))
 
     p2 = p2.real
 
@@ -332,7 +334,7 @@ def cryo_epsds(imstack, samples_idx, max_d, verbose=None):
         max_neg_err = np.max(np.abs(p2[neg_idx]))
         if max_neg_err > 1e-2:
             neg_norm = np.linalg.norm(p2[neg_idx])
-            raise Warning('Power specrtum P2 has negative values with energy {}'.format(neg_norm))
+            logger.warning('Power specrtum P2 has negative values with energy {}'.format(neg_norm))
         p2[neg_idx] = 0
     return p2, r, r2, x
 
@@ -362,7 +364,7 @@ def cryo_epsdr(vol, samples_idx, max_d, verbose):
             if d <= max_d ** 2:
                 idx, _ = bsearch(dsquare, d - 1e-13, d + 1e-13)
                 if idx is None:
-                    raise Warning('something went wrong in bsearch')
+                    logger.warning('something went wrong in bsearch')
                 dist_map[i, j] = idx
 
     dist_map = dist_map.astype('int') - 1
