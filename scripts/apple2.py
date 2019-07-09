f1c187318cfab3924a11b9cf01d86b170bff01a8
Vineet Bansal <vineetbansal@gmail.com>
2019-07-09 12:45:03 -0400

    Added 'binaries' folder. If any binary files are needed they are better included in the source. Removed 'download_binaries' script.

--- scripts/apple2.py
+++
@@ -1,5 +0,0 @@
-from aspyre.apple.apple import Apple
-
-if __name__ == '__main__':
-    apple = Apple(output_dir='apple_out', create_jpg=True)
-    apple.process_folder('../tests/saved_test_data/mrc_files')
