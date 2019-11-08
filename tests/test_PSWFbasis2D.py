2002953ae8458f4a244462c8ea2c43a481d6357c
Junchao Xia <junchaoxia@hotmail.com>
2019-11-08 11:56:17 -0500

    reduce the size of unit tests for PSWF class

--- tests/test_PSWFbasis2D.py
+++ tests/test_PSWFbasis2D.py
@@ -1,30 +1,32 @@
 import numpy as np
 from unittest import TestCase
+import os.path
 
 from aspire.basis.pswf_2d import PSWFBasis2D
 
-import os.path
+
 DATA_DIR = os.path.join(os.path.dirname(__file__), 'saved_test_data')
+
 
 class PSWFBasis2DTestCase(TestCase):
     def setUp(self):
-        self.basis = PSWFBasis2D((129, 129), 1.0, 1.0)
+        self.basis = PSWFBasis2D((8, 8), 1.0, 1.0)
 
     def tearDown(self):
         pass
 
     def testPSWFBasis2DEvaluate_t(self):
-        images = np.load(os.path.join(DATA_DIR, 'example_data_np_array.npy'))
+        images = np.load(os.path.join(DATA_DIR, 'ffbbasis2d_xcoeff_in_8_8.npy'))
         result = self.basis.evaluate_t(images)
-        coeffs = np.load(os.path.join(DATA_DIR, 'example_data_pswf2d_coeffs64.npy'))
+        coeffs = np.load(os.path.join(DATA_DIR, 'pswf2d_vcoeffs_out_8_8.npy'))
         # make sure both real and imaginary parts are consistent.
         self.assertTrue(np.allclose(np.real(result), np.real(coeffs)) and
                         np.allclose(np.imag(result)*1j, np.imag(coeffs)*1j))
 
     def testPSWFBasis2DEvaluate(self):
-        coeffs = np.load(os.path.join(DATA_DIR, 'example_data_pswf2d_coeffs64.npy'))
+        coeffs = np.load(os.path.join(DATA_DIR, 'pswf2d_vcoeffs_out_8_8.npy'))
         result = self.basis.evaluate(coeffs)
-        images = np.load(os.path.join(DATA_DIR, 'example_data_pswf2d_reconsImgs64.npy'))
+        images = np.load(os.path.join(DATA_DIR, 'pswf2d_xcoeff_out_8_8.npy'))
         self.assertTrue(np.allclose(result, images))
 
 
