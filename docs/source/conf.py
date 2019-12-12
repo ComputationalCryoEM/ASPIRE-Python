b9e6b898b5f96656a8b64c0a3b3347ece9068671
Vineet Bansal <vineetbansal@gmail.com>
2019-12-12 13:47:31 -0500

    Added a _static folder to investigate issues with RTD theme files.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -97,6 +97,7 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_logo = '_static/logo.jpg'
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
