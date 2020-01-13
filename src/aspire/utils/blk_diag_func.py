bd80fd8858098053c7689ca12622538fafae2b9e
Junchao Xia <junchaoxia@hotmail.com>
2020-01-13 17:22:46 -0500

    Add docstring and unit tests for operation functions of block diagonal matrix

--- src/aspire/utils/blk_diag_func.py
+++ src/aspire/utils/blk_diag_func.py
@@ -1,15 +1,29 @@
+"""
+Define operation functions of block diagonal matrix needed by Cov2D analysis.
+"""
 import numpy as np
 from numpy.linalg import norm
-
 from numpy.linalg import solve
 from scipy.linalg import block_diag
 from scipy.special import jv
 
 from aspire.utils.cell import Cell2D
+from aspire.basis.ffb_2d import FFBBasis2D
 from aspire.basis.basis_utils import lgwt
 
 
 def blk_diag_zeros(blk_partition, dtype=None):
+    """
+    Build a block diagonal zero matrix
+
+    :param blk_partition: The matrix block partition of `blk_diag` in the form of a
+        K-element list storing all shapes of K diagonal matrix blocks,
+        where `blk_partition[i]` corresponds to the shape (number of rows and columns)
+        of the `i` diagonal matrix block.
+    :param dtype: The data type to set precision of diagonal matrix block.
+    :return: A block diagonal matrix consisting of `K` zero blocks.
+    """
+
     if dtype is None:
         dtype = 'double'
     blk_diag = []
@@ -19,6 +33,16 @@ def blk_diag_zeros(blk_partition, dtype=None):
 
 
 def blk_diag_eye(blk_partition, dtype=None):
+    """
+    Build a block diagonal eye matrix
+
+    :param blk_partition: The matrix block partition of `blk_diag` in the form of a
+        K-element list storing all shapes of K diagonal matrix blocks,
+        where `blk_partition[i]` corresponds to the shape (number of rows and columns)
+        of the `i` diagonal matrix block.
+    :param dtype: The data type to set the pricision of diagonal matrix block.
+    :return: A block diagonal matrix consisting of `K` eye (identity) blocks.
+    """
     if dtype is None:
         dtype = 'double'
     blk_diag = []
@@ -31,6 +55,16 @@ def blk_diag_eye(blk_partition, dtype=None):
 
 
 def blk_diag_partition(blk_diag):
+    """
+    Create a partition of block diagonal matrix
+
+    :param blk_diag: A block diagonal matrix in the form of a list. Each
+        element corresponds to a diagonal block.
+    :return: The matrix block partition of `blk_diag` in the form of a
+        K-element list storing all shapes of K diagonal matrix blocks,
+        where `blk_partition[i]` corresponds to the shape (number of rows and columns)
+        of the `i` diagonal matrix block.
+    """
     blk_partition = []
     for mat in blk_diag:
         blk_partition.append(np.shape(mat))
@@ -38,6 +72,14 @@ def blk_diag_partition(blk_diag):
 
 
 def blk_diag_add(blk_diag_a, blk_diag_b):
+    """
+    Define the element addition of block diagonal matrix
+
+    :param blk_diag_a: The block diagonal A matrix
+    :param blk_diag_b: The block diagonal B matrix
+    :return: A block diagonal matrix with elements equal to corresponding
+        sums from A and B elements.
+    """
     if len(blk_diag_a) != len(blk_diag_b):
         raise RuntimeError('Dimensions of two block diagonal matrices are not equal!')
     elif np.allclose([np.size(mat_a) for mat_a in blk_diag_a], [np.size(mat_b) for mat_b in blk_diag_b]):
@@ -51,12 +93,20 @@ def blk_diag_add(blk_diag_a, blk_diag_b):
 
 
 def blk_diag_minus(blk_diag_a, blk_diag_b):
+    """
+    Define the element subtraction of block diagonal matrix
+
+    :param blk_diag_a: The block diagonal A matrix
+    :param blk_diag_b: The block diagonal B matrix
+    :return: A block diagonal matrix with elements equal to corresponding
+        subtraction of A from B elements.
+    """
     if len(blk_diag_a) != len(blk_diag_b):
         raise RuntimeError('Dimensions of two block diagonal matrices are not equal!')
     elif np.allclose([np.size(mat_a) for mat_a in blk_diag_a], [np.size(mat_b) for mat_b in blk_diag_b]):
         blk_diag_c = []
         for i in range(0, len(blk_diag_a)):
-            mat_c = blk_diag_c[i] - blk_diag_c[i]
+            mat_c = blk_diag_a[i] - blk_diag_b[i]
             blk_diag_c.append(mat_c)
     else:
         raise RuntimeError('Elements of two block diagonal matrices have not the same size!')
@@ -64,6 +114,13 @@ def blk_diag_minus(blk_diag_a, blk_diag_b):
 
 
 def blk_diag_apply(blk_diag, x):
+    """
+    Define the apply option of a block diagonal matrix with a matrix of coefficient vectors
+
+    :param blk_diag: The block diagonal matrix
+    :param x: The coefficient matrix with each column is a coefficient vector
+    :return: A matrix with new coefficient vectors
+    """
     cols = np.array([np.size(mat, 1) for mat in blk_diag])
 
     if np.sum(cols) != np.size(x, 0):
@@ -80,6 +137,14 @@ def blk_diag_apply(blk_diag, x):
 
 
 def blk_diag_mult(blk_diag_a, blk_diag_b):
+    """
+    Define the matrix multiplication of two block diagonal matrices
+
+    :param blk_diag_a: The block diagonal A matrix
+    :param blk_diag_b: The block diagonal B matrix
+    :return: A block diagonal matrix with elements resulted from
+        the matrix multiplication of A from B block diagonal matrices.
+    """
 
     if blk_diag_isnumeric(blk_diag_a):
         blk_diag_c = blk_diag_mult(blk_diag_b, blk_diag_a)
@@ -105,6 +170,12 @@ def blk_diag_mult(blk_diag_a, blk_diag_b):
 
 
 def blk_diag_norm(blk_diag):
+    """
+    Compute the 2-norm of a block diagonal matrix
+
+    :param blk_diag: The block diagonal matrix
+    :return: The 2-norm of the block diagonal matrix
+    """
     maxvalues = []
     for i in range(0, len(blk_diag)):
         maxvalues.append(norm(blk_diag[i],2))
@@ -113,6 +184,15 @@ def blk_diag_norm(blk_diag):
 
 
 def blk_diag_solve(blk_diag, y):
+    """
+    Solve a linear system involving a block diagonal matrix
+
+    :param blk_diag: The block diagonal matrix
+    :param y: The right-hand side in the linear system.  May be a matrix
+        consisting of  coefficient vectors, in which case each column is
+        solved for separately.
+    :return: The result of solving the linear system formed by the matrix.
+    """
     rows = np.array([np.size(mat_a, 0) for mat_a in blk_diag])
     if sum(rows) != np.size(y, 0):
         raise RuntimeError('Sizes of matrix `blk_diag` and `y` are not compatible.')
@@ -127,11 +207,30 @@ def blk_diag_solve(blk_diag, y):
 
 
 def blk_diag_to_mat(blk_diag):
+    """
+    Convert list representation of block diagonal matrix into full matrix
+
+    :param blk_diag: The block diagonal matrix
+
+    :return: The block diagonal matrix including the zero elements of
+        non-diagonal blocks.
+    """
     mat = block_diag(blk_diag)
     return mat
 
 
 def mat_to_blk_diag(mat, blk_partition):
+    """
+    Convert full block diagonal matrix into list representation
+    :param mat; The full block diagonal matrix including the zero elements of
+        non-diagonal blocks.
+    :param blk_partition: The matrix block partition of `blk_diag` in the form of a
+        K-element list storing all shapes of K diagonal matrix blocks,
+        where `blk_partition[i]` corresponds to the shape (number of rows and columns)
+        of the `i` diagonal matrix block.
+
+    :return: The block diagonal matrix in the list representation of diagonal blocks
+    """
     rows = blk_partition[:, 0]
     cols = blk_partition[:, 1]
     cellarray = Cell2D(rows, cols, dtype=mat.dtype)
@@ -140,6 +239,13 @@ def mat_to_blk_diag(mat, blk_partition):
 
 
 def blk_diag_transpose(blk_diag):
+    """
+    Get the transpose matrix of a block diagonal matrix
+
+    :param blk_diag: The block diagonal matrix
+    :return: The corresponding transpose form of a block diagonal matrix
+    """
+
     blk_diag_t = []
     for i in range(0, len(blk_diag)):
         blk_diag_t.append(blk_diag[i].T)
@@ -147,15 +253,27 @@ def blk_diag_transpose(blk_diag):
 
 
 def blk_diag_compatible(blk_diag_a, blk_diag_b):
+    """
+    Check whether two block diagonal matrices are compatible or not
+
+    :param blk_diag_a: The block diagonal A matrix
+    :param blk_diag_b: The block diagonal B matrix
+    :return: `True` value if all of shapes in two list representations of
+        block diagonal matrices are the same, otherwise return `False`.
+    """
     if len(blk_diag_a) != len(blk_diag_b):
         return False
-    elif np.allclose([np.size(mat_a, 1) for mat_a in blk_diag_a], [np.size(mat_b, 0) for mat_b in blk_diag_b]):
+    elif np.allclose([np.size(mat_a, 1) for mat_a in blk_diag_a],
+                     [np.size(mat_b, 0) for mat_b in blk_diag_b]):
         return True
     else:
         return False
 
 
 def blk_diag_isnumeric(x):
+    """
+    Check a block diag matrix is numeric or not
+    """
     try:
         return 0 == x*0
     except:
@@ -163,6 +281,16 @@ def blk_diag_isnumeric(x):
 
 
 def radial_filter2fb_mat(h_fun, fbasis):
+    """
+    Convert a function in k space into a basis representation
+
+    :param h_fun: The function form in k space
+    :param fbasis: The basis object for expanding
+    :return: a matrix representation using the `fbasis` expansion
+    """
+    if not isinstance(fbasis, FFBBasis2D):
+            raise NotImplementedError('Currently only fast FB method is supported')
+
     n_r = fbasis.sz[0]
     k_vals, wts = lgwt(n_r, 0, 0.5)
     h_vals = h_fun(k_vals*2*np.pi)
