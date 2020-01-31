154dd2f34fd8ae7961d500f514f33eb8c14b0f72
Junchao Xia <junchaoxia@hotmail.com>
2020-01-31 08:55:26 -0500

    Improve consistency of comments in Cov3D example

--- examples/cov3d_simulation.py
+++ examples/cov3d_simulation.py
@@ -40,11 +40,12 @@ noise_estimator = WhiteNoiseEstimator(sim, batchSize=500)
 noise_variance = noise_estimator.estimate()
 logger.info(f'Noise Variance = {noise_variance}')
 
-"""
-Estimate the mean. This uses conjugate gradient on the normal equations for the least-squares estimator of the mean
-volume. The mean volume is represented internally using the basis object, but the output is in the form of an
-L-by-L-by-L array.
-"""
+
+# Estimate the mean. This uses conjugate gradient on the normal equations for
+# the least-squares estimator of the mean volume. The mean volume is represented internally
+# using the basis object, but the output is in the form of an
+# L-by-L-by-L array.
+
 mean_estimator = MeanEstimator(sim, basis)
 mean_est = mean_estimator.estimate()
 
@@ -52,72 +53,66 @@ mean_est = mean_estimator.estimate()
 covar_estimator = CovarianceEstimator(sim, basis, mean_kernel=mean_estimator.kernel)
 covar_est = covar_estimator.estimate(mean_est, noise_variance)
 
-"""
-Extract the top eigenvectors and eigenvalues of the covariance estimate.
-Since we know the population covariance is low-rank, we are only interested
-in the top eigenvectors.
-"""
+
+# Extract the top eigenvectors and eigenvalues of the covariance estimate.
+# Since we know the population covariance is low-rank, we are only interested
+# in the top eigenvectors.
+
 eigs_est, lambdas_est = eigs(covar_est, num_eigs)
 
-"""
-Truncate the eigendecomposition. Since we know the true rank of the
-covariance matrix, we enforce it here.
-"""
+
+# Truncate the eigendecomposition. Since we know the true rank of the
+# covariance matrix, we enforce it here.
+
 eigs_est_trunc = eigs_est[:, :, :, :num_vols-1]
 lambdas_est_trunc = lambdas_est[:num_vols-1, :num_vols-1]
 
-# Estimate the coordinates in the eigenbasis. Given the images, we find the coordinates in the basis that
-# minimize the mean squared error, given the (estimated) covariances of the volumes and the noise process.
+# Estimate the coordinates in the eigenbasis. Given the images, we find the
+# coordinates in the basis that minimize the mean squared error, given the
+# (estimated) covariances of the volumes and the noise process.
 coords_est = src_wiener_coords(sim, mean_est, eigs_est_trunc, lambdas_est_trunc, noise_variance)
 
-# Cluster the coordinates using k-means. Again, we know how many volumes we expect, so we can use this parameter
-# here. Typically, one would take the number of clusters to be one plus the number of eigenvectors extracted.
+# Cluster the coordinates using k-means. Again, we know how many volumes
+# we expect, so we can use this parameter here. Typically, one would take
+# the number of clusters to be one plus the number of eigenvectors extracted.
 
 # Since kmeans2 relies on randomness for initialization, important to push random seed to context manager here.
 with Random(0):
     centers, vol_idx = kmeans2(coords_est.T, num_vols)
     centers = centers.squeeze()
 
-"""
--------------------
-EVALUATION
--------------------
-"""
 
-"""
-Evaluate performance of mean estimation.
-"""
+# Evaluate performance of mean estimation.
+
 mean_perf = sim.eval_mean(mean_est)
 
-"""
-Evaluate performance of covariance estimation. We also evaluate the truncated
-eigendecomposition. This is expected to be a closer approximation since it
-imposes an additional low-rank condition on the estimate.
-"""
+
+# Evaluate performance of covariance estimation. We also evaluate the truncated
+# eigendecomposition. This is expected to be a closer approximation since it
+# imposes an additional low-rank condition on the estimate.
+
 covar_perf = sim.eval_covar(covar_est)
 eigs_perf = sim.eval_eigs(eigs_est_trunc, lambdas_est_trunc)
 
-"""
-Evaluate clustering performance.
-"""
+# Evaluate clustering performance.
+
 clustering_accuracy = sim.eval_clustering(vol_idx)
 
-"""
-Assign the cluster centroids to the different images. Since we expect a discrete distribution of volumes
-(and therefore of coordinates), we assign the centroid coordinate to each image that belongs to that cluster.
-Evaluate the coordinates estimated
-"""
+# Assign the cluster centroids to the different images. Since we expect a discrete distribution of volumes
+# (and therefore of coordinates), we assign the centroid coordinate to each image that belongs to that cluster.
+# Evaluate the coordinates estimated
+
 clustered_coords_est = centers[vol_idx]
 coords_perf = sim.eval_coords(mean_est, eigs_est_trunc, clustered_coords_est)
 
-"""
-Output estimated covariance spectrum.
-"""
+
+# Output estimated covariance spectrum.
+
 logger.info(f'Population Covariance Spectrum = {np.diag(lambdas_est)}')
 
-"""
-Output performance results.
-"""
+
+# Output performance results.
+
 logger.info(f'Mean (rel. error) = {mean_perf["rel_err"]}')
 logger.info(f'Mean (correlation) = {mean_perf["corr"]}')
 logger.info(f'Covariance (rel. error) = {covar_perf["rel_err"]}')
