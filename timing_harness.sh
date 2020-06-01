bf9f9c09c77651f7ab6e57a43ea84f51100e7c85
Garrett Wright <garrettwrong@gmail.com>
2020-06-01 09:42:16 -0400

    update timing harness with gpu relevant sizes

--- timing_harness.sh
+++ timing_harness.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-python tutorials/examples/cov2d_hackathon2020_scriptable.py -s 64 -n 128
-python tutorials/examples/cov2d_hackathon2020_scriptable.py -s 64 -n 256
-python tutorials/examples/cov2d_hackathon2020_scriptable.py -s 64 -n 512
+python tutorials/examples/cov2d_hackathon2020_scriptable.py -s 64 -n 128 -p -f vol_10028_emd_2660.mrc
+python tutorials/examples/cov2d_hackathon2020_scriptable.py -s 64 -n 256 -p -f vol_10028_emd_2660.mrc
+python tutorials/examples/cov2d_hackathon2020_scriptable.py -s 64 -n 512 -p -f vol_10028_emd_2660.mrc
+python tutorials/examples/cov2d_hackathon2020_scriptable.py -s 64 -n 1024 -p -f vol_10028_emd_2660.mrc
 
-python tutorials/examples/cov2d_hackathon2020_scriptable.py -s 128 -n 128
-python tutorials/examples/cov2d_hackathon2020_scriptable.py -s 128 -n 256
-python tutorials/examples/cov2d_hackathon2020_scriptable.py -s 128 -n 512
+python tutorials/examples/cov2d_hackathon2020_scriptable.py -s 128 -n 128 -p -f vol_10028_emd_2660.mrc
+python tutorials/examples/cov2d_hackathon2020_scriptable.py -s 128 -n 256 -p -f vol_10028_emd_2660.mrc
+python tutorials/examples/cov2d_hackathon2020_scriptable.py -s 128 -n 512 -p -f vol_10028_emd_2660.mrc
+python tutorials/examples/cov2d_hackathon2020_scriptable.py -s 128 -n 1024 -p -f vol_10028_emd_2660.mrc
 
-python tutorials/examples/cov2d_hackathon2020_scriptable.py -s 256 -n 128
-python tutorials/examples/cov2d_hackathon2020_scriptable.py -s 256 -n 256
-python tutorials/examples/cov2d_hackathon2020_scriptable.py -s 256 -n 512
+python tutorials/examples/cov2d_hackathon2020_scriptable.py -s 180 -n 128 -p -f vol_10028_emd_2660.mrc
+python tutorials/examples/cov2d_hackathon2020_scriptable.py -s 180 -n 256 -p -f vol_10028_emd_2660.mrc
+python tutorials/examples/cov2d_hackathon2020_scriptable.py -s 180 -n 512 -p -f vol_10028_emd_2660.mrc
+python tutorials/examples/cov2d_hackathon2020_scriptable.py -s 180 -n 1024 -p -f vol_10028_emd_2660.mrc
+
+python tutorials/examples/cov2d_hackathon2020_scriptable.py -s 256 -n 128 -p -f vol_10028_emd_2660.mrc
+python tutorials/examples/cov2d_hackathon2020_scriptable.py -s 256 -n 256 -p -f vol_10028_emd_2660.mrc
+python tutorials/examples/cov2d_hackathon2020_scriptable.py -s 256 -n 512 -p -f vol_10028_emd_2660.mrc
+#python tutorials/examples/cov2d_hackathon2020_scriptable.py -s 256 -n 1024 -p -f vol_10028_emd_2660
