Fix sign arg that was forcing a unit test reference change

src/aspire/basis/polar_2d.py
@@ -118,7 +118,7 @@ class PolarBasis2D(Basis):
         half_size = self.ntheta // 2
         pf = np.empty((self.nrad * half_size, nimgs), dtype='complex128', order='F')
         for isample in range(0, nimgs):
-            pf[..., isample] = nufft3(x[..., isample], self.freqs, self.sz)
+            pf[..., isample] = nufft3(x[..., isample], self.freqs, self.sz, isign=1)
 
         pf = m_reshape(pf, (self.nrad, half_size, nimgs))
         v = np.concatenate((pf, pf.conj()), axis=1)

src/aspire/nfft/__init__.py
@@ -114,13 +114,13 @@ class Plan:
             return super(Plan, cls).__new__(cls)
 
 
-def anufft3(vol_f, fourier_pts, sz, real=False):
-    plan = Plan(sz=sz, fourier_pts=fourier_pts)
+def anufft3(vol_f, fourier_pts, sz, real=False, isign=1):
+    plan = Plan(sz=sz, fourier_pts=fourier_pts, isign=isign)
     adjoint = plan.adjoint(vol_f)
     return np.real(adjoint) if real else adjoint
 
 
-def nufft3(vol_f, fourier_pts, sz, real=False):
-    plan = Plan(sz=sz, fourier_pts=fourier_pts)
+def nufft3(vol_f, fourier_pts, sz, real=False, isign=-1):
+    plan = Plan(sz=sz, fourier_pts=fourier_pts, isign=isign)
     transform = plan.transform(vol_f)
     return np.real(transform) if real else transform

src/aspire/nfft/finufft.py
@@ -5,13 +5,14 @@ from aspire.utils import ensure
 
 
 class FINufftPlan(Plan):
-    def __init__(self, sz, fourier_pts, epsilon=1e-15, **kwargs):
+    def __init__(self, sz, fourier_pts, epsilon=1e-15, isign=None, **kwargs):
         """
         A plan for non-uniform FFT (3D)
         :param sz: A tuple indicating the geometry of the signal
         :param fourier_pts: The points in Fourier space where the Fourier transform is to be calculated,
             arranged as a 3-by-K array. These need to be in the range [-pi, pi] in each dimension.
         :param epsilon: The desired precision of the NUFFT
+        :param isign: Defaults to -1 for transform, and 1 for adjoint. May force -1 or 1.
         """
         self.sz = sz
         self.dim = len(sz)
@@ -19,6 +20,7 @@ class FINufftPlan(Plan):
         self.fourier_pts = np.asarray(np.mod(fourier_pts + np.pi, 2 * np.pi) - np.pi, order='C')
         self.num_pts = fourier_pts.shape[1]
         self.epsilon = epsilon
+        self.isign = isign
 
         # Get a handle on the appropriate 1d/2d/3d forward transform function in finufftpy
         self.transform_function = getattr(finufftpy, {1: 'nufft1d2', 2: 'nufft2d2', 3: 'nufft3d2'}[self.dim])
@@ -42,7 +44,7 @@ class FINufftPlan(Plan):
         result_code = self.transform_function(
             *self.fourier_pts,
             result,
-            -1,
+            self.isign or -1,
             epsilon,
             signal
         )
@@ -69,7 +71,7 @@ class FINufftPlan(Plan):
         result_code = self.adjoint_function(
             *self.fourier_pts,
             signal,
-            1,
+            self.isign or 1,
             epsilon,
             *self.sz,
             result