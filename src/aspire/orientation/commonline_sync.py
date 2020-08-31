Fix sign problem of eigenvectors obtained from scipy.parse.linalg.eigsh

src/aspire/orientation/commonline_sync.py
@@ -6,6 +6,7 @@ import scipy.sparse as sparse
 
 from aspire.orientation import CLOrient3D
 from aspire.utils import ensure
+from aspire.utils.matlab_compat import top_eigenvectors
 
 logger = logging.getLogger(__name__)
 
@@ -62,7 +63,11 @@ class CLSyncVoting(CLOrient3D):
         d, v = sparse.linalg.eigsh(S, 10)
         logger.info(f'Top 10 eigenvalues from synchronization voting matrix: {d}')
         sort_idx = np.argsort(-d)
-        v = v[:, sort_idx[:3]]
+
+        # Only need the top 3 eigen-vectors. But there is an ambiguous sign problem.
+        # We need to rescale the eigenvectors to fix it and make consistent
+        # with the Matlab version.
+        v = top_eigenvectors(v, sort_idx[:3])
 
         # According to the structure of W^{T} above, the odd rows of V, denoted V1,
         # are a linear combination of the vectors R_{i}^{1}, i=1,...,K, that is of

src/aspire/utils/matlab_compat.py
@@ -84,7 +84,7 @@ class Random:
             random_states.append(np.random.get_state())
 
             seed = self.seed
-            # 5489 is the default seed used by MATLAB for seed 0 !
+            # 5489 is the default seed used by M   ATLAB for seed 0 !
             if seed == 0:
                 seed = 5489
 
@@ -94,3 +94,21 @@ class Random:
     def __exit__(self, *args):
         if self.seed is not None:
             np.random.set_state(random_states.pop())
+
+
+def top_eigenvectors(v, sort_idx):
+    """
+    Obtain the top eigenvectors and rescale them to fix the sign problem
+
+    :param v: The eigenvectors with each column representing one eigenvector
+    :param sort_idx: The list of sorted indices based on eigenvalues
+    :return:  The eigenvectors consistent with the sorted indices
+    """
+    v = v[:, sort_idx]
+    # Find component index of maximum absolute value of each eigenvector
+    ind_max = np.argmax(np.absolute(v), axis=0)
+    # Rescale eigenvector based on sign from the component with the
+    # maximum absolute value
+    signs = np.array([np.sign(v[ind_max[k], k]) for k in range(len(sort_idx))])
+
+    return v * signs