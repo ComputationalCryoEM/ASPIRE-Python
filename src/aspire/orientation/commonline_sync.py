Change to wrapper function to fix sign problem of eigenvectors

src/aspire/orientation/commonline_sync.py
@@ -2,11 +2,10 @@ import logging
 import numpy as np
 
 from scipy.spatial.transform import Rotation
-import scipy.sparse as sparse
 
 from aspire.orientation import CLOrient3D
 from aspire.utils import ensure
-from aspire.utils.matlab_compat import top_eigenvectors
+from aspire.utils.matlab_compat import m_eigsh
 
 logger = logging.getLogger(__name__)
 
@@ -60,15 +59,12 @@ class CLSyncVoting(CLOrient3D):
         # combinations of the column space of S, namely, W^{T}.
 
         # Extract three eigenvectors corresponding to non-zero eigenvalues.
-        d, v = sparse.linalg.eigsh(S, 10)
+        d, v = m_eigsh(S, 10)
         logger.info(f'Top 10 eigenvalues from synchronization voting matrix: {d}')
         sort_idx = np.argsort(-d)
 
-        # Only need the top 3 eigen-vectors. But there is an ambiguous sign problem.
-        # We need to rescale the eigenvectors to fix it and make consistent
-        # with the Matlab version.
-        v = top_eigenvectors(v, sort_idx[:3])
-
+        # Only need the top 3 eigen-vectors.
+        v = v[:, sort_idx[:3]]
         # According to the structure of W^{T} above, the odd rows of V, denoted V1,
         # are a linear combination of the vectors R_{i}^{1}, i=1,...,K, that is of
         # column 1 of all rotation matrices. Similarly, the even rows of V,

src/aspire/utils/matlab_compat.py
@@ -6,6 +6,8 @@ directly by the caller).
 """
 
 import numpy as np
+import scipy.sparse as sparse
+
 from scipy.special import erfinv
 
 SQRT2 = np.sqrt(2)
@@ -96,19 +98,24 @@ class Random:
             np.random.set_state(random_states.pop())
 
 
-def top_eigenvectors(v, sort_idx):
+def m_eigsh(*args, **kwargs):
     """
-    Obtain the top eigenvectors and rescale them to fix the sign problem
+    A Wrapper function to fix sign problem of eigen-vectors
 
-    :param v: The eigenvectors with each column representing one eigenvector
-    :param sort_idx: The list of sorted indices based on eigenvalues
-    :return:  The eigenvectors consistent with the sorted indices
+    There is an ambiguous sign problem for the eigenvectors from
+    scipy.sparse.linalg.eigsh function. We need to rescale the
+    eigenvectors and make consistent with the Matlab version.
+
+    :param *args: Positional arguments
+    :param **kwargs: Keyword arguments
+    :return: Eigenvalues and eigenvectors
     """
-    v = v[:, sort_idx]
+
+    d, v = sparse.linalg.eigsh(*args, **kwargs)
     # Find component index of maximum absolute value of each eigenvector
     ind_max = np.argmax(np.absolute(v), axis=0)
     # Rescale eigenvector based on sign from the component with the
     # maximum absolute value
-    signs = np.array([np.sign(v[ind_max[k], k]) for k in range(len(sort_idx))])
+    signs = np.array([np.sign(v[ind_max[k], k]) for k in range(len(d))])
 
-    return v * signs
+    return d, v * signs