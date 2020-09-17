a185663624d5a5fefc725f958ab384fcdd2c42e7
Junchao Xia <junchaoxia@hotmail.com>
2020-09-17 17:20:32 -0400

    Implement more wrapper functions for Cupy FFT

--- src/aspire/utils/numeric/cupy.py
+++ src/aspire/utils/numeric/cupy.py
@@ -2,8 +2,16 @@ import cupy as cp
 
 
 class Cupy:
+
+    fft = staticmethod(cp.fft.fft)
+    ifft = staticmethod(cp.fft.ifft)
     fft2 = staticmethod(cp.fft.fft2)
     ifft2 = staticmethod(cp.fft.ifft2)
+    fftn = staticmethod(cp.fft.fftn)
+    ifftn = staticmethod(cp.fft.ifftn)
+
+    fftshift = staticmethod(cp.fft.fftshift)
+    ifftshift = staticmethod(cp.fft.ifftshift)
 
     def __getattr__(self, item):
         """
