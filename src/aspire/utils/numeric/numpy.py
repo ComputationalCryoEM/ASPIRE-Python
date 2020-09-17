1d8902d28e8ee9bfb77047900e4cec879a882577
Junchao Xia <junchaoxia@hotmail.com>
2020-09-17 17:17:33 -0400

    Implement more wrapper functions for pyfftw

--- src/aspire/utils/numeric/numpy.py
+++ src/aspire/utils/numeric/numpy.py
@@ -9,6 +9,23 @@ mutex = Lock()
 class Numpy:
 
     asnumpy = staticmethod(lambda x: x)
+    # no fftshift and ifftshift from pyffw,use those from numpy
+    fftshift = staticmethod(np.fft.fftshift)
+    ifftshift = staticmethod(np.fft.ifftshift)
+
+    @staticmethod
+    def fft(a, axis=-1):
+        mutex.acquire()
+
+        try:
+            a_ = pyfftw.empty_aligned(a.shape, dtype='complex128')
+            b = pyfftw.empty_aligned(a.shape, dtype='complex128')
+            cls = pyfftw.FFTW(a_, b, axes=(axis,), direction='FFTW_FORWARD')
+            cls(a, b)
+        finally:
+            mutex.release()
+
+        return b
 
     @staticmethod
     def fft2(a, axes=(0, 1)):
@@ -31,7 +48,49 @@ class Numpy:
         return b
 
     @staticmethod
+    def fftn(a, axes):
+        mutex.acquire()
+
+        try:
+            a_ = pyfftw.empty_aligned(a.shape, dtype='complex128')
+            b = pyfftw.empty_aligned(a.shape, dtype='complex128')
+            cls = pyfftw.FFTW(a_, b, axes=axes, direction='FFTW_FORWARD')
+            cls(a, b)
+        finally:
+            mutex.release()
+
+        return b
+
+    @staticmethod
+    def ifft(a, axis=-1):
+        mutex.acquire()
+
+        try:
+            a_ = pyfftw.empty_aligned(a.shape, dtype='complex128')
+            b = pyfftw.empty_aligned(a.shape, dtype='complex128')
+            cls = pyfftw.FFTW(a_, b, axes=(axis,), direction='FFTW_BACKWARD')
+            cls(a, b)
+        finally:
+            mutex.release()
+
+        return b
+
+    @staticmethod
     def ifft2(a, axes=(0, 1)):
+        mutex.acquire()
+
+        try:
+            a_ = pyfftw.empty_aligned(a.shape, dtype='complex128')
+            b = pyfftw.empty_aligned(a.shape, dtype='complex128')
+            cls = pyfftw.FFTW(a_, b, axes=axes, direction='FFTW_BACKWARD')
+            cls(a, b)
+        finally:
+            mutex.release()
+
+        return b
+
+    @staticmethod
+    def ifftn(a, axes):
         mutex.acquire()
 
         try:
