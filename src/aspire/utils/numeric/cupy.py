b1fd1943cbb7e218f11de2ab9324c34f6f6fc015
Junchao Xia <junchaoxia@hotmail.com>
2020-09-21 14:47:36 -0400

    Remove old fft wrapper functions for Cupy and Numpy

--- src/aspire/utils/numeric/cupy.py
+++ src/aspire/utils/numeric/cupy.py
@@ -3,16 +3,6 @@ import cupy as cp
 
 class Cupy:
 
-    fft = staticmethod(cp.fft.fft)
-    ifft = staticmethod(cp.fft.ifft)
-    fft2 = staticmethod(cp.fft.fft2)
-    ifft2 = staticmethod(cp.fft.ifft2)
-    fftn = staticmethod(cp.fft.fftn)
-    ifftn = staticmethod(cp.fft.ifftn)
-
-    fftshift = staticmethod(cp.fft.fftshift)
-    ifftshift = staticmethod(cp.fft.ifftshift)
-
     def __getattr__(self, item):
         """
         Catch-all method to to allow a straight pass-through of any attribute that is not supported above.
