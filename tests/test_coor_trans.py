9fa125a223160b1cfc32f262f8a641a24291f4c8
Garrett Wright <garrettwrong@gmail.com>
2020-09-22 14:16:02 -0400

    _dcm ~~> _matrix rename

--- tests/test_coor_trans.py
+++ tests/test_coor_trans.py
@@ -54,10 +54,10 @@ class UtilsTestCase(TestCase):
 
     def testRegisterRots(self):
         angles = uniform_random_angles(32, seed=0)
-        rots_ref = Rotation.from_euler('ZYZ', angles).as_dcm()
+        rots_ref = Rotation.from_euler('ZYZ', angles).as_matrix()
 
         q_ang = [[45, 45, 45]]
-        q_mat = Rotation.from_euler('ZYZ', q_ang, degrees=True).as_dcm()[0]
+        q_mat = Rotation.from_euler('ZYZ', q_ang, degrees=True).as_matrix()[0]
         flag = 0
         regrots_ref = get_aligned_rotations(rots_ref, q_mat, flag)
         q_mat_est, flag_est = register_rotations(rots_ref, regrots_ref)
