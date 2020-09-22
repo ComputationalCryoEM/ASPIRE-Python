c3bf31b5b847fcbe26605946b04a69fe34e7fb00
Junchao Xia <junchaoxia@hotmail.com>
2020-09-22 17:15:06 -0400

    Add unit tests for fft wrapper functions from both scipy and pyfftw

--- tests/test_fft.py
+++ tests/test_fft.py
@@ -1,40 +1,49 @@
 from unittest import TestCase
 
+from aspire.utils.config import config_override
 from aspire.utils.numeric import xp
 from aspire.utils.numeric import fft
 
 
 class ConfigTest(TestCase):
     def setUp(self):
-        pass
+        self.options = ('scipy', 'pyfftw')
 
     def tearDown(self):
         pass
 
     def testFft(self):
-        a = xp.random.random((100))
-        b = fft.fft(a)
-        c = fft.ifft(b)
+        for fft_opt in self.options:
+            with config_override({'common.normal_fft': fft_opt}):
+                a = xp.random.random((100))
+                b = fft.fft(a)
+                c = fft.ifft(b)
 
-        self.assertTrue(xp.allclose(a, c))
+                self.assertTrue(xp.allclose(a, c))
 
     def testFft2(self):
-        a = xp.random.random((100, 100))
-        b = fft.fft2(a)
-        c = fft.ifft2(b)
+        for fft_opt in self.options:
+            with config_override({'common.normal_fft': fft_opt}):
+                a = xp.random.random((100, 100))
+                b = fft.fft2(a)
+                c = fft.ifft2(b)
 
-        self.assertTrue(xp.allclose(a, c))
+                self.assertTrue(xp.allclose(a, c))
 
     def testFftn(self):
-        a = xp.random.random((50, 50, 50))
-        b = fft.fftn(a, axes=(0, 1, 2))
-        c = fft.ifftn(b, axes=(0, 1, 2))
+        for fft_opt in self.options:
+            with config_override({'common.normal_fft': fft_opt}):
+                a = xp.random.random((50, 50, 50))
+                b = fft.fftn(a, axes=(0, 1, 2))
+                c = fft.ifftn(b, axes=(0, 1, 2))
 
-        self.assertTrue(xp.allclose(a, c))
+                self.assertTrue(xp.allclose(a, c))
 
     def testShift(self):
-        a = xp.random.random((100))
-        b = fft.ifftshift(a)
-        c = fft.fftshift(b)
+        for fft_opt in self.options:
+            with config_override({'common.normal_fft': fft_opt}):
+                a = xp.random.random((100))
+                b = fft.ifftshift(a)
+                c = fft.fftshift(b)
 
-        self.assertTrue(xp.allclose(a, c))
+                self.assertTrue(xp.allclose(a, c))
