bcc8a9c871a521beeeeddf95483cbe01058ace47
Junchao Xia <junchaoxia@hotmail.com>
2020-09-22 16:01:46 -0400

    Import scipy.fftpack directly

--- src/aspire/utils/numeric/scipy_fft.py
+++ src/aspire/utils/numeric/scipy_fft.py
@@ -1,14 +1,14 @@
-import scipy as sp
+from scipy import fftpack
 
 
 class ScipyFFT:
 
-    fft = staticmethod(sp.fftpack.fft)
-    ifft = staticmethod(sp.fftpack.ifft)
-    fft2 = staticmethod(sp.fftpack.fft2)
-    ifft2 = staticmethod(sp.fftpack.ifft2)
-    fftn = staticmethod(sp.fftpack.fftn)
-    ifftn = staticmethod(sp.fftpack.ifftn)
+    fft = staticmethod(fftpack.fft)
+    ifft = staticmethod(fftpack.ifft)
+    fft2 = staticmethod(fftpack.fft2)
+    ifft2 = staticmethod(fftpack.ifft2)
+    fftn = staticmethod(fftpack.fftn)
+    ifftn = staticmethod(fftpack.ifftn)
 
-    fftshift = staticmethod(sp.fftpack.fftshift)
-    ifftshift = staticmethod(sp.fftpack.ifftshift)
+    fftshift = staticmethod(fftpack.fftshift)
+    ifftshift = staticmethod(fftpack.ifftshift)
