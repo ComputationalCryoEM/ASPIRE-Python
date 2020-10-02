49c5bf0265effa510f71386e8bebb3768de62e0c
Junchao Xia <junchaoxia@hotmail.com>
2020-10-02 15:27:50 -0400

    Upate Cupy FFT interface and unit tests to be consistent with Scipy and Pyfftw

--- src/aspire/utils/numeric/cupy_fft.py
+++ src/aspire/utils/numeric/cupy_fft.py
@@ -2,13 +2,39 @@ import cupy as cp
 
 
 class CupyFFT:
+    """
+    Define a unified wrapper class for Cupy FFT functions
 
-    fft = staticmethod(cp.fft.fft)
-    ifft = staticmethod(cp.fft.ifft)
-    fft2 = staticmethod(cp.fft.fft2)
-    ifft2 = staticmethod(cp.fft.ifft2)
-    fftn = staticmethod(cp.fft.fftn)
-    ifftn = staticmethod(cp.fft.ifftn)
+    To be consistent with Scipy and Pyfftw, not all arguments are included.
+    """
+    @staticmethod
+    def fft(x, axis=-1, workers=-1):
+        return cp.fft.fft(x, axis=axis)
 
-    fftshift = staticmethod(cp.fft.fftshift)
-    ifftshift = staticmethod(cp.fft.ifftshift)
+    @staticmethod
+    def ifft(x, axis=-1, workers=-1):
+        return cp.fft.ifft(x, axis=axis)
+
+    @staticmethod
+    def fft2(x, axes=(-2, -1), workers=-1):
+        return cp.fft.fft2(x, axes=axes)
+
+    @staticmethod
+    def ifft2(x,  axes=(-2, -1), workers=-1):
+        return cp.fft.ifft2(x, axes=axes)
+
+    @staticmethod
+    def fftn(x, axes=None, workers=-1):
+        return cp.fft.fftn(x, axes=axes)
+
+    @staticmethod
+    def ifftn(x,  axes=None, workers=-1):
+        return cp.fft.ifft2(x, axes=axes)
+
+    @staticmethod
+    def fftshift(x, axes=None):
+        return cp.fft.fftshift(x, axes=axes)
+
+    @staticmethod
+    def ifftshift(x, axes=None):
+        return cp.fft.ifftshift(x, axes=axes)
--- tests/test_fft.py
+++ tests/test_fft.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 import numpy as np
+from aspire import config
 from aspire.utils.numeric import fft_class
 
 fft_backends = ('scipy', 'pyfftw')
@@ -9,32 +10,66 @@ fft_classes = [fft_class(backend) for backend in fft_backends]
 class ConfigTest(TestCase):
     def testFft(self):
         for fft in fft_classes:
-            a = np.random.random(100)
-            b = fft.fft(a, workers=2)
-            c = fft.ifft(b, workers=-1)
+            for nworkers in (-1, 1):
+                a = np.random.random(100)
+                b = fft.fft(a, workers=nworkers)
+                c = fft.ifft(b, workers=nworkers)
+                self.assertTrue(np.allclose(a, c))
 
-            self.assertTrue(np.allclose(a, c))
+        if config.common.cupy:
+            from aspire.utils.numeric import xp
+            fft = fft_class('cupy')
+            a = xp.random.random(100)
+            # workers is not used for cupy
+            b = fft.fft(a, workers=-1)
+            c = fft.ifft(b, workers=-1)
+            self.assertTrue(xp.allclose(a, c))
 
     def testFft2(self):
         for fft in fft_classes:
-            a = np.random.random((100, 100))
-            b = fft.fft2(a, workers=-1)
-            c = fft.ifft2(b)
+            for nworkers in (-1, 1):
+                a = np.random.random((100, 100))
+                b = fft.fft2(a, workers=nworkers)
+                c = fft.ifft2(b, workers=nworkers)
+                self.assertTrue(np.allclose(a, c))
 
-            self.assertTrue(np.allclose(a, c))
+        if config.common.cupy:
+            from aspire.utils.numeric import xp
+            fft = fft_class('cupy')
+            a = xp.random.random((100, 100))
+            # workers is not used for cupy
+            b = fft.fft2(a, workers=-1)
+            c = fft.ifft2(b, workers=-1)
+            self.assertTrue(xp.allclose(a, c))
 
     def testFftn(self):
         for fft in fft_classes:
-            a = np.random.random((50, 50, 50))
-            b = fft.fftn(a, axes=(0, 1, 2), workers=2)
-            c = fft.ifftn(b, axes=(0, 1, 2), workers=3)
+            for nworkers in (-1, 1):
+                a = np.random.random((50, 50, 50))
+                b = fft.fftn(a, axes=(0, 1, 2), workers=nworkers)
+                c = fft.ifftn(b, axes=(0, 1, 2), workers=nworkers)
+                self.assertTrue(np.allclose(a, c))
 
-            self.assertTrue(np.allclose(a, c))
+        if config.common.cupy:
+            from aspire.utils.numeric import xp
+            fft = fft_class('cupy')
+            a = xp.random.random((50, 50, 50))
+            # workers is not used for cupy
+            b = fft.fftn(a, axes=(0, 1, 2), workers=-1)
+            c = fft.ifftn(b, axes=(0, 1, 2), workers=-1)
+            self.assertTrue(xp.allclose(a, c))
 
     def testShift(self):
         for fft in fft_classes:
             a = np.random.random(100)
             b = fft.ifftshift(a)
             c = fft.fftshift(b)
-
             self.assertTrue(np.allclose(a, c))
+
+        if config.common.cupy:
+            from aspire.utils.numeric import xp
+            fft = fft_class('cupy')
+            a = xp.random.random(100)
+            b = fft.ifftshift(a)
+            c = fft.fftshift(b)
+            self.assertTrue(xp.allclose(a, c))
