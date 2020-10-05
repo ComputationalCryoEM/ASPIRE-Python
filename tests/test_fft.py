903700ef330e5dcf2e818fba92aa6f0b4a1819a4
Junchao Xia <junchaoxia@hotmail.com>
2020-10-05 09:30:56 -0400

    Improve unit tests of normal FFT

--- tests/test_fft.py
+++ tests/test_fft.py
@@ -10,54 +10,57 @@ fft_classes = [fft_class(backend) for backend in fft_backends]
 class ConfigTest(TestCase):
     def testFft(self):
         for fft in fft_classes:
-            for nworkers in (-1, 1):
+            for nworkers in (-1, 1, 2):
                 a = np.random.random(100)
                 b = fft.fft(a, workers=nworkers)
                 c = fft.ifft(b, workers=nworkers)
                 self.assertTrue(np.allclose(a, c))
 
+        # Cupy is not default install and need test separately
         if config.common.cupy:
-            from aspire.utils.numeric import xp
+            import cupy as cp
             fft = fft_class('cupy')
-            a = xp.random.random(100)
+            a = cp.random.random(100)
             # workers is not used for cupy
             b = fft.fft(a, workers=-1)
             c = fft.ifft(b, workers=-1)
-            self.assertTrue(xp.allclose(a, c))
+            self.assertTrue(cp.allclose(a, c))
 
     def testFft2(self):
         for fft in fft_classes:
-            for nworkers in (-1, 1):
+            for nworkers in (-1, 1, 2):
                 a = np.random.random((100, 100))
                 b = fft.fft2(a, workers=nworkers)
                 c = fft.ifft2(b, workers=nworkers)
                 self.assertTrue(np.allclose(a, c))
 
+        # Cupy is not default install and need test separately
         if config.common.cupy:
-            from aspire.utils.numeric import xp
+            import cupy as cp
             fft = fft_class('cupy')
-            a = xp.random.random((100, 100))
+            a = cp.random.random((100, 100))
             # workers is not used for cupy
             b = fft.fft2(a, workers=-1)
             c = fft.ifft2(b, workers=-1)
-            self.assertTrue(xp.allclose(a, c))
+            self.assertTrue(cp.allclose(a, c))
 
     def testFftn(self):
         for fft in fft_classes:
-            for nworkers in (-1, 1):
+            for nworkers in (-1, 1, 2):
                 a = np.random.random((50, 50, 50))
                 b = fft.fftn(a, axes=(0, 1, 2), workers=nworkers)
                 c = fft.ifftn(b, axes=(0, 1, 2), workers=nworkers)
                 self.assertTrue(np.allclose(a, c))
 
+        # Cupy is not default install and need test separately
         if config.common.cupy:
-            from aspire.utils.numeric import xp
+            import cupy as cp
             fft = fft_class('cupy')
-            a = xp.random.random((50, 50, 50))
+            a = cp.random.random((50, 50, 50))
             # workers is not used for cupy
             b = fft.fftn(a, axes=(0, 1, 2), workers=-1)
             c = fft.ifftn(b, axes=(0, 1, 2), workers=-1)
-            self.assertTrue(xp.allclose(a, c))
+            self.assertTrue(cp.allclose(a, c))
 
     def testShift(self):
         for fft in fft_classes:
@@ -66,10 +69,11 @@ class ConfigTest(TestCase):
             c = fft.fftshift(b)
             self.assertTrue(np.allclose(a, c))
 
+        # Cupy is not default install and need test separately
         if config.common.cupy:
-            from aspire.utils.numeric import xp
+            import cupy as cp
             fft = fft_class('cupy')
-            a = xp.random.random(100)
+            a = cp.random.random(100)
             b = fft.ifftshift(a)
             c = fft.fftshift(b)
-            self.assertTrue(xp.allclose(a, c))
+            self.assertTrue(cp.allclose(a, c))
