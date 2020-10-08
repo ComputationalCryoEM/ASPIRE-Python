d71df3c98ea961efad2c99344527e125ef6115b7
Garrett Wright <garrettwrong@gmail.com>
2020-10-08 13:08:46 -0400

    Convert logging to config file format, add level and fileHandler
    Tee DEBUG to file, INFO to console

--- src/aspire/__init__.py
+++ src/aspire/__init__.py
@@ -1,5 +1,7 @@
+import os
 import logging.config
 
+from datetime import datetime
 from importlib_resources import read_text
 
 import aspire
@@ -9,31 +11,15 @@ from aspire.utils.config import Config
 # version in maj.min.bld format
 __version__ = '0.6.0'
 
-logging.config.dictConfig({
-    "version": 1,
-    "formatters": {
-        "simple_formatter": {
-            "format": "%(asctime)s %(message)s",
-            "datefmt": "%Y/%m/%d %H:%M:%S"
-        }
-    },
-    "handlers": {
-        "console": {
-            "class": "logging.StreamHandler",
-            "formatter": "simple_formatter",
-            "level": "DEBUG",
-            "stream": "ext://sys.stdout"
-        }
-    },
-    "loggers": {
-        "aspire": {
-            "level": "DEBUG",
-            "handlers": ["console"]
-        }
-    }
-})
+# Generates file name and opens log file defined in config file.
+# The default is to use the current time stamp provided in the dictionary,
+#   but that is not required if a user wishes to override filename.
+logging.config.fileConfig(
+    os.path.join(os.path.dirname(__file__), 'logging.conf'),
+    defaults={'dt_stamp': datetime.now().strftime('%Y-%m-%dT%H-%M-%S.%f')})
 
+# Implements some code that writes out exceptions to 'aspire.err.log'.
 config = Config(read_text(aspire, 'config.ini'))
-if config.common.log_errors:
+if config.common.log_exceptions:
     import sys
     sys.excepthook = handle_exception
