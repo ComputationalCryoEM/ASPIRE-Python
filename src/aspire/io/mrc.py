46085a398ab740e05ca8e56704ec0f0a417f65bc
Garrett Wright <garrettwrong@gmail.com>
2020-10-23 09:41:01 -0400

    Some cleanup before review

--- src/aspire/io/mrc.py
+++ src/aspire/io/mrc.py
@@ -5,6 +5,8 @@ class MrcStats:
     def __init__(self):
         """
         Instantiate an empty instance ready to receive slices of arrays.
+
+        :return: MrcStats instance
         """
 
         self.amin = np.inf
@@ -16,6 +18,8 @@ class MrcStats:
     def push(self, array_slice):
         """
         Incrementally add contribution of array_slice to stats.
+
+        :param array_slice: An ndarray to contribute to stats.
         """
 
         self.amin = min(self.amin, np.min(array_slice))
@@ -40,12 +44,15 @@ class MrcStats:
 
         We use the simplest method.
         """
+
         var = self.asum2 / self.asize - (self.asum / self.asize) ** 2
         return np.sqrt(var)
 
     def update_header(self, mrcobj):
         """
         Assign the statistics to `mrcobj` header.
+
+        :param mrcobj: The `mrcfile` instance we want stats written to.
         """
 
         mrcobj.header.dmin = self.amin.astype(np.float32)
