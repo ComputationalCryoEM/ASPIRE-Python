092fa11dc7d9dfa155c0a4fe4a610e15786b2b0c
Junchao Xia <junchaoxia@hotmail.com>
2020-10-23 17:37:03 -0400

    Rename numeric_classes to numeric_modules

--- tests/test_fft.py
+++ tests/test_fft.py
@@ -5,14 +5,14 @@ from aspire import config
 from aspire.utils.numeric import fft_object
 
 fft_backends = ['scipy', 'pyfftw']
-numeric_classes = [np, np]
+numeric_modules = [np, np]
 
 # Create Cupy fft backend if Cupy is enabled and lib exits.
 if config.common.cupy:
     try:
         import cupy as cp
         fft_backends.append('cupy')
-        numeric_classes.append(cp)
+        numeric_modules.append(cp)
     except ImportError:
         pass
 
@@ -21,7 +21,7 @@ fft_objects = [fft_object(backend) for backend in fft_backends]
 
 class ConfigTest(TestCase):
     def testFft(self):
-        for fft, xp in zip(fft_objects, numeric_classes):
+        for fft, xp in zip(fft_objects, numeric_modules):
             for nworkers in (-1, 1, 2):
                 a = xp.random.random(100)
                 b = fft.fft(a, workers=nworkers)
@@ -29,7 +29,7 @@ class ConfigTest(TestCase):
                 self.assertTrue(xp.allclose(a, c))
 
     def testFft2(self):
-        for fft, xp in zip(fft_objects, numeric_classes):
+        for fft, xp in zip(fft_objects, numeric_modules):
             for nworkers in (-1, 1, 2):
                 a = xp.random.random((100, 100))
                 b = fft.fft2(a, workers=nworkers)
@@ -37,7 +37,7 @@ class ConfigTest(TestCase):
                 self.assertTrue(xp.allclose(a, c))
 
     def testFftn(self):
-        for fft, xp in zip(fft_objects, numeric_classes):
+        for fft, xp in zip(fft_objects, numeric_modules):
             for nworkers in (-1, 1, 2):
                 a = xp.random.random((50, 50, 50))
                 b = fft.fftn(a, axes=(0, 1, 2), workers=nworkers)
@@ -45,14 +45,14 @@ class ConfigTest(TestCase):
                 self.assertTrue(xp.allclose(a, c))
 
     def testShift(self):
-        for fft, xp in zip(fft_objects, numeric_classes):
+        for fft, xp in zip(fft_objects, numeric_modules):
             a = xp.random.random(100)
             b = fft.ifftshift(a)
             c = fft.fftshift(b)
             self.assertTrue(xp.allclose(a, c))
 
     def testCenteredFft(self):
-        for fft, xp in zip(fft_objects, numeric_classes):
+        for fft, xp in zip(fft_objects, numeric_modules):
             for nworkers in (-1, 1, 2):
                 a = xp.random.random(100)
                 b = fft.centered_fft(a, workers=nworkers)
@@ -60,7 +60,7 @@ class ConfigTest(TestCase):
                 self.assertTrue(xp.allclose(a, c))
 
     def testCenteredFft2(self):
-        for fft, xp in zip(fft_objects, numeric_classes):
+        for fft, xp in zip(fft_objects, numeric_modules):
             for nworkers in (-1, 1, 2):
                 a = xp.random.random((100, 100))
                 b = fft.centered_fft2(a, workers=nworkers)
@@ -68,7 +68,7 @@ class ConfigTest(TestCase):
                 self.assertTrue(xp.allclose(a, c))
 
     def testCenteredFftn(self):
-        for fft, xp in zip(fft_objects, numeric_classes):
+        for fft, xp in zip(fft_objects, numeric_modules):
             for nworkers in (-1, 1, 2):
                 a = xp.random.random((50, 50, 50))
                 b = fft.centered_fftn(a, axes=(0, 1, 2), workers=nworkers)
