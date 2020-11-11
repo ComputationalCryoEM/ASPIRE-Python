161c5a520cd56ac7144a10ea613ac00c750fa354
Junchao Xia <junchaoxia@hotmail.com>
2020-11-11 12:03:11 -0500

    Rum commands to improve format

--- tests/test_preprocess_pipeline.py
+++ tests/test_preprocess_pipeline.py
@@ -10,7 +10,7 @@ from aspire.utils.coor_trans import grid_2d, grid_3d
 from aspire.utils.filters import RadialCTFFilter, ScalarFilter
 from aspire.volume import Volume
 
-DATA_DIR = os.path.join(os.path.dirname(__file__), 'saved_test_data')
+DATA_DIR = os.path.join(os.path.dirname(__file__), "saved_test_data")
 
 
 class PreprocessPLTestCase(TestCase):
@@ -25,9 +25,10 @@ class PreprocessPLTestCase(TestCase):
         self.sim = Simulation(
             L=self.L,
             n=self.n,
-            unique_filters=[RadialCTFFilter(defocus=d)
-                            for d in np.linspace(1.5e4, 2.5e4, 7)],
-            noise_filter=self.noise_filter
+            unique_filters=[
+                RadialCTFFilter(defocus=d) for d in np.linspace(1.5e4, 2.5e4, 7)
+            ],
+            noise_filter=self.noise_filter,
         )
         self.imgs_org = self.sim.images(start=0, num=self.n)
 
@@ -41,10 +42,10 @@ class PreprocessPLTestCase(TestCase):
     def testDownsample(self):
         # generate a 3D map with density decays as Gaussian function
         g3d = grid_3d(self.L)
-        coords = np.array([g3d['x'].flatten(), g3d['y'].flatten(), g3d['z'].flatten()])
+        coords = np.array([g3d["x"].flatten(), g3d["y"].flatten(), g3d["z"].flatten()])
         sigma = 0.2
-        vol = np.exp(-0.5 * np.sum(np.abs(coords/sigma)**2, axis=0))
-        vol = np.reshape(vol, g3d['x'].shape)
+        vol = np.exp(-0.5 * np.sum(np.abs(coords / sigma) ** 2, axis=0))
+        vol = np.reshape(vol, g3d["x"].shape)
         vols = Volume(vol)
 
         # set noise to zero and CFT filters to unity for simulation object
@@ -56,10 +57,10 @@ class PreprocessPLTestCase(TestCase):
             vols=vols,
             offsets=0.0,
             amplitudes=1.0,
-            unique_filters=[ScalarFilter(dim=2, value=1)
-                            for d in np.linspace(1.5e4, 2.5e4, 7)],
-            noise_filter=noise_filter
-
+            unique_filters=[
+                ScalarFilter(dim=2, value=1) for d in np.linspace(1.5e4, 2.5e4, 7)
+            ],
+            noise_filter=noise_filter,
         )
         # get images before downsample
         imgs_org = sim.images(start=0, num=self.n)
@@ -71,20 +72,21 @@ class PreprocessPLTestCase(TestCase):
         # check resolution
         self.assertTrue(np.allclose(max_resolution, imgs_ds.shape[1]))
         # check energy conservation after downsample
-        self.assertTrue(np.allclose(imgs_org.norm(),
-                                    self.L/max_resolution*imgs_ds.norm()))
+        self.assertTrue(
+            np.allclose(imgs_org.norm(), self.L / max_resolution * imgs_ds.norm())
+        )
 
     def testNormBackground(self):
         bg_radius = 1.0
         grid = grid_2d(self.L)
-        mask = (grid['r'] > bg_radius)
+        mask = grid["r"] > bg_radius
         self.sim.normalize_background()
         imgs_nb = self.sim.images(start=0, num=self.n).asnumpy()
         new_mean = np.mean(imgs_nb[:, mask])
         new_variance = np.var(imgs_nb[:, mask])
 
         # new mean of noise should be close to zero and variance should be close to 1
-        self.assertTrue(new_mean < 1e-7 and abs(new_variance-1) < 1e-7)
+        self.assertTrue(new_mean < 1e-7 and abs(new_variance - 1) < 1e-7)
 
     def testWhiten(self):
         noise_estimator = WhiteNoiseEstimator(self.sim)
@@ -92,8 +94,9 @@ class PreprocessPLTestCase(TestCase):
         imgs_wt = self.sim.images(start=0, num=self.n).asnumpy()
 
         # calculate correlation between two neighboring pixels from background
-        corr_coef = np.corrcoef(imgs_wt[:, self.L-1, self.L-1],
-                                imgs_wt[:, self.L-2, self.L-1])
+        corr_coef = np.corrcoef(
+            imgs_wt[:, self.L - 1, self.L - 1], imgs_wt[:, self.L - 2, self.L - 1]
+        )
 
         # correlation matrix should be close to identity
         self.assertTrue(np.allclose(np.eye(2), corr_coef, atol=1e-1))
