37a95708e2ca7bf95601eb0ed973f4d21199ca31
Junchao Xia <junchaoxia@hotmail.com>
2020-11-16 12:22:17 -0500

    Clean up all unit tests for Batched Cov2D class

--- tests/test_batched_covar2d.py
+++ tests/test_batched_covar2d.py
@@ -13,49 +13,25 @@ class BatchedRotCov2DTestCase(TestCase):
     def setUp(self):
         n = 32
         L = 8
-        self.dtype = np.float32
-        noise_var = 0.1848
-        noise_filter = ScalarFilter(dim=2, value=noise_var)
-        self.noise_var = noise_var
-
-        pixel_size = 5
-        voltage = 200
-        defocus_min = 1.5e4
-        defocus_max = 2.5e4
-        defocus_ct = 7
-
         filters = [
-            RadialCTFFilter(pixel_size, voltage, defocus=d, Cs=2.0, alpha=0.1)
-            for d in np.linspace(defocus_min, defocus_max, defocus_ct)
+            RadialCTFFilter(5, 200, defocus=d, Cs=2.0, alpha=0.1)
+            for d in np.linspace(1.5e4, 2.5e4, 7)
         ]
+        self.dtype = np.float32
+        self.noise_var = 0.1848
+        noise_filter = ScalarFilter(dim=2, value=self.noise_var)
 
-        # Since FFBBasis2D doesn't yet implement dtype, we'll set this to double to match its built in types.
-        src = Simulation(
+        self.src = Simulation(
             L, n, unique_filters=filters, dtype=self.dtype, noise_filter=noise_filter
         )
+        self.basis = FFBBasis2D((L, L), dtype=self.dtype)
+        self.coeff = self.basis.evaluate_t(self.src.images(0, self.src.n))
 
-        basis = FFBBasis2D((L, L), dtype=self.dtype)
+        self.ctf_idx = self.src.filter_indices
+        self.ctf_fb = [f.fb_mat(self.basis) for f in self.src.unique_filters]
 
-        unique_filters = src.unique_filters
-        ctf_idx = src.filter_indices
-
-        ctf_fb = [f.fb_mat(basis) for f in unique_filters]
-
-        im = src.images(0, src.n)
-        coeff = basis.evaluate_t(im.data)
-
-        cov2d = RotCov2D(basis)
-        bcov2d = BatchedRotCov2D(src, basis, batch_size=7)
-
-        self.src = src
-        self.basis = basis
-        self.ctf_fb = ctf_fb
-        self.ctf_idx = ctf_idx
-
-        self.cov2d = cov2d
-        self.bcov2d = bcov2d
-
-        self.coeff = coeff
+        self.cov2d = RotCov2D(self.basis)
+        self.bcov2d = BatchedRotCov2D(self.src, self.basis, batch_size=7)
 
     def tearDown(self):
         pass
@@ -66,7 +42,7 @@ class BatchedRotCov2DTestCase(TestCase):
             close = close and np.allclose(blk_a, blk_b, atol=atol)
         return close
 
-    def test01(self):
+    def testMeanCovar(self):
         # Test basic functionality against RotCov2D.
 
         mean_cov2d = self.cov2d.get_mean(
@@ -93,7 +69,7 @@ class BatchedRotCov2DTestCase(TestCase):
             )
         )
 
-    def test02(self):
+    def testZeroMean(self):
         # Make sure it works with zero mean (pure second moment).
         zero_coeff = np.zeros((self.basis.count,), dtype=self.dtype)
 
@@ -109,7 +85,7 @@ class BatchedRotCov2DTestCase(TestCase):
             )
         )
 
-    def test03(self):
+    def testAutoMean(self):
         # Make sure it automatically calls get_mean if needed.
         covar_cov2d = self.cov2d.get_covar(
             self.coeff, ctf_fb=self.ctf_fb, ctf_idx=self.ctf_idx
@@ -123,7 +99,7 @@ class BatchedRotCov2DTestCase(TestCase):
             )
         )
 
-    def test04(self):
+    def testShrink(self):
         # Make sure it properly shrinks the right-hand side if specified.
         covar_est_opt = {
             "shrinker": "frobenius_norm",
@@ -146,7 +122,7 @@ class BatchedRotCov2DTestCase(TestCase):
 
         self.assertTrue(self.blk_diag_allclose(covar_cov2d, covar_bcov2d))
 
-    def test05(self):
+    def testAutoBasis(self):
         # Make sure basis is automatically created if not specified.
         nbcov2d = BatchedRotCov2D(self.src)
 
@@ -159,44 +135,43 @@ class BatchedRotCov2DTestCase(TestCase):
             )
         )
 
-    def test06(self):
+    def testCWFCoeff(self):
         # Calculate CWF coefficients using Cov2D base class
-        mean_bcov2d = self.cov2d.get_mean(
+        mean_cov2d = self.cov2d.get_mean(
             self.coeff, ctf_fb=self.ctf_fb, ctf_idx=self.ctf_idx
         )
-        covar_bcov2d = self.cov2d.get_covar(
+        covar_cov2d = self.cov2d.get_covar(
             self.coeff,
             ctf_fb=self.ctf_fb,
             ctf_idx=self.ctf_idx,
             noise_var=self.noise_var,
         )
 
-        coeff_bcov2d = self.bcov2d.get_cwf_coeffs(
+        coeff_cov2d = self.cov2d.get_cwf_coeffs(
             self.coeff,
             self.ctf_fb,
             self.ctf_idx,
-            mean_coeff=mean_bcov2d,
-            covar_coeff=covar_bcov2d,
+            mean_coeff=mean_cov2d,
+            covar_coeff=covar_cov2d,
             noise_var=self.noise_var,
         )
 
         # Calculate CWF coefficients using Batched Cov2D class
-        nbcov2d = BatchedRotCov2D(self.src)
-        mean_nbcov2d = nbcov2d.get_mean()
-        covar_nbcov2d = nbcov2d.get_covar(noise_var=self.noise_var)
+        mean_bcov2d = self.bcov2d.get_mean()
+        covar_bcov2d = self.bcov2d.get_covar(noise_var=self.noise_var)
 
-        coeff_nbcov2d = nbcov2d.get_cwf_coeffs(
+        coeff_bcov2d = self.bcov2d.get_cwf_coeffs(
             self.coeff,
             self.ctf_fb,
             self.ctf_idx,
-            mean_nbcov2d,
-            covar_nbcov2d,
+            mean_bcov2d,
+            covar_bcov2d,
             noise_var=self.noise_var,
         )
         self.assertTrue(
             self.blk_diag_allclose(
+                coeff_cov2d,
                 coeff_bcov2d,
-                coeff_nbcov2d,
                 # Note, the Batched class has reduced resolution,
                 #  compared to the non batched method.
                 atol=utest_tolerance(self.dtype),
