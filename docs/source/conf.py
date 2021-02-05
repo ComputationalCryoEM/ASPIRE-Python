b33e1e652cf7f67929213bc0ccefaebbc9b4119b
Garrett Wright <garrettwrong@gmail.com>
2021-02-05 09:02:20 -0500

    Add bibtex_bibfiles to docs conf
    sphinxcontrib.bibtex now requires bibtex_bibfiles setting

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -39,6 +39,8 @@ extensions = [
     "sphinxcontrib.bibtex",
     "numpydoc",
 ]
+bibtex_bibfiles = ["references.bib"]
+
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
