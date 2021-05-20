edb5224926b6f21b241813b41ace740f9fefc373
Garrett Wright <garrettwrong@gmail.com>
2021-05-20 10:59:01 -0400

    add wemd function and change docstring format

--- src/aspire/operators/wemd.py
+++ src/aspire/operators/wemd.py
@@ -13,24 +13,23 @@ import pywt
 
 def embed(arr, wavelet, level):
     """
-    This function computes an embedding of Numpy arrays such that the L1 distance
-    between the resulting embeddings is approximately equal to the Earthmover distance of the arrays.
+    This function computes an embedding of Numpy arrays such that
+    the L1 distance between the resulting embeddings is approximately
+    equal to the Earthmover distance of the arrays.
 
-    Input:
-        arr - numpy array.
-        level - Decomposition level of the wavelets. Larger levels yield more coefficients and more accurate results.
-        wavelet - either the name of a wavelet supported by PyWavelets (e.g. 'coif3', 'sym3') or a pywt.Wavelet object.
-                  See https://pywavelets.readthedocs.io/en/latest/ref/wavelets.html#built-in-wavelets-wavelist
-    Output:
-        One-dimensional numpy array containing weighted details coefficients.
-        Approximate Earthmover's distances are then given by the l_1 distances of the results, e.g.
-            wemd(arr1, arr2) := numpy.linalg.norm(embed(arr1, wavelet, level)-embed(arr2, wavelet, level), ord=1)
+    :param arr: Numpy array
+    :param level: Decomposition level of the wavelets
+    Larger levels yield more coefficients and more accurate results
+    :param wavelet: Either the name of a wavelet supported by PyWavelets
+    (e.g. 'coif3', 'sym3') or a pywt.Wavelet object
+    See https://pywavelets.readthedocs.io/en/latest/ref/wavelets.html#built-in-wavelets-wavelist
+    :returns: One-dimensional numpy array containing weighted details coefficients.
     """
 
     arrdwt = pywt.wavedecn(arr/arr.sum(), wavelet, mode='zero', level=level)
 
     dimension = len(arr.shape)
-    assert dimension in [2,3]
+    assert dimension in [2,3], f'Dimension {dimension} should be 2 or 3'
 
     n_levels = len(arrdwt[1:])
 
@@ -41,3 +40,22 @@ def embed(arr, wavelet, level):
             weighted_coefs.append(coefs.flatten()*multiplier)
 
     return np.concatenate(weighted_coefs)
+
+def wemd(arr1, arr2, wavelet, level):
+    """
+    Approximate Earthmover's distance between  using `embed`.
+
+    :param arr1: Numpy array
+    :param arr2: Numpy array
+    :param level: Decomposition level of the wavelets
+    Larger levels yield more coefficients and more accurate results
+    :param wavelet: Either the name of a wavelet supported by PyWavelets
+    (e.g. 'coif3', 'sym3') or a pywt.Wavelet object
+    See https://pywavelets.readthedocs.io/en/latest/ref/wavelets.html#built-in-wavelets-wavelist
+    :return: Approximated Earthmover Distance
+    """
+
+    coefs1 = embed(arr1, wavelet, level)
+    coefs2 = embed(arr2, wavelet, level)
+
+    return np.linalg.norm(coefs1 - coefs2, ord=1)
