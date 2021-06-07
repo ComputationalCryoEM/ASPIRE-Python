29982cf65878e5302f50709f11c7ac20276dc307
Amit Moscovich <moscovich@gmail.com>
2021-06-07 19:32:19 -0400

    Minor performance improvement of wemd_embed

--- src/aspire/operators/wemd.py
+++ src/aspire/operators/wemd.py
@@ -42,9 +42,9 @@ def wemd_embed(arr, wavelet='coif3', level=None):
 
     weighted_coefs = []
     for (j, details_level_j) in enumerate(detail_coefs):
+        multiplier = 2 ** ((level-1-j) * (1+(dimension/2.0)))
         for coefs in details_level_j.values():
-            multiplier = 2 ** ((level-1-j) * (1+(dimension/2.0)))
-            weighted_coefs.append(coefs.flatten() * multiplier)
+            weighted_coefs.append(multiplier * coefs.flatten())
 
     return np.concatenate(weighted_coefs)
 
