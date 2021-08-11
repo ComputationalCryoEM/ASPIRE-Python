587ec1755d9d36de67f58d2e4cf21bcdb5bf3fd0
Garrett Wright <garrettwrong@gmail.com>
2021-08-11 13:27:33 -0400

    Remove superflous unit tests

--- tests/test_adaptive_support.py
+++ tests/test_adaptive_support.py
@@ -8,7 +8,7 @@ import pytest
 from aspire.denoising import adaptive_support
 from aspire.image import Image
 from aspire.source import ArrayImageSource
-from aspire.utils import circ, gaussian_2d, inverse_r
+from aspire.utils import gaussian_2d
 
 logger = logging.getLogger(__name__)
 
@@ -57,34 +57,6 @@ class AdaptiveSupportTest(TestCase):
             # Pass numpy array.
             _ = adaptive_support(np.empty((10, 32, 32)))
 
-    @pytest.mark.xfail(reason="Adaptive Support Algorithm may be incorrect.")
-    def test_adaptive_support_inverse_r(self):
-        """
-        Test `inverse_r` function support in Real and Fourier space is similar.
-
-        The Fourier transform of `inverse_r` should be similar to real space,
-        so we can test the support is similar.
-        """
-
-        # Generate stack of inverse_r function images.
-        imgs = np.tile(
-            inverse_r(self.size),
-            (self.n_disc, 1, 1),
-        )
-
-        # Setup ImageSource like objects
-        img_src = ArrayImageSource(Image(imgs))
-
-        thresholds = list(self.references.values())
-
-        for threshold in thresholds:
-            c, r = adaptive_support(img_src, threshold)
-            # Test support is similar between original and transformed
-            logger.info(
-                f"Threshold {threshold} c {c} cr { c * self.size} R {r} diff {abs(r - c * self.size) / r }"
-            )
-            self.assertTrue(abs(r - c * self.size) / r < 0.2)
-
     def test_adaptive_support_F(self):
         """
         Test Fourier support of Gaussian relates to normal distribution.
@@ -121,30 +93,3 @@ class AdaptiveSupportTest(TestCase):
             # range (too small, R is inaccurate; too big, c is inaccurate.
             self.assertTrue(abs(R - R_true) / R_true < 0.05)
             self.assertTrue(abs(c - c_true) / c_true < 0.05)
-
-    def test_adaptive_support_gaussian_circ(self):
-        """
-        Test against known Gaussian + circ.
-        """
-
-        # Create 2D Gaussian as initial array.
-        discs = np.tile(
-            gaussian_2d(self.size, sigma_x=self.sigma, sigma_y=self.sigma),
-            (self.n_disc, 1, 1),
-        )
-
-        # Add varying radius of solid disc.
-        #  The Fourier transform will yield Airy disc
-        #  which has more interesting content in F space.
-        for d in range(self.n_disc):
-            discs[d] = discs[d] + circ(self.size, radius=(d + 1) ** 2)
-
-        img_src = ArrayImageSource(Image(discs))
-
-        # for one, two, three, inf standard deviations (one sided)
-        for stddevs, threshold in self.references.items():
-            # Real support should be ~ 1, 2, 3 times sigma
-            c, r = adaptive_support(img_src, threshold)
-            # Check closer to this threshold than next
-            logger.info(f"{c} {r} = adaptive_support(..., {threshold})")
-            self.assertTrue(np.abs(r / self.sigma - stddevs) < 0.5)
