db5617810fc7bf5251538c0eb10230c625b2c63c
Chris Langfield <christopher.langfield@gmail.com>
2021-09-01 09:41:15 -0400

    staticmethod not classmethod

--- src/aspire/storage/starfile.py
+++ src/aspire/storage/starfile.py
@@ -7,7 +7,7 @@ class StarFile:
     # StarFile class currently exists to contain the methods below, which wrap the read() and write() 
     # methods of the starfile package
 
-    @classmethod
+    @staticmethod
     def read(filepath, n_blocks=None):
         # for consistency's sake, we enable the always_dict flag 
         # this overrides the default behavior of the method,
@@ -20,7 +20,7 @@ class StarFile:
         star = starfile.read(filepath, read_n_blocks=n_blocks, always_dict=True)
         return star
 
-    @classmethod
+    @staticmethod
     def write(df, filename, float_format='%.6f', sep='\t', na_rep='<NA>', overwrite=True):
         # wraps the write() method exactly, except that we enable the overwrite flag by default
         # to mimic the behavior of the previous StarFile class
