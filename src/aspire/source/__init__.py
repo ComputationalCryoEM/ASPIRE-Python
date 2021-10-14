6164dcc9913ff8b3a137986390fafe277c17ba45
Chris Langfield <christopher.langfield@gmail.com>
2021-10-14 18:00:39 -0400

    creation of EmanSource

--- src/aspire/source/__init__.py
+++ src/aspire/source/__init__.py
@@ -2,6 +2,7 @@ import logging
 
 from aspire.source.image import ArrayImageSource, ImageSource
 from aspire.source.relion import RelionSource
+from aspire.source.eman import EmanSource
 from aspire.source.simulation import Simulation
 
 logger = logging.getLogger(__name__)
