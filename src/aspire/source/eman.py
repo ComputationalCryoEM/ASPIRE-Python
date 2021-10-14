8118107c15250f1e3156acc29a48b374e92e0058
Chris Langfield <christopher.langfield@gmail.com>
2021-10-14 19:55:45 -0400

    cleanup

--- src/aspire/source/eman.py
+++ src/aspire/source/eman.py
@@ -8,6 +8,7 @@ from aspire.image import Image
 import mrcfile
 import numpy as np
 from pandas import DataFrame
+from collections import OrderedDict
 
 logger = logging.getLogger(__name__)
 
@@ -28,7 +29,7 @@ class EmanSource(ImageSource):
         
         # dictionary indexed by mrc file paths, leading to a list of coordinates
         # coordinates represented by a tuple of integers
-        self.mrc2coords = {}
+        self.mrc2coords = OrderedDict()
         
         # load in the STAR file as a data frame. this STAR file has one block
         df = StarFile(filepath).get_block_by_index(0)
@@ -46,25 +47,29 @@ class EmanSource(ImageSource):
             # open box file and read in the coordinates (one particle per line)
             with open(box_paths[i], 'r') as boxfile:
                 for line in boxfile.readlines():
-                    coordList.append((int(x) for x in line.split()))
+                    coordList.append([int(x) for x in line.split()])
             self.mrc2coords[mrc_paths[i]] = coordList     
 
         self.num_particles = sum([len(self.mrc2coords[x]) for x in self.mrc2coords])
         logger.info(f"EmanSource from {filepath} contains {len(self.mrc2coords)} micrographs, {self.num_particles} picked particles.")        
         
         # open first mrc file to populate metadata
-        first_mrc_filepath = mrc_paths[0]
-        mrc = mrcfile.open(first_mrc_filepath)
-        mode = int(mrc.header.mode)
-        dtypes = {0: "int8", 1: "int16", 2: "float32", 6: "uint16"}
-        dtype = dtypes[mode]
-        shape = mrc.data.shape
+        with mrcfile.open(mrc_paths[0]) as mrc:
+            mode = int(mrc.header.mode)
+            dtypes = {0: "int8", 1: "int16", 2: "float32", 6: "uint16"}
+            self.dtype = dtypes[mode]
+            shape = mrc.data.shape
         assert(len(shape)==2)
-        assert(shape[0] == shape[1])
-        L = shape[1]
-        logger.info(f"Image size = {L}x{L}")
-        self._original_resolution = L
+        self.X = shape[0]
+        self.Y = shape[1]
+        logger.info(f"Image size = {self.X}x{self.Y}")
         
+        # open first box file to get particle size
+        first_box_filepath = box_paths[0]
+        with open(first_box_filepath, 'r') as boxfile:
+            first_line = boxfile.readlines()[0]
+            self.particle_size = int(first_line.split()[2])
+        logger.info(f"Particle size = {self.particle_size}x{self.particle_size}")        
 
        
     def _images(self, start=0, num=np.inf, indices=None):
@@ -78,14 +83,24 @@ class EmanSource(ImageSource):
         
         # explode mrc2coords into a flat list
         all_particles = []
+        # identify each particle as e.g. 000001@mrcfile, analogously to Relion
         for mrc in self.mrc2coords:
             for i in range(len(self.mrc2coords[mrc])):
-                all_particles.append(f"{i:05d}@{mrc}")
+                all_particles.append(f"{i:06d}@{mrc}")
+        # select the desired particles from this list
         _particles = [all_particles[i] for i in indices]
-        im = np.empty((len(indices),self._original_resolution, self._original_resolution), dtype=self.dtype)
-        
+        # initialize empty array to hold particle stack
+        im = np.empty((len(indices),self.particle_size, self.particle_size), dtype=self.dtype)
+
+        def crop_micrograph(data, coord):
+            start_x, start_y, size_x, size_y = *coord
+            # according to MRC 2014 convention, origin represents
+            # bottom-left corner of image
+            return data[start_y:start_y+size_y,start_x:start_x+size_x]
+  
         for i in range(len(_particles)):
-            num, fp = int(particle.split("@")[0]), particle.split("@")[1]
+            # get the particle number and the migrocraph
+            num, fp = int(_particles[i].split("@")[0]), _particles[i].split("@")[1]
             # load the image data for this micrograph
             arr = mrcfile.open(fp).data
             # get the specified particle coordinates
@@ -94,9 +109,4 @@ class EmanSource(ImageSource):
 
         return Image(im)
 
-    def crop_micrograph(data, coord):         
-        bottom_left_corner_x = coord[0]
-        bottom_left_corner_y = coord[1]
-        size_x = coord[2]
-        size_y = coord[3]
-        
+
