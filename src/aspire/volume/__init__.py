fixed gaussian blob coords rotation. refactored rotated_grids_3d to remove m_reshape.

src/aspire/volume/__init__.py
@@ -451,7 +451,9 @@ def gaussian_blob_Cn_vols(
         for k in range(K):
             for j in range(order):
                 coords_k = rot[:, :, j] @ coords - mu[:, k, np.newaxis]
-                coords_k = Q[:, :, k] / np.sqrt(np.diag(D[:, :, k])) @ coords_k
+                coords_k = (
+                   np.eye(3) / np.sqrt(np.diag(D[:, :, k])) @ Q[:, :, k].T @ coords_k
+                )
 
                 vol += np.exp(-0.5 * np.sum(np.abs(coords_k) ** 2, axis=0))
 
@@ -532,10 +534,9 @@ def rotated_grids_3d(L, rot_matrices):
             grid3d["z"].flatten(),
         ]
     )
-    pts_rot = np.zeros((3, num_pts, num_rots), dtype=rot_matrices.dtype)
+    pts_rot = np.zeros((3, num_rots, num_pts), dtype=rot_matrices.dtype)
     for i in range(num_rots):
-        pts_rot[:, :, i] = rot_matrices[i, :, :] @ pts
+        pts_rot[:, i, :] = rot_matrices[i, :, :] @ pts
 
-    pts_rot = m_reshape(pts_rot, (3, L ** 3 * num_rots))
-    # Note we return grids as (X,Y,Z)
-    return pts_rot
+    # Note we return grids as (Z,Y,X)
+    return pts_rot.reshape(3,-1)

tests/test_volume.py
@@ -13,7 +13,7 @@ from aspire.source.simulation import Simulation
 from aspire.utils import Rotation, powerset
 from aspire.utils.coor_trans import grid_3d
 from aspire.utils.types import utest_tolerance
-from aspire.volume import Volume, parseSymmetry
+from aspire.volume import Volume, parse_symmetry
 
 DATA_DIR = os.path.join(os.path.dirname(__file__), "saved_test_data")
 
@@ -227,11 +227,11 @@ class VolumeTestCase(TestCase):
             # ref_vol[0] is a volume rotated by zero degrees.
             sim = Simulation(L=L, C=1, symmetry_type=s, dtype=np.float64)
             vol = sim.vols
-            ref_vol = vol.rotate(0, rot_mat, nyquist=False)
+            ref_vol = vol.rotate(0, rot_mat, zero_nyquist=False)
 
             # We rotate ref_vol[0] by the stack of rotation matrices
             # rot_vol is a stack of rotated ref_vol[0]
-            rot_vol = ref_vol.rotate(0, rot_mat, nyquist=False)
+            rot_vol = ref_vol.rotate(0, rot_mat, zero_nyquist=False)
 
             # Compare rotated volumes to reference volume within the shpere of radius L/4.
             # Check that rotated volumes are within 0.4% of reference volume.
@@ -246,9 +246,9 @@ class VolumeTestCase(TestCase):
             _ = Simulation(symmetry_type="Ch2")
 
     def testParseSymmetryError(self):
-        # Test we raise with expected message from parseSymmetry
+        # Test we raise with expected message from parse_symmetry
         with raises(NotImplementedError, match=r"J type symmetry.*"):
-            _ = parseSymmetry("junk")
+            _ = parse_symmetry("junk")
 
     def to_vec(self):
         """Compute the to_vec method and compare."""