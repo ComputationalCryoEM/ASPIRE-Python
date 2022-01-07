6c22f250e195a7f021f69ff7cf5006b90ab4150e
Josh Carmichael <carmichael@princeton.edu>
2022-01-07 14:38:40 -0500

    Removed parameter setting from config.ini. Added parameters to __init__ with default settings.

--- src/aspire/abinitio/commonline_sync.py
+++ src/aspire/abinitio/commonline_sync.py
@@ -24,15 +24,25 @@ class CLSyncVoting(CLOrient3D):
     Journal of Structural Biology, 169, 312-322 (2010).
     """
 
-    def __init__(self, src, n_rad=None, n_theta=None):
+    def __init__(self, src, n_rad=None, n_theta=360, max_shift=0.15, shift_step=1):
         """
         Initialize an object for estimating 3D orientations using synchronization matrix
 
         :param src: The source object of 2D denoised or class-averaged images with metadata
         :param n_rad: The number of points in the radial direction
-        :param n_theta: The number of points in the theta direction
+        :param n_theta: The number of points in the theta direction.
+            Default is 360.
+        :param max_shift: Determines maximum range for shifts as a proportion
+            of the resolution. Default is 0.15.
+        :param shift_step: Resolution for shift estimation in pixels. Default is 1 pixel.
         """
-        super().__init__(src, n_rad=n_rad, n_theta=n_theta)
+        super().__init__(
+            src,
+            n_rad=n_rad,
+            n_theta=n_theta,
+            max_shift=max_shift,
+            shift_step=shift_step,
+        )
         self.syncmatrix = None
 
     def estimate_rotations(self):
