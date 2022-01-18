24bf821dac1d82c1c1a9da1b26336549f4763a6d
Josh Carmichael <carmichael@princeton.edu>
2022-01-18 12:30:39 -0500

    staticmethod Rotation.about_xyz

--- src/aspire/utils/rotation.py
+++ src/aspire/utils/rotation.py
@@ -239,6 +239,21 @@ class Rotation:
         return Rotation(matrices)
 
     @staticmethod
+    def about_xyz(axis, angle, dtype=np.float32):
+        """
+        Build singleton rotation object from axis and angle of rotation.
+
+        :param axis: A string denoting the axis of rotation. "x", "y", or "z".
+        :param angle: Rotation angle in radians.
+        :param dtype: Data type for rotational angles and matrix.
+        
+        :return: Rotation object
+        """
+        rotation = sp_rot.from_euler(axis, angle.astype(dtype), degrees=False)
+        matrix = rotations.as_matrix().astype(dtype)
+        return Rotation(matrix)
+    
+    @staticmethod
     def from_matrix(values, dtype=np.float32):
         """
         build rotation object from rotational matrices
