eb36ae733f4b0213aa065265c334d8fc27574139
Josh Carmichael <carmichael@princeton.edu>
2022-03-11 11:46:44 -0500

    isort off

--- src/aspire/abinitio/__init__.py
+++ src/aspire/abinitio/__init__.py
@@ -1,4 +1,8 @@
 from .commonline_base import CLOrient3D
-from .commonline_c3_c4 import CLSymmetryC3C4
+
+# isort: off
 from .commonline_sync import CLSyncVoting
+from .commonline_c3_c4 import CLSymmetryC3C4
+
+# isort: on
 from .orientation_src import OrientEstSource
