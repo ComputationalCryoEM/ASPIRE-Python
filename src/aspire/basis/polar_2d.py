d9a9e623c8b03d014b17c6558694abfbb6cf3047
Chris Langfield <christopher.langfield@gmail.com>
2022-03-17 14:27:14 -0400

    format

--- src/aspire/basis/polar_2d.py
+++ src/aspire/basis/polar_2d.py
@@ -19,7 +19,7 @@ class PolarBasis2D(Basis):
         """
         Initialize an object for the 2D polar Fourier grid class
 
-        :param size: The shape of the vectors for which to define the grid. 
+        :param size: The shape of the vectors for which to define the grid.
             May be a 2-tuple or an integer, in which case a square basis is assumed.
             Currently only square images are supported.
         :param nrad: The number of points in the radial dimension. Default is resolution // 2.
