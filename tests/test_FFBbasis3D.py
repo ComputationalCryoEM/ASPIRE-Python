d7e0199884488631caf87788d2626f7ad650a82c
Joakim Andén <janden@kth.se>
2022-04-04 12:35:00 +0200

    Include new tests in 3D FFB tests

--- tests/test_FFBbasis3D.py
+++ tests/test_FFBbasis3D.py
@@ -5,16 +5,17 @@ import numpy as np
 
 from aspire.basis import FFBBasis3D
 
-from ._basis_util import UniversalBasisMixin
+from ._basis_util import Steerable3DMixin, UniversalBasisMixin
 
 DATA_DIR = os.path.join(os.path.dirname(__file__), "saved_test_data")
 
 
-class FFBBasis3DTestCase(TestCase, UniversalBasisMixin):
+class FFBBasis3DTestCase(TestCase, Steerable3DMixin, UniversalBasisMixin):
     def setUp(self):
         self.L = 8
         self.dtype = np.float32
         self.basis = FFBBasis3D((self.L, self.L, self.L), dtype=self.dtype)
+        self.seed = 9161341
 
     def tearDown(self):
         pass
