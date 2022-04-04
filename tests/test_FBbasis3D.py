e496a49e36f934376cb224e9c44e1519dea52fb8
Joakim Andén <janden@kth.se>
2022-04-04 12:41:32 +0200

    Parameterize 3D (F)FB tests

--- tests/test_FBbasis3D.py
+++ tests/test_FBbasis3D.py
@@ -2,6 +2,7 @@ import os.path
 from unittest import TestCase
 
 import numpy as np
+from parameterized import parameterized_class
 
 from aspire.basis import FBBasis3D
 
@@ -10,10 +11,20 @@ from ._basis_util import Steerable3DMixin, UniversalBasisMixin
 DATA_DIR = os.path.join(os.path.dirname(__file__), "saved_test_data")
 
 
+# NOTE: Class with default values is already present, so don't list it below.
+@parameterized_class(
+    ("L", "dtype"),
+    [
+        (8, np.float64),
+        (16, np.float32),
+        (16, np.float64),
+    ],
+)
 class FBBasis3DTestCase(TestCase, Steerable3DMixin, UniversalBasisMixin):
+    L = 8
+    dtype = np.float32
+
     def setUp(self):
-        self.L = 8
-        self.dtype = np.float32
         self.basis = FBBasis3D((self.L, self.L, self.L), dtype=self.dtype)
         self.seed = 9161341
 
--- tests/test_FFBbasis3D.py
+++ tests/test_FFBbasis3D.py
@@ -2,6 +2,7 @@ import os.path
 from unittest import TestCase
 
 import numpy as np
+from parameterized import parameterized_class
 
 from aspire.basis import FFBBasis3D
 
@@ -10,10 +11,22 @@ from ._basis_util import Steerable3DMixin, UniversalBasisMixin
 DATA_DIR = os.path.join(os.path.dirname(__file__), "saved_test_data")
 
 
+# NOTE: Class with default values is already present, so don't list it below.
+@parameterized_class(
+    ("L", "dtype"),
+    [
+        (8, np.float64),
+        (16, np.float32),
+        (16, np.float64),
+        (32, np.float32),
+        (32, np.float64),
+    ],
+)
 class FFBBasis3DTestCase(TestCase, Steerable3DMixin, UniversalBasisMixin):
+    L = 8
+    dtype = np.float32
+
     def setUp(self):
-        self.L = 8
-        self.dtype = np.float32
         self.basis = FFBBasis3D((self.L, self.L, self.L), dtype=self.dtype)
         self.seed = 9161341
 
