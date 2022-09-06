e8cf78c1c52a11c4ccc9104476c05eb8eb237a6b
Chris Langfield <christopher.langfield@gmail.com>
2022-09-06 15:32:05 -0400

    logical tests for 3D downsampling

--- tests/test_downsample.py
+++ tests/test_downsample.py
@@ -1,17 +1,20 @@
+from itertools import product
 from unittest import TestCase
 
 import numpy as np
 
+from aspire.image import Image
 from aspire.source import Simulation
 from aspire.utils import utest_tolerance
 from aspire.utils.matrix import anorm
 from aspire.utils.misc import gaussian_3d
+from aspire.utils.rotation import Rotation
 from aspire.volume import Volume
 
 
 class DownsampleTestCase(TestCase):
     def setUp(self):
-        self.n = 128
+        self.n = 27
         self.dtype = np.float32
 
     def tearDown(self):
@@ -23,6 +26,16 @@ class DownsampleTestCase(TestCase):
         # check resolution is correct
         self.assertEqual((self.n, L_ds, L_ds), imgs_ds.shape)
         # check center points for all images
+        self.assertTrue(self.checkCenterPoint(imgs_org, imgs_ds))
+        # check signal energy is conserved
+        self.assertTrue(self.checkSignalEnergy(imgs_org, imgs_ds))
+
+    def _testDownsample3DCase(self, L, L_ds):
+        # downsampling from size L to L_ds
+        imgs_org, imgs_ds = self.createVolumes(L, L_ds)
+        # check resolution is correct
+        self.assertEqual((self.n, L_ds, L_ds, L_ds), imgs_ds.shape)
+        # check center points for all volumes
         self.assertTrue(self.checkCenterPoint(imgs_org, imgs_ds))
         # check signal energy is conserved
         self.assertTrue(self.checkSignalEnergy(imgs_org, imgs_ds))
@@ -47,25 +60,59 @@ class DownsampleTestCase(TestCase):
         # target resolution: 32
         self._testDownsample2DCase(65, 32)
 
+    def testDownsample3D_EvenEven(self):
+        # source resolution: 64
+        # target resolution: 32
+        self._testDownsample3DCase(64, 32)
+
+    def testDownsample3D_EvenOdd(self):
+        # source resolution: 64
+        # target resolution: 33
+        self._testDownsample3DCase(64, 33)
+
+    def testDownsample3D_OddOdd(self):
+        # source resolution: 65
+        # target resolution: 33
+        self._testDownsample3DCase(65, 33)
+
+    def testDownsample3D_OddEven(self):
+        # source resolution: 65
+        # target resolution: 32
+        self._testDownsample3DCase(64, 32)
+
     def checkCenterPoint(self, imgs_org, imgs_ds):
         # Check that center point is the same after ds
-        L = imgs_org.res
-        max_resolution = imgs_ds.res
+        L = imgs_org.shape[-1]
+        L_ds = imgs_ds.shape[-1]
+        # grab the center point via tuple of length 2 or 3 (image or volume)
+        center_org, center_ds = (L // 2, L // 2), (L_ds // 2, L_ds // 2)
+        if isinstance(imgs_org, Volume):
+            center_org += (L // 2,)
+            center_ds += (L_ds // 2,)
         return np.allclose(
-            imgs_org[:, L // 2, L // 2],
-            imgs_ds[:, max_resolution // 2, max_resolution // 2],
-            atol=utest_tolerance(self.dtype),
+            imgs_org[(..., *center_org)],
+            imgs_ds[(..., *center_ds)],
+            atol=1e-3, # this indeterminacy only appears with the 3D centerpoints..
         )
 
     def checkSignalEnergy(self, imgs_org, imgs_ds):
         # check conservation of energy after downsample
-        L = imgs_org.res
-        max_resolution = imgs_ds.res
-        return np.allclose(
-            anorm(imgs_org.asnumpy(), axes=(1, 2)) / L,
-            anorm(imgs_ds.asnumpy(), axes=(1, 2)) / max_resolution,
-            atol=utest_tolerance(self.dtype),
-        )
+        if isinstance(imgs_org, Image):
+            L = imgs_org.res
+            max_resolution = imgs_ds.res
+            return np.allclose(
+                anorm(imgs_org.asnumpy(), axes=(1, 2)) / L,
+                anorm(imgs_ds.asnumpy(), axes=(1, 2)) / max_resolution,
+                atol=utest_tolerance(self.dtype),
+            )
+        elif isinstance(imgs_org, Volume):
+            L = imgs_org.resolution
+            L_ds = imgs_ds.resolution
+            return np.allclose(
+                anorm(imgs_org.asnumpy(), axes=(1, 2, 3)) / (L ** (3 / 2)),
+                anorm(imgs_ds.asnumpy(), axes=(1, 2, 3)) / (L_ds ** (3 / 2)),
+                atol=utest_tolerance(self.dtype),
+            )
 
     def createImages(self, L, L_ds):
         # generate a 3D Gaussian volume
@@ -84,3 +131,11 @@ class DownsampleTestCase(TestCase):
         imgs_ds = sim.images(start=0, num=self.n)
 
         return imgs_org, imgs_ds
+
+    def createVolumes(self, L, L_ds):
+        # generate a set of volumes with various anisotropic spreads
+        sigmas = list(product([L * 0.1, L * 0.2, L * 0.3], repeat=3))
+        vol = Volume(
+            np.array([gaussian_3d(L, sigma=s, dtype=self.dtype) for s in sigmas])
+        )
+        return vol, vol.downsample(L_ds)
