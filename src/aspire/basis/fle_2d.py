4799c52d6779a87981f2e360144b7555622c5c68
Chris Langfield <christopher.langfield@gmail.com>
2022-09-09 17:30:37 -0400

    computing bessel zeros

--- src/aspire/basis/fle_2d.py
+++ src/aspire/basis/fle_2d.py
@@ -1,21 +1,23 @@
 import logging
+import os
 
 import numpy as np
+from scipy.io import loadmat
 
-from aspire.basis import Basis
+from aspire.basis import FBBasisMixin, SteerableBasis2D
 from aspire.image import Image
 
 logger = logging.getLogger(__name__)
 
 
-class FLEBasis2D(Basis):
+class FLEBasis2D(SteerableBasis2D, FBBasisMixin):
     """
     FLE Basis.
 
     https://arxiv.org/pdf/2207.13674.pdf
     """
 
-    def __init__(self, size, bandlimit=None, epsilon=None, dtype=np.float32):
+    def __init__(self, size, bandlimit=None, epsilon=1e-10, dtype=np.float32):
         """
         :param size: The size of the vectors for which to define the FLE basis.
                  Currently only square images are supported.
@@ -27,30 +29,37 @@ class FLEBasis2D(Basis):
         self.bandlimit = bandlimit
         self.epsilon = epsilon
         self.dtype = dtype
-
         # Basis.__init__()
-        super().__init__(self, size, ell_max=None, dtype=self.dtype)
+        super().__init__(size, ell_max=None, dtype=self.dtype)
 
     def _build(self):
+        # get upper bound of zeros, ells, and ks of Bessel functions
+        # self._calc_k_max()
+        # self.count = self.k_max[0] + sum(2*self.k_max[1:])
+
         # Heuristic for max iterations
         maxitr = 1 + int(3 * np.log2(self.nres))
         numsparse = 32
-        if eps >= 1e-10:
+        if self.epsilon >= 1e-10:
             numsparse = 22
-            maxitr = 1 + int(2 * np.log2(L))
-        if eps >= 1e-7:
+            maxitr = 1 + int(2 * np.log2(self.nres))
+        if self.epsilon >= 1e-7:
             numsparse = 16
-            maxitr = 1 + int(np.log2(L))
-        if eps >= 1e-4:
+            maxitr = 1 + int(np.log2(self.nres))
+        if self.epsilon >= 1e-4:
             numsparse = 8
-            maxitr = 1 + int(np.log2(L)) // 2
+            maxitr = 1 + int(np.log2(self.nres)) // 2
         self.maxitr = maxitr
         self.numsparse = numsparse
+
+        self._precomp()
 
     def _precomp(self):
 
         # Regular Fourier-Bessel bandlimit (equivalent to pi*R**2)
-        self.num_basis_functions = int(L**2 * np.pi / 4)
+        self.num_basis_functions = int(self.nres**2 * np.pi / 4)
+
+        self._lap_eig_disk()
 
         # Number of radial nodes
         # (Lemma 4.1)
@@ -58,33 +67,99 @@ class FLEBasis2D(Basis):
         Q = int(np.ceil(2.4 * self.nres))
         tmp = 1 / (np.sqrt(np.pi))
         for q in range(1, Q + 1):
-            tmp = tmp / q * (np.sqrt(np.pi) * L / 4)
+            tmp = tmp / q * (np.sqrt(np.pi) * self.nres / 4)
             if tmp <= self.epsilon:
                 num_radial_nodes = int(max(q, np.log2(1 / self.epsilon)))
                 break
-        num_radial_nodes = max(num_radial_nodes, int(np.ceil(np.log2(1 / eps))))
+        num_radial_nodes = max(
+            num_radial_nodes, int(np.ceil(np.log2(1 / self.epsilon)))
+        )
 
         # Number of angular nodes
         # (Lemma 4.2)
         # compute max {7.08 * self.nres, Log2(1/epsilon) + Log2(self.nres**2) }
-        S = int(max(7.08*self.nres, -np.log2(epsilon) + 2*np.log2(self.nres)))
-        num_angular_nodes = S
-        for s in range(int(lmd1 + ndmax) + 1, S + 1):
-            tmp = L**2 * ((lmd1 + ndmax) / s)**s
-            if tmp <= self.epsilon:
-                num_angular_nodes = int(max(int(s), np.log2(1/self.epsilon)))
-                break
 
-        # must be even
-        if num_angular_nodes % 2 == 1:
-            num_angular_nodes += 1
+    #        S = int(max(7.08*self.nres, -np.log2(epsilon) + 2*np.log2(self.nres)))
+    #        num_angular_nodes = S
+    #        for s in range(int(lmd1 + ndmax) + 1, S + 1):
+    #            tmp = self.nres**2 * ((lmd1 + ndmax) / s)**s
+    #            if tmp <= self.epsilon:
+    #                num_angular_nodes = int(max(int(s), np.log2(1/self.epsilon)))
+    #                break
+
+    # must be even
+    #        if num_angular_nodes % 2 == 1:
+    #            num_angular_nodes += 1
 
     def _lap_eig_disk(self):
         """
         Compute the eigenvalues of the Laplacian operator on a disk with Dirichlet boundary conditions.
         """
-        
-        
+        # number of Bessel function orders being considered
+        nc = int(3 * np.sqrt(self.num_basis_functions))
+        # max number of zeros per Bessel function
+        nd = int(2 * np.sqrt(self.num_basis_functions))
+
+        # preallocate containers for roots
+        # 0 frequency plus pos and negative frequencies for each bessel function
+        nn = 1 + 2 * nc
+        self.ns = np.zeros((nn, nd), dtype=int, order="F")
+        self.ks = np.zeros((nn, nd), dtype=int, order="F")
+        self.lmds = np.ones((nn, nd), dtype=np.float64) * np.Inf
+
+        path_to_module = os.path.dirname(__file__)
+        zeros_path = os.path.join(path_to_module, "jn_zeros_n=3000_nt=2500.mat")
+        data = loadmat(zeros_path)
+        roots_table = data["roots_table"]
+
+        # table of values of n, the Bessel function order
+        self.ns[0, :] = 0
+        # lmds[0, m] is the m'th zero of J_0
+        self.lmds[0, :] = roots_table[0, :nd]
+        # table of values of k, the zeros of the given Bessel function
+        self.ks[0, :] = np.arange(nd) + 1
+
+        # add roots of J_n for n>0 twice with +k and -k (frequencies)
+        # iterate over Bessel function order
+        for n in range(1, nc + 1):
+            self.ns[2 * n - 1, :] = -n
+            self.ks[2 * n - 1, :] = np.arange(nd) + 1
+
+            self.lmds[2 * n - 1, :nd] = roots_table[n, :nd]
+
+            self.ns[2 * n, :] = n
+            self.ks[2 * n, :] = self.ks[2 * n - 1, :]
+            self.lmds[2 * n, :] = self.lmds[2 * n - 1, :]
+
+        ### lmds
+
+        # [ R_0_1, R_0_2, R_0_3 ... R_0_nd ]
+        # [ R_1_1, R_1_2, R_1_3 ... R_1_nd ]
+        # [ R_1_1, R_1_2, R_1_3 ... R_1_nd ]
+        # [ R_2_1, R_2_2, R_2_3 ... R_2_nd ]
+        # [ R_2_1, R_2_2, R_2_3 ... R_2_nd ]
+        # ... ...
+        # [ R_nc_1,R_nc_2,R_nn_3 ... R_nn_nd ]
+        # [ R_nc_1,R_nc_2,R_nn_3 ... R_nn_nd ]
+
+        ### nds
+
+        # [ 0, 0, 0, ... 0 ] (nn)
+        # [-1,-1,-1, ...-1 ]
+        # [ 1, 1, 1, ... 1 ]
+        # [-2,-2,-2, ...-2 ]
+        # [ 2, 2, 2, ... 2 ]
+        # ... ...
+        # [-nc,-nc,-nc...-nc ]
+        # [ nc, nc, nc... nc ]
+
+        ### kds
+
+        # [1, 2, 3, ... nd ]
+        # [1, 2, 3, ... nd ]
+        # ...
+        # [1, 2, 3, ... nd ]
+
         # Maximum bandlimit
         # (Section 4.1)
         # Can remove frequencies above this threshold based on the fact that
@@ -92,5 +167,3 @@ class FLEBasis2D(Basis):
         # unit disk inscribed on the image
         if self.bandlimit:
             pass
-        
-        
