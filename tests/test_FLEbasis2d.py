bc0bb32d6f1f5169d17aef1632e9980ac7fa0c27
Chris Langfield <christopher.langfield@gmail.com>
2022-09-12 11:07:34 -0400

    add test file

---
+++ tests/test_FLEbasis2d.py
@@ -0,0 +1,16 @@
+import os.path
+from unittest import TestCase
+
+import numpy as np
+
+from aspire.basis import FLEBasis2D
+
+from ._basis_util import UniversalBasisMixin
+
+
+class FLEBasis2DTestCase(TestCase, UniversalBasisMixin):
+    L = 8
+    dtype = np.float32
+
+    def setUp(self):
+        self.basis = FLEBasis2D((self.L, self.L), dtype=self.dtype)
