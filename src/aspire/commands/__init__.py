c8bdc8fdfdaa60e9b8b97117761725abfe31e1bb
Chris Langfield <christopher.langfield@gmail.com>
2022-12-06 12:18:05 -0500

    get dict of keys for loglevel CLI opt

--- src/aspire/commands/__init__.py
+++ src/aspire/commands/__init__.py
@@ -1,14 +1,13 @@
 import click
 
 from aspire import config
+from aspire.utils.logging import pythonLoggingLevelDict
 
 # universal option for aspire commands
-_log_levels = ["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]
+_log_levels = pythonLoggingLevelDict().keys()
 log_level_option = click.option(
     "--loglevel",
-    type=click.Choice(
-        _log_levels, case_sensitive=False
-    ),
+    type=click.Choice(_log_levels, case_sensitive=False),
     default=config["logging"]["console_level"].as_choice(_log_levels),
     help="Logging verbosity level of console output.",
 )
