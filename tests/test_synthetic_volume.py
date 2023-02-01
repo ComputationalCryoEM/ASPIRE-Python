66c77f788c45f7b552d72ba8c9f7209cc914e96c
Josh Carmichael <carmichael@princeton.edu>
2023-02-01 15:51:14 -0500

    remove parameterized_class. Use pytest.

--- tests/test_synthetic_volume.py
+++ tests/test_synthetic_volume.py
@@ -1,7 +1,7 @@
-from unittest import TestCase
+import itertools
 
 import numpy as np
-from parameterized import parameterized_class
+import pytest
 
 from aspire.source.simulation import Simulation
 from aspire.utils import Rotation, grid_3d
@@ -14,127 +14,120 @@ from aspire.volume import (
     TSymmetricVolume,
 )
 
+# dtype fixture to pass into volume fixture.
+DTYPES = [np.float32, pytest.param(np.float64, marks=pytest.mark.expensive)]
 
-class Base:
-    def setUp(self):
-        self.dtype = np.float32
-        self.C = 1
-        self.seed = 0
-        vol_kwargs = dict(
-            L=self.L,
-            C=self.C,
-            seed=self.seed,
-            dtype=self.dtype,
+
+@pytest.fixture(params=DTYPES)
+def dtype_fixture(request):
+    dtype = request.param
+    return dtype
+
+
+# Parameter combinations for testing SyntheticVolumes with cyclic and dihedral symmetry.
+# Each tuple represents (volume class, resolution in pixels, cyclic order)
+PARAMS_Cn_Dn = [
+    (CnSymmetricVolume, 20, 2),
+    (CnSymmetricVolume, 21, 2),
+    (CnSymmetricVolume, 30, 3),
+    (CnSymmetricVolume, 31, 3),
+    (CnSymmetricVolume, 40, 4),
+    (CnSymmetricVolume, 41, 4),
+    (CnSymmetricVolume, 52, 5),
+    (CnSymmetricVolume, 53, 5),
+    (CnSymmetricVolume, 64, 6),
+    (CnSymmetricVolume, 65, 6),
+    (DnSymmetricVolume, 20, 2),
+    (DnSymmetricVolume, 21, 2),
+    (DnSymmetricVolume, 40, 3),
+    (DnSymmetricVolume, 41, 3),
+    (DnSymmetricVolume, 42, 4),
+    (DnSymmetricVolume, 43, 4),
+    (DnSymmetricVolume, 55, 5),
+    (DnSymmetricVolume, 56, 5),
+    (DnSymmetricVolume, 64, 6),
+    (DnSymmetricVolume, 65, 6),
+]
+
+
+# Parameters for tetrahedral, octahedral, asymmetric, and legacy volumes.
+# These volumes do not have an `order` parameter.
+VOL_CLASSES = [TSymmetricVolume, OSymmetricVolume, AsymmetricVolume, LegacyVolume]
+RESOLUTIONS = [20, 21]
+PARAMS = list(itertools.product(VOL_CLASSES, RESOLUTIONS))
+
+
+def vol_fixture_id(params):
+    vol_class = params[0]
+    res = params[1]
+    if len(params) > 2:
+        order = params[2]
+        return f"{vol_class.__name__}, res={res}, order={order}"
+    else:
+        return f"{vol_class.__name__}, res={res}"
+
+
+# Create SyntheticVolume fixture for the set of parameters.
+@pytest.fixture(params=PARAMS_Cn_Dn + PARAMS, ids=vol_fixture_id)
+def vol_fixture(request, dtype_fixture):
+    params = request.param
+    vol_class = params[0]
+    res = params[1]
+    vol_kwargs = dict(
+        L=res,
+        C=1,
+        seed=0,
+        dtype=dtype_fixture,
+    )
+    if len(params) > 2:
+        vol_kwargs["order"] = params[2]
+
+    return vol_class(**vol_kwargs)
+
+
+# SyntheticVolume tests:
+def testVolumeRepr(vol_fixture):
+    """Test Synthetic Volume repr"""
+    assert repr(vol_fixture).startswith(f"{vol_fixture.__class__.__name__}")
+
+
+def testVolumeGenerate(vol_fixture):
+    """Test that a volume is generated"""
+    _ = vol_fixture.generate()
+
+
+def testSimulationInit(vol_fixture):
+    """Test that a Simulation initializes provided a synthetic Volume."""
+    _ = Simulation(L=vol_fixture.L, vols=vol_fixture.generate())
+
+
+def testCompactSupport(vol_fixture):
+    """Test that volumes have compact support."""
+    if not isinstance(vol_fixture, LegacyVolume):
+        # Mask to check support
+        g_3d = grid_3d(vol_fixture.L, dtype=vol_fixture.dtype)
+        inside = g_3d["r"] < (vol_fixture.L - 1) / vol_fixture.L
+        outside = g_3d["r"] > 1
+        vol = vol_fixture.generate()
+
+        # Check that volume is zero outside of support and positive inside.
+        assert vol[0][outside].all() == 0
+        assert (vol[0][inside] > 0).all()
+
+
+def testVolumeSymmetry(vol_fixture):
+    """Test that volumes have intended symmetry."""
+    vol = vol_fixture.generate()
+
+    # Rotations in symmetry group, excluding the Identity.
+    rots = vol_fixture.symmetry_group.matrices[1:]
+
+    for rot in rots:
+        # Rotate volume by an element of the symmetric group.
+        rot_vol = vol.rotate(Rotation(rot), zero_nyquist=False)
+
+        # Check that correlation is close to 1.
+        corr = np.dot(rot_vol[0].flatten(), vol[0].flatten()) / np.dot(
+            vol[0].flatten(), vol[0].flatten()
         )
-        if hasattr(self, "order"):
-            vol_kwargs["order"] = self.order
-
-        self.vol_obj = self.vol_class(**vol_kwargs)
-        self.vol = self.vol_obj.generate()
-
-    def testVolumeRepr(self):
-        """Test Synthetic Volume repr"""
-        self.assertTrue(
-            repr(self.vol_obj).startswith(f"{self.vol_obj.__class__.__name__}")
-        )
-
-    def testVolumeGenerate(self):
-        """Test that a volume is generated"""
-        _ = self.vol
-
-    def testSimulationInit(self):
-        """Test that a Simulation initializes provided a synthetic Volume."""
-        _ = Simulation(L=self.L, vols=self.vol)
-
-    def testCompactSupport(self):
-        """Test that volumes have compact support."""
-        if self.vol_class != LegacyVolume:
-            # Mask to check support
-            g_3d = grid_3d(self.L, dtype=self.dtype)
-            inside = g_3d["r"] < (self.L - 1) / self.L
-            outside = g_3d["r"] > 1
-
-            # Check that volume is zero outside of support and positive inside.
-            self.assertTrue(self.vol[0][outside].all() == 0)
-            self.assertTrue((self.vol[0][inside] > 0).all())
-
-    def testVolumeSymmetry(self):
-        """Test that volumes have intended symmetry."""
-        vol = self.vol
-
-        # Rotations in symmetry group, excluding the Identity.
-        rots = self.vol_obj.symmetry_group.matrices[1:]
-
-        for rot in rots:
-            # Rotate volume by an element of the symmetric group.
-            rot_vol = vol.rotate(Rotation(rot), zero_nyquist=False)
-
-            # Check that correlation is close to 1.
-            corr = np.dot(rot_vol[0].flatten(), vol[0].flatten()) / np.dot(
-                vol[0].flatten(), vol[0].flatten()
-            )
-            self.assertTrue(abs(corr - 1) < 1e-5)
-
-
-@parameterized_class(
-    ("L", "order"),
-    [
-        (21, 2),
-        (30, 3),
-        (31, 3),
-        (40, 4),
-        (41, 4),
-        (52, 5),
-        (53, 5),
-        (64, 6),
-        (65, 6),
-    ],
-)
-class CnSymmetricVolumeCase(Base, TestCase):
-    vol_class = CnSymmetricVolume
-    L = 20
-    order = 2
-
-
-@parameterized_class(
-    ("L", "order"),
-    [
-        (21, 2),
-        (40, 3),
-        (41, 3),
-        (42, 4),
-        (43, 4),
-        (55, 5),
-        (56, 5),
-        (64, 6),
-        (65, 6),
-    ],
-)
-class DnSymmetricVolumeCase(Base, TestCase):
-    vol_class = DnSymmetricVolume
-    L = 20
-    order = 2
-
-
-@parameterized_class(("L"), [(21,)])
-class TSymmetricVolumeCase(Base, TestCase):
-    vol_class = TSymmetricVolume
-    L = 20
-
-
-@parameterized_class(("L"), [(21,)])
-class OSymmetricVolumeCase(Base, TestCase):
-    vol_class = OSymmetricVolume
-    L = 20
-
-
-@parameterized_class(("L"), [(21,)])
-class AsymmetricVolumeCase(Base, TestCase):
-    vol_class = AsymmetricVolume
-    L = 20
-
-
-@parameterized_class(("L"), [(21,)])
-class LegacyVolumeCase(Base, TestCase):
-    vol_class = LegacyVolume
-    L = 20
+        assert abs(corr - 1) < 1e-5
