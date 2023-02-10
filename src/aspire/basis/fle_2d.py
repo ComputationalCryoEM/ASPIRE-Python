7da71853ac15794ca252fcc3ef73f10d498b34d7
Chris Langfield <christopher.langfield@gmail.com>
2023-02-10 09:59:29 -0500

    2nd docstring fix

--- src/aspire/basis/fle_2d.py
+++ src/aspire/basis/fle_2d.py
@@ -37,9 +37,8 @@ class FLEBasis2D(SteerableBasis2D, FBBasisMixin):
         """
         :param size: The size of the vectors for which to define the FLE basis.
             Currently only square images are supported.
-        :param bandlimit: Maximum frequency band for computing basis functions. Note that the
-            `ell_max` of other Basis objects is computed *from* the bandlimit for the FLE basis.
-             Defaults to the resolution of the basis.
+        :param bandlimit: Maximum frequency band for computing basis functions. Defaults to the 
+            resolution of the basis.
         :param epsilon: Relative precision between FLE fast method and dense matrix multiplication.
         :param dtype: Datatype of images and coefficients represented.
         :param match_fb:
