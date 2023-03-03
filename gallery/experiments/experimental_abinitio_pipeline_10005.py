bc8ed560509452346bafad0717326e3f0eea77a3
Garrett Wright <garrettwrong@gmail.com>
2023-03-03 12:08:55 -0500

    initial draft 10005 experiment

--- gallery/experiments/experimental_abinitio_pipeline_10005.py
+++ gallery/experiments/experimental_abinitio_pipeline_10005.py
@@ -1,18 +1,18 @@
 """
-Abinitio Pipeline - Experimental Data
-=====================================
+Abinitio Pipeline - Experimental Data Empiar 10005
+==================================================
 
 This notebook introduces a selection of
-components corresponding to loading real Relion picked
-particle Cryo-EM data and running key ASPIRE-Python
+components corresponding to loading real coordinate
+base particle Cryo-EM data and running key ASPIRE-Python
 Abinitio model components as a pipeline.
 
 Specifically this pipeline uses the
-EMPIAR 10028 picked particles data, available here:
+EMPIAR 10005 picked particles data, available here:
 
-https://www.ebi.ac.uk/empiar/EMPIAR-10028
+https://www.ebi.ac.uk/empiar/EMPIAR-10005
 
-https://www.ebi.ac.uk/emdb/EMD-10028
+https://www.ebi.ac.uk/emdb/EMD-5778
 """
 
 # %%
@@ -22,17 +22,16 @@ https://www.ebi.ac.uk/emdb/EMD-10028
 # In addition, import some classes from
 # the ASPIRE package that will be used throughout this experiment.
 
+import os
+import glob
 import logging
-
-import matplotlib.pyplot as plt
-import numpy as np
 
 from aspire.abinitio import CLSyncVoting
 from aspire.basis import FFBBasis3D
-from aspire.denoising import ClassAvgSourcev11, DenoiserCov2D
+from aspire.denoising import ClassAvgSourcev11
 from aspire.noise import AnisotropicNoiseEstimator
 from aspire.reconstruction import MeanEstimator
-from aspire.source import ArrayImageSource, RelionSource
+from aspire.source import ArrayImageSource, CentersCoordinateSource
 
 logger = logging.getLogger(__name__)
 
@@ -42,38 +41,35 @@ logger = logging.getLogger(__name__)
 # ---------------
 # Example simulation configuration.
 
-interactive = False  # Draw blocking interactive plots?
-do_cov2d = True  # Use CWF coefficients
+working_dir = "/scratch/ExperimentalData/staging/10017/data"
 n_imgs = None  # Set to None for all images in starfile, can set smaller for tests.
 img_size = 32  # Downsample the images/reconstruction to a desired resolution
 n_classes = 2000  # How many class averages to compute.
 n_nbor = 100  # How many neighbors to stack
-starfile_in = "10028/data/shiny_2sets_fixed9.star"
-data_folder = "."  # This depends on the specific starfile entries.
-volume_filename_prefix_out = f"10028_recon_c{n_classes}_m{n_nbor}_{img_size}.mrc"
-pixel_size = 1.34
+volume_filename_prefix_out = f"10005_abinitio_c{n_classes}_m{n_nbor}_{img_size}.mrc"
+particle_size = 300
 
 
 # %%
 # Source data and Preprocessing
 # -----------------------------
 #
-# `RelionSource` is used to access the experimental data via a `starfile`.
+# `CentersCoordinateSource` is used to access the experimental data from
+# mrcs using coordinate files.
+#
 # Begin by downsampling to our chosen resolution, then preprocess
 # to correct for CTF and noise.
 
+mrcs = sorted(glob.glob(os.path.join(working_dir, "*.mrc")))
+coords = sorted(glob.glob(os.path.join(working_dir, "*.coord")))
+files = list(zip(mrcs, coords))
+
 # Create a source object for the experimental images
-src = RelionSource(
-    starfile_in, pixel_size=pixel_size, max_rows=n_imgs, data_folder=data_folder
-)
+src = CentersCoordinateSource(files, particle_size=particle_size, max_rows=n_imgs)
 
 # Downsample the images
 logger.info(f"Set the resolution to {img_size} X {img_size}")
 src.downsample(img_size)
-
-# Peek
-if interactive:
-    src.images[:10].show()
 
 # Use phase_flip to attempt correcting for CTF.
 logger.info("Perform phase flip to input images.")
@@ -81,46 +77,8 @@ src.phase_flip()
 
 # Estimate the noise and `Whiten` based on the estimated noise
 aiso_noise_estimator = AnisotropicNoiseEstimator(src)
+src.normalize_background()
 src.whiten(aiso_noise_estimator.filter)
-
-# Plot the noise profile for inspection
-if interactive:
-    plt.imshow(aiso_noise_estimator.filter.evaluate_grid(img_size))
-    plt.show()
-
-# Peek, what do the whitened images look like...
-if interactive:
-    src.images[:10].show()
-
-# # Optionally invert image contrast, depends on data convention.
-# # This is not needed for 10028, but included anyway.
-# logger.info("Invert the global density contrast")
-# src.invert_contrast()
-
-# %%
-# Optional: CWF Denoising
-# -----------------------
-#
-# Optionally generate an alternative source that is denoised with `cov2d`,
-# then configure a customized averager. This allows the use of CWF denoised
-# images for classification, but stacks the original images for averages
-# used in the remainder of the reconstruction pipeline.
-#
-# In this example, this behavior is controlled by the `do_cov2d` boolean variable.
-# When disabled, the original src and default averager is used.
-# If you will not be using cov2d,
-# you may remove this code block and associated variables.
-
-classification_src = src
-custom_averager = None
-if do_cov2d:
-    # Use CWF denoising
-    cwf_denoiser = DenoiserCov2D(src)
-    # Use denoised src for classification
-    classification_src = cwf_denoiser.denoise()
-    # Peek, what do the denoised images look like...
-    if interactive:
-        classification_src.images[:10].show()
 
 # %%
 # Class Averaging
@@ -131,20 +89,11 @@ if do_cov2d:
 logger.info("Begin Class Averaging")
 
 # Now perform classification and averaging for each class.
-# This also demonstrates the potential to use a different source for classification and averaging.
-
-avgs_src = ClassAvgSourcev11(
-    classification_src,
-    n_nbor=n_nbor,
-    averager_src=src,
-    num_procs=None,  # Automaticaly configure parallel processing
-)
+# Automaticaly configure parallel processing
+avgs = ClassAvgSourcev11(src, n_nbor=n_nbor, num_procs=None)
 
 # We'll manually cache `n_classes` worth to speed things up.
-avgs = ArrayImageSource(avgs_src.images[:n_classes])
-
-if interactive:
-    avgs.images[:10].show()
+avgs = ArrayImageSource(avgs.images[:n_classes])
 
 
 # %%
@@ -181,8 +130,3 @@ estimator = MeanEstimator(avgs, basis)
 # Perform the estimation and save the volume.
 estimated_volume = estimator.estimate()
 estimated_volume.save(volume_filename_prefix_out, overwrite=True)
-
-# Peek at result
-if interactive:
-    plt.imshow(np.sum(estimated_volume[0], axis=-1))
-    plt.show()
