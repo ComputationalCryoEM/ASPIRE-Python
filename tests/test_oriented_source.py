7b6e3fd53f04ae55835dfc79701333999c695cff
Josh Carmichael <carmichael@princeton.edu>
2023-05-17 14:12:30 -0400

    refactor test_oriented_source. Add parameteriztion over estimators.

--- tests/test_oriented_source.py
+++ tests/test_oriented_source.py
@@ -3,108 +3,111 @@ import logging
 import numpy as np
 import pytest
 
-from aspire.abinitio import CLSymmetryC3C4, CLSyncVoting
+from aspire.abinitio import CLSymmetryC3C4, CLSymmetryCn, CLSyncVoting
 from aspire.source import OrientedSource, Simulation
 from aspire.volume import CnSymmetricVolume
 
 logger = logging.getLogger(__name__)
 
 
-@pytest.fixture
-def sim_fixture():
+ESTIMATOR_SYMMETRY = [
+    (CLSyncVoting, None),
+    (CLSymmetryC3C4, "C4"),
+    pytest.param((CLSymmetryCn, "C6"), marks=pytest.mark.expensive),
+]
+
+
+def src_fixture_id(params):
+    estimator, symmetry = params
+    return f"Orientation estimator: {estimator}, Symmetry: {symmetry}"
+
+
+# Create a source fixture that provides an original source and an oriented source.
+@pytest.fixture(params=ESTIMATOR_SYMMETRY, ids=src_fixture_id)
+def src_fixture(request):
+    estimator, symmetry = request.param
     L = 8
     n = 10
-    sim = Simulation(L=L, n=n, C=1)
-    vol_C4 = CnSymmetricVolume(L=L, order=4, C=1)
-    sim_C4 = Simulation(L=L, n=n, vols=vol_C4.generate(), offsets=0)
-    return sim, sim_C4
+    vol = None
+
+    # Symmetric Volume and additional kwargs for symmetric orientation estimation.
+    estimator_kwargs = {}
+    if symmetry:
+        order = int(symmetry[1:])
+        vol = CnSymmetricVolume(L=L, order=order, C=1).generate()
+        estimator_kwargs.update({"n_theta": 36, "symmetry": symmetry})
+
+    # Generate an origianl source and an oriented source.
+    og_src = Simulation(L=L, n=n, vols=vol, offsets=0)
+    orient_est = estimator(og_src, max_shift=1 / L, **estimator_kwargs)
+    oriented_src = OrientedSource(og_src, orient_est)
+
+    return og_src, oriented_src
 
 
-@pytest.fixture
-def oriented_src_fixture(sim_fixture):
-    # Original sources
-    sim, sim_C4 = sim_fixture
-
-    # Orientation estimators
-    estimator = CLSyncVoting(sim)
-    estimator_C4 = CLSymmetryC3C4(sim_C4, symmetry="C4", n_theta=72)
-
-    # `OrientedSource`s
-    src = OrientedSource(sim, orientation_estimator=estimator)
-    src_C4 = OrientedSource(sim_C4, orientation_estimator=estimator_C4)
-
-    return src, src_C4
-
-
-def test_repr(oriented_src_fixture, sim_fixture):
-    sim, sim_C4 = sim_fixture
-    src, src_C4 = oriented_src_fixture
+def test_repr(src_fixture):
+    og_src, oriented_src = src_fixture
 
     # Check that original source is mentioned in repr
-    logger.debug(f"repr(OrientedSrc): {repr(src)}")
-    assert type(sim).__name__ in repr(src)
-
-    logger.debug(f"repr(OrientedSrc): {repr(src_C4)}")
-    assert type(sim_C4).__name__ in repr(src_C4)
+    logger.debug(f"repr(OrientedSrc): {repr(oriented_src)}")
+    assert type(og_src).__name__ in repr(oriented_src)
 
 
-def test_images(oriented_src_fixture, sim_fixture):
-    src, src_C4 = oriented_src_fixture
-    sim, sim_C4 = sim_fixture
-    assert np.allclose(src.images[:], sim.images[:])
-    assert np.allclose(src_C4.images[:], sim_C4.images[:])
+def test_images(src_fixture):
+    og_src, oriented_src = src_fixture
+    assert np.allclose(og_src.images[:], oriented_src.images[:])
 
 
-def test_rotations(oriented_src_fixture):
-    src, src_C4 = oriented_src_fixture
+def test_rotations(src_fixture):
+    _, oriented_src = src_fixture
     # Smoke test for rotations
-    _ = src.rotations
-    _ = src_C4.rotations
+    _ = oriented_src.rotations
 
 
-def test_angles(oriented_src_fixture):
-    src, src_C4 = oriented_src_fixture
+def test_angles(src_fixture):
+    _, oriented_src = src_fixture
     # Smoke test for angles
-    _ = src.angles
-    _ = src_C4.angles
+    _ = oriented_src.angles
 
 
-def test_symmetry_group(oriented_src_fixture):
-    src, src_C4 = oriented_src_fixture
-    assert str(src.symmetry_group) == "C1"
-    assert str(src_C4.symmetry_group) == "C4"
+def test_symmetry_group(src_fixture):
+    og_src, oriented_src = src_fixture
+    assert type(og_src.symmetry_group) == type(oriented_src.symmetry_group)
 
 
-def test_default_estimator(sim_fixture):
-    sim, _ = sim_fixture
-    oriented_src = OrientedSource(sim)
+def test_default_estimator(src_fixture):
+    og_src, _ = src_fixture
+
+    # Instantiate OrientedSource without providing orientation estimator.
+    oriented_src = OrientedSource(og_src)
     assert isinstance(oriented_src.orientation_estimator, CLSyncVoting)
 
 
-def test_estimator_error(sim_fixture):
-    sim, _ = sim_fixture
+def test_estimator_error(src_fixture):
+    og_src, _ = src_fixture
     junk_estimator = 123
     with pytest.raises(
         ValueError,
         match="`orientation_estimator` should be subclass of `CLOrient3D`,* ",
     ):
-        _ = OrientedSource(sim, junk_estimator)
+        _ = OrientedSource(og_src, junk_estimator)
 
 
-def test_lazy_orientation(oriented_src_fixture, caplog):
-    for oriented_source in oriented_src_fixture:
-        # Check that instantiated oriented sources don't have rotation metadata.
-        rotation_metadata = ["_rlnAngleRot", "_rlnAngleTilt", "_rlnAnglePsi"]
-        assert not oriented_source.has_metadata(rotation_metadata)
+def test_lazy_orientation(src_fixture, caplog):
+    _, oriented_src = src_fixture
 
-        # Request rotations and check that metadata is populated.
-        _ = oriented_source.rotations
-        assert oriented_source.has_metadata(rotation_metadata)
+    # Check that instantiated oriented sources don't have rotation metadata.
+    rotation_metadata = ["_rlnAngleRot", "_rlnAngleTilt", "_rlnAnglePsi"]
+    assert not oriented_src.has_metadata(rotation_metadata)
 
-        # Check that requesting rotations again logs a debug message about skippin orientation.
-        caplog.clear()
-        msg = f"{oriented_source.__class__.__name__} already oriented, skipping"
-        caplog.set_level(logging.DEBUG)
-        assert msg not in caplog.text
-        _ = oriented_source.rotations
-        assert msg in caplog.text
+    # Request rotations and check that metadata is populated.
+    _ = oriented_src.rotations
+    assert oriented_src.has_metadata(rotation_metadata)
+
+    # Check that requesting rotations again logs a debug message about skippin orientation.
+    caplog.clear()
+    msg = f"{oriented_src.__class__.__name__} already oriented, skipping"
+    caplog.set_level(logging.DEBUG)
+    assert msg not in caplog.text
+    _ = oriented_src.rotations
+    assert msg in caplog.text
