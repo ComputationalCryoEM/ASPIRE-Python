755ec87eef2e1806a9ab61a3a52c5257c8a55003
Josh Carmichael <carmichael@princeton.edu>
2023-05-22 08:53:36 -0400

    cleanup copied annotation.

--- src/aspire/abinitio/commonline_c2.py
+++ src/aspire/abinitio/commonline_c2.py
@@ -41,17 +41,11 @@ class CLSymmetryC2(CLSymmetryC3C4):
         self.max_iters = max_iters
         self.degree_res = degree_res
         self.seed = seed
+        self.order = 2
 
     def _check_symmetry(self, symmetry):
-        if symmetry is None:
-            raise NotImplementedError("Symmetry type not supplied.")
-        else:
-            symmetry = symmetry.upper()
-            if symmetry not in ["C2"]:
-                raise NotImplementedError(
-                    f"Only C2 symmetry supported. {symmetry} was supplied."
-                )
-            self.order = int(symmetry[1])
+        # `symmetry` is not configurable for `CLSymmetryC2`
+        pass
 
     def build_clmatrix(self):
         """
@@ -69,13 +63,10 @@ class CLSymmetryC2(CLSymmetryC3C4):
         # need to do a copy to prevent modifying self.pf for other functions
         pf = self.pf.copy()
 
-        # Allocate local variables for return
-        # clmatrix represents the common lines matrix.
-        # Namely, clmatrix[i,j] contains the index in image i of
-        # the common line with image j. Note the common line index
-        # starts from 0 instead of 1 as Matlab version. -1 means
-        # there is no common line such as clmatrix[i,i].
+        # clmatrix[i,j] contains the index in image i of the common line with image j.
+        # -1 means there is no common line such as clmatrix[i,i].
         clmatrix = -np.ones((2, n_img, n_img), dtype=self.dtype)
+
         # When cl_dist[i, j] is not -1, it stores the maximum value
         # of correlation between image i and j for all possible 1D shifts.
         # We will use cl_dist[i, j] = -1 (including j<=i) to
@@ -89,13 +80,12 @@ class CLSymmetryC2(CLSymmetryC3C4):
         # set maximum value of 1D shift (in pixels) to search
         # between common-lines.
         max_shift = self.max_shift
-        # Set resolution of shift estimation in pixels. Note that
-        # shift_step can be any positive real number.
+        # Set resolution of shift estimation in pixels.
         shift_step = self.shift_step
         # 1D shift between common-lines
         shifts_1d = np.zeros((2, n_img, n_img))
 
-        # Prepare the shift phases to try and generate filter for common-line detection
+        # Prepare the shift phases and generate filter for common-line detection
         r_max = pf.shape[2]
         shifts, shift_phases, h = self._generate_shift_phase_and_filter(
             r_max, max_shift, shift_step
@@ -107,7 +97,6 @@ class CLSymmetryC2(CLSymmetryC3C4):
         pf = self._apply_filter_and_norm("ijk, k -> ijk", pf, r_max, h)
 
         # Search for common lines between [i, j] pairs of images.
-        # Creating pf and building common lines are different to the Matlab version.
         # The random selection is implemented.
         for i in range(n_img - 1):
             p1 = pf[i]
@@ -125,11 +114,10 @@ class CLSymmetryC2(CLSymmetryC3C4):
                 for shift in range(len(shifts)):
                     shift_phases = all_shift_phases[shift]
                     p2_shifted_flipped = (shift_phases * p2_flipped).T
-                    # Compute correlations in the positive r direction
-                    part1 = p1_real.dot(np.real(p2_shifted_flipped))
-                    # Compute correlations in the negative r direction
-                    part2 = p1_imag.dot(np.imag(p2_shifted_flipped))
 
+                    # Compute correlations in the positive and negative r directions
+                    part1 = p1_real.dot(np.real(p2_shifted_flipped))
+                    part2 = p1_imag.dot(np.imag(p2_shifted_flipped))
                     c1 = part1 - part2
                     c2 = part1 + part2
                     C = np.concatenate((c1, c2), axis=0)
@@ -144,8 +132,8 @@ class CLSymmetryC2(CLSymmetryC3C4):
                         shifts_1d[0, i, j] = shifts[shift]
 
                     # Mask cl_dist to find second highest correlation.
-                    dist = self.min_dist_cls * 2 * self.n_theta // 360
-                    C_masked = self.square_mask(C, first_cl1, first_cl2, dist)
+                    mask_dist = self.min_dist_cls * 2 * self.n_theta // 360
+                    C_masked = self.square_mask(C, first_cl1, first_cl2, mask_dist)
                     sidx = C_masked.argmax()
                     second_cl1, second_cl2 = np.unravel_index(sidx, C.shape)
                     s_val_2 = C[second_cl1, second_cl2]
@@ -160,11 +148,14 @@ class CLSymmetryC2(CLSymmetryC3C4):
 
     @staticmethod
     def square_mask(arr, x, y, dist):
+        """
+        Mask input array around the point (x, y) with a square mask of half-length `dist`.
+        """
         left = max(0, x - dist)
         right = min(len(arr), x + dist)
         bottom = max(0, y - dist)
         top = min(len(arr[0]), y + dist)
-        mask = np.ones_like(arr).astype(arr.dtype)
+        mask = np.ones_like(arr)
         mask[left:right, bottom:top] = 0
 
         return arr * mask
@@ -172,8 +163,6 @@ class CLSymmetryC2(CLSymmetryC3C4):
     def estimate_rotations(self):
         """
         Estimate rotation matrices for molecules with C2 symmetry.
-
-        :return: Array of rotation matrices, size n_imgx3x3.
         """
         Rijs, Rijgs = self._estimate_relative_viewing_directions()
 
