515ece79a87aa6ad36537779461b7011e76ecc40
Josh Carmichael <carmichael@princeton.edu>
2023-06-02 16:09:48 -0400

    initial data_downloader example

---
+++ src/aspire/datasets/_registry.py
@@ -0,0 +1,16 @@
+##########################################################################
+# This file serves as the dataset registry for ASPIRE Datasets SubModule.
+##########################################################################
+
+
+registry = {
+    "emdb_2660.map": "49aecfd4efce09afc937d1786bbed6f18c2a353c73a4e16a643a304342d0660e",
+}
+
+registry_urls = {
+    "emdb_2660.map": "https://zenodo.org/record/7730530/files/emd_2660.map.gz",
+}
+
+method_files_map = {
+    "emdb_2660": ["emdb_2660.map"],
+}
