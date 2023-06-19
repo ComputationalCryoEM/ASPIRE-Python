46f106c5633d912da718f54fdf22f0e4ebc4ea9b
Josh Carmichael <carmichael@princeton.edu>
2023-06-19 15:58:28 -0400

    Add emdb_8012 (EMPIAR 10073) to registry.

--- src/aspire/utils/data_fetcher.py
+++ src/aspire/utils/data_fetcher.py
@@ -3,14 +3,17 @@ from aspire import config
 # dataset registry for ASPIRE example data.
 registry = {
     "emdb_2660.map": "49aecfd4efce09afc937d1786bbed6f18c2a353c73a4e16a643a304342d0660e",
+    "emdb_8012.map": "85a1c9ab958b1dd051d011515212d58decaf2537351b9c016acd3e5852e30d63",
 }
 
 registry_urls = {
     "emdb_2660.map": "https://zenodo.org/record/7730530/files/emd_2660.map.gz",
+    "emdb_8012.map": "https://ftp.ebi.ac.uk/pub/databases/emdb/structures/EMD-8012/map/emd_8012.map.gz",
 }
 
 method_files_map = {
     "emdb_2660": ["emdb_2660.map"],
+    "emdb_8012": ["emdb_8012.map"],
 }
 
 
@@ -52,3 +55,11 @@ def emdb_2660():
     bound to the anti-protozoan drug emetine.
     """
     return fetch_data("emdb_2660.map")
+
+def emdb_8012():
+    """
+    Downloads the EMDB-8012 volume map and returns the file path.
+
+    The overall structure of the yeast spliceosomal U4/U6.U5 tri-snRNP at 3.7 Angstrom.
+    """
+    return fetch_data("emdb_8012.map")
