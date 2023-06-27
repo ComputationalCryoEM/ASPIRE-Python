fb3fa47ae9d394a70153698b9389c2102039ce29
Josh Carmichael <carmichael@princeton.edu>
2023-06-27 14:07:59 -0400

    Remove deprecated import

--- src/aspire/abinitio/__init__.py
+++ src/aspire/abinitio/__init__.py
@@ -6,7 +6,6 @@ from .commonline_sync import CLSyncVoting
 from .commonline_c3_c4 import CLSymmetryC3C4
 from .commonline_cn import CLSymmetryCn
 from .commonline_c2 import CLSymmetryC2
-
 # isort: on
-from .orientation_src import OrientEstSource
+
 
