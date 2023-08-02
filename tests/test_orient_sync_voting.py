8e8dba027b274548c47db6cedae8f9714b9f3ee0
Josh Carmichael <carmichael@princeton.edu>
2023-08-02 12:08:59 -0400

    test_estimated_rotations

--- tests/test_orient_sync_voting.py
+++ tests/test_orient_sync_voting.py
@@ -3,14 +3,17 @@ import pytest
 
 from aspire.abinitio import CLOrient3D, CLSyncVoting
 from aspire.source import Simulation
-from aspire.utils import rots_to_clmatrix
+from aspire.utils import (
+    Rotation,
+    get_aligned_rotations,
+    register_rotations,
+    rots_to_clmatrix,
+)
 from aspire.volume import AsymmetricVolume
 
 # Parametrize over (n_img, L, dtype)
 PARAMS = [
     (50, 32, np.float32),
-    (51, 32, np.float64),
-    (50, 33, np.float32),
     (51, 33, np.float64),
 ]
 
@@ -46,3 +49,29 @@ def test_build_clmatrix(n_img, L, dtype):
 
     # Check that at least 99% of estimates are within 5 degrees.
     assert within_5 / angle_diffs.size > 0.99
+
+
+@pytest.mark.parametrize("n_img, L, dtype", PARAMS)
+def test_estimated_rotations(n_img, L, dtype):
+    src, orient_est = source_orientation_objs(n_img, L, dtype)
+
+    orient_est.estimate_rotations()
+
+    # Register estimates to ground truth rotations and compute the
+    # angular distance between them (in degrees).
+    Q_mat, flag = register_rotations(orient_est.rotations, src.rotations)
+    regrot = get_aligned_rotations(orient_est.rotations, Q_mat, flag)
+    ang_dist = np.zeros(n_img, dtype=dtype)
+    for i in range(n_img):
+        ang_dist[i] = (
+            Rotation.angle_dist(
+                regrot[i],
+                src.rotations[i],
+                dtype=dtype,
+            )
+            * 180
+            / np.pi
+        )
+
+    # Assert that mean angular distance is less than 1 degree.
+    assert np.mean(ang_dist) < 1
