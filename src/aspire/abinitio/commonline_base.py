d1ff65ed47ebc97317e2e13335fe914efc1d62a1
Josh Carmichael <carmichael@princeton.edu>
2023-08-10 10:48:12 -0400

    Add offsets to tests. Revert common_line_from_rots.

--- src/aspire/abinitio/commonline_base.py
+++ src/aspire/abinitio/commonline_base.py
@@ -239,7 +239,7 @@ class CLOrient3D:
         if logging.getLogger().isEnabledFor(logging.DEBUG):
             show = True
         # Negative sign comes from using -i conversion of Fourier transformation
-        est_shifts = sparse.linalg.lsqr(shift_equations, shift_b, show=show)[0]
+        est_shifts = sparse.linalg.lsqr(shift_equations, -shift_b, show=show)[0]
         est_shifts = est_shifts.reshape((2, self.n_img), order="F")
 
         return est_shifts
