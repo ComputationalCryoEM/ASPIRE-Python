7f2f11677faf60f431ccaba532e7cfc92965cc54
Josh Carmichael <carmichael@princeton.edu>
2023-09-22 15:03:38 -0400

    Mark top views and equators. Generate inplane rotations.

--- src/aspire/abinitio/commonline_d2.py
+++ src/aspire/abinitio/commonline_d2.py
@@ -3,6 +3,7 @@ import logging
 import numpy as np
 
 from aspire.abinitio import CLOrient3D
+from aspire.utils import Rotation
 
 logger = logging.getLogger(__name__)
 
@@ -91,6 +92,30 @@ class CLSymmetryD2(CLOrient3D):
         # We detect such directions by taking a strip of radius
         # eq_filter_angle about the 3 great circles perpendicular to the symmetry
         # axes of D2 (i.e to X,Y and Z axes).
+        eq_mask1, top_view_mask1 = self.mark_equators(sphere_grid1, self.eq_min_dist)
+        eq_mask2, top_view_mask2 = self.mark_equators(sphere_grid2, self.eq_min_dist)
+
+        #  Mark Top View Directions.
+        #  A Top view projection image is taken from the direction of one of the
+        #  symmetry axes. Since all symmetry axes of D2 molecules are perpendicular
+        #  this means that such an image is an equator with repect to both symmetry
+        #  axes which are perpendicular to the direction of the symmetry axis from
+        #  which the image was made, e.g. if the image was formed by projecting in
+        #  the direction of the X (symmetry) axis, then it is an equator with
+        #  respect to both Y and Z symmetry axes (it's direction is the
+        #  interesection of 2 great circles perpendicular to Y and Z axes).
+        #  Such images have severe degeneracies. A pair of Top View images (taken
+        #  from different directions or a Top View and equator image only have a
+        #  single common line. A top view and a regular non-equator image only have
+        #  two common lines.
+
+        # Remove top views from sphere grids and update equator masks.
+        sphere_grid1 = sphere_grid1[~top_view_mask1]
+        sphere_grid2 = sphere_grid2[~top_view_mask2]
+        eq_mask1 = eq_mask1[~top_view_mask1]
+        eq_mask2 = eq_mask2[~top_view_mask2]
+
+        # Generate in-plane rotations for each grid point on the sphere.
 
     @staticmethod
     def saff_kuijlaars(N):
@@ -135,35 +160,82 @@ class CLSymmetryD2(CLOrient3D):
         """
         # Project each vector onto xy, xz, yz planes and measure angular distance
         # from each plane.
-        n_rots = len(sphere_grid)
-        angular_dists = np.zeros(3, n_rots, dtype=sphere_grid.dtype)
+        eq_min_dist = np.cos(eq_filter_angle * np.pi / 180)
 
+        # Mask for z-axis equator views.
         proj_xy = sphere_grid.copy()
         proj_xy[:, 2] = 0
         proj_xy /= np.linalg.norm(proj_xy, axis=1)[:, None]
-        angular_dists[0] = np.sum(sphere_grid * proj_xy, axis=-1)
+        ang_dists_xy = np.sum(sphere_grid * proj_xy, axis=-1)
+        z_eq_mask = ang_dists_xy > eq_min_dist
 
+        # Mask for y-axis equator views.
         proj_xz = sphere_grid.copy()
         proj_xz[:, 1] = 0
         proj_xz /= np.linalg.norm(proj_xz, axis=1)[:, None]
-        angular_dists[1] = np.sum(sphere_grid * proj_xz, axis=-1)
+        ang_dists_xz = np.sum(sphere_grid * proj_xz, axis=-1)
+        y_eq_mask = ang_dists_xz > eq_min_dist
 
+        # Mask for x-axis equator views.
         proj_yz = sphere_grid.copy()
         proj_yz[:, 0] = 0
         proj_yz /= np.linalg.norm(proj_yz, axis=1)[:, None]
-        angular_dists[2] = np.sum(sphere_grid * proj_yz, axis=-1)
+        ang_dists_yz = np.sum(sphere_grid * proj_yz, axis=-1)
+        x_eq_mask = ang_dists_yz > eq_min_dist
 
-        # Mark points close to equator (within eq_filter_angle).
-        eq_min_dist = np.cos(eq_filter_angle * np.pi / 180)
-        n_eqs_close = np.sum(angular_dists > eq_min_dist, axis=0)
-        eq_mask = n_eqs_close > 0
+        # Mask for all views close to an equator.
+        eq_mask = z_eq_mask | y_eq_mask | x_eq_mask
 
-        # Classify equators.
-        # 1 -> z equator
-        # 2 -> y equator
-        # 3 -> x equator
-        # 4 -> z top view, ie. both x and y equator
-        # 5 -> y top view, ie. both x and z equator
-        # 6 -> x top view, ie. both y and z equator
-        eq_class = np.zeros(n_rots)
-        top_view_mask = n_eqs_close > 1
+        # Top view masks.
+        # A top view is a view along an axis of symmetry (ie. x, y, or z).
+        # A top view is also at the intersection of the two equator views
+        # perpendicular to the axis of symmetry.
+        z_top_view_mask = y_eq_mask & x_eq_mask
+        y_top_view_mask = z_eq_mask & x_eq_mask
+        x_top_view_mask = z_eq_mask & y_eq_mask
+        top_view_mask = z_top_view_mask | y_top_view_mask | x_top_view_mask
+
+        return eq_mask, top_view_mask
+
+    @staticmethod
+    def generate_inplane_rots(sphere_grid, d_theta):
+        """
+        This function takes projection directions (points on the 2-sphere) and
+        generates rotation matrices in SO(3). The projection direction
+        is the 3rd column and columns 1 and 2 span the perpendicular plane.
+        To properly discretize SO(3), for each projection direction we generate
+        [2*pi/dtheta] "in-plane" rotations, of the plane
+        perpendicular to this direction. This is done by generating one rotation
+        for each direction and then multiplying on the right by a rotation about
+        the Z-axis by k*dtheta degrees, k=0...2*pi/dtheta-1.
+
+        :param sphere_grid: A set of points on the 2-sphere.
+        :param d_theta: Resolution for in-plane rotations (in degrees)
+        :returns: 4D array of rotations of size len(sphere_grid) x n_inplane_rots x 3 x 3.
+        """
+        dtype = sphere_grid.dtype
+        # Generate one rotation for each point on the sphere.
+        n_rots = len(sphere_grid)
+        Ri2 = np.column_stack((-sphere_grid[:, 2], sphere_grid[:, 1], np.zeros(n_rots)))
+        Ri2 /= np.linalg.norm(Ri2, axis=1)[:, None]
+        Ri1 = np.cross(Ri2, sphere_grid)
+        Ri1 /= np.linalg.norm(Ri1, axis=1)[:, None]
+
+        rots_grid = np.zeros((n_rots, 3, 3), dtype=dtype)
+        rots_grid[:, :, 0] = Ri1
+        rots_grid[:, :, 1] = Ri2
+        rots_grid[:, :, 2] = sphere_grid
+
+        # Generate in-plane rotations.
+        d_theta *= np.pi / 180
+        inplane_rots = Rotation.about_axis(
+            "z", np.arange(0, 2 * np.pi, d_theta), dtype=dtype
+        ).matrices
+        n_inplane_rots = len(inplane_rots)
+
+        # Generate in-plane rotations of rots_grid.
+        inplane_rotated_grid = np.zeros((n_rots, n_inplane_rots, 3, 3), dtype=dtype)
+        for i in range(n_rots):
+            inplane_rotated_grid[i] = rots_grid[i] @ inplane_rots
+
+        return inplane_rotated_grid
