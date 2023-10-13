a3660915af40142d162a0b40ac3d18468f4030d7
Josh Carmichael <carmichael@princeton.edu>
2023-10-13 14:55:04 -0400

    Add files from commonline_sdp branch

--- src/aspire/abinitio/commonline_base.py
+++ src/aspire/abinitio/commonline_base.py
@@ -27,9 +27,9 @@ class CLOrient3D:
         mask=True,
     ):
         """
-        Initialize an object for estimating 3D orientations using common lines
+        Initialize an object for estimating 3D orientations using common lines.
 
-        :param src: The source object of 2D denoised or class-averaged imag
+        :param src: The source object of 2D denoised or class-averaged images.
         :param n_rad: The number of points in the radial direction. If None,
             n_rad will default to the ceiling of half the resolution of the source.
         :param n_theta: The number of points in the theta direction. This value must be even.
