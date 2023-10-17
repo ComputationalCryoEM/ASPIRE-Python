11e6a1c05a53c4ebe0d42bc67806322cb63dd0be
Josh Carmichael <carmichael@princeton.edu>
2023-10-17 13:26:00 -0400

    os.path.join, test cleanup.

--- tests/test_docstring_checker.py
+++ tests/test_docstring_checker.py
@@ -1,22 +1,32 @@
+import os
 import subprocess
 
 
 def test_check_docstrings():
+    DATA_DIR = os.path.join(os.path.dirname(__file__), "saved_test_data")
+    DOCS_CHECKER = os.path.join(
+        os.path.dirname(__file__), "..", "docs", "check_docstrings.py"
+    )
+
     result = subprocess.run(
-        ["python", "docs/check_docstrings.py", "tests/saved_test_data"],
+        ["python", DOCS_CHECKER, DATA_DIR],
         capture_output=True,
         text=True,
     )
 
-    good_doc_1 = "sample_docstrings.py: 2: Must have exactly 1 blank line"
-    good_doc_2 = "sample_docstrings.py: 16: Must have exactly 1 blank line"
-    err_1 = "sample_docstrings.py: 24: Must have exactly 1 blank line"
-    err_2 = "sample_docstrings.py: 34: Must have exactly 1 blank line"
+    good_doc_line_nums = [2, 16, 25]
+    bad_doc_line_nums = [35, 45, 57]
 
     # Check that good docstrings do not log error
-    assert good_doc_1 not in result.stderr
-    assert good_doc_2 not in result.stderr
+    for line_num in good_doc_line_nums:
+        msg = f"sample_docstrings.py: {line_num}: Must have exactly 1 blank line"
+        assert msg not in result.stderr
 
     # Check that bad docstrings log error
-    assert err_1 in result.stderr
-    assert err_2 in result.stderr
+    for line_num in bad_doc_line_nums:
+        msg = f"sample_docstrings.py: {line_num}: Must have exactly 1 blank line"
+        assert msg in result.stderr
+
+    # Check total error count log
+    msg = f"Found {len(bad_doc_line_nums)} docstring errors"
+    assert msg in result.stderr
