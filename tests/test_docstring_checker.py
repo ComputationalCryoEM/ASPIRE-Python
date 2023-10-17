040718c488cbc5abdbe85197a8ceb3941f845cbd
Josh Carmichael <carmichael@princeton.edu>
2023-10-17 12:11:24 -0400

    update test

--- tests/test_docstring_checker.py
+++ tests/test_docstring_checker.py
@@ -8,9 +8,15 @@ def test_check_docstrings():
         text=True,
     )
 
-    err_1 = "sample_docstrings.py: 22"
-    err_2 = "sample_docstrings.py: 31"
+    good_doc_1 = "sample_docstrings.py: 2: Must have exactly 1 blank line"
+    good_doc_2 = "sample_docstrings.py: 16: Must have exactly 1 blank line"
+    err_1 = "sample_docstrings.py: 24: Must have exactly 1 blank line"
+    err_2 = "sample_docstrings.py: 34: Must have exactly 1 blank line"
 
-    assert result.stdout == ""
+    # Check that good docstrings do not log error
+    assert good_doc_1 not in result.stderr
+    assert good_doc_2 not in result.stderr
+
+    # Check that bad docstrings log error
     assert err_1 in result.stderr
     assert err_2 in result.stderr
