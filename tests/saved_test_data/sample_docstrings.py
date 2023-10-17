7646dd5d55f717f61085c307094e0063e279fec4
Josh Carmichael <carmichael@princeton.edu>
2023-10-17 11:58:37 -0400

    Initial test

---
+++ tests/saved_test_data/sample_docstrings.py
@@ -0,0 +1,39 @@
+def good_fun1(frog, dog):
+    """
+    This docstring is properly formatted.
+
+    It has a multi-line, multi-section body
+    followed by exactly one blank line.
+
+    :param frog: This param description is
+        multiline.
+    :param dog: Single line description
+    :return: A frog on a dog
+    """
+
+def good_fun2():
+    """
+    This function has only a return.
+
+    :return: Just a return.
+    """
+
+def bad_fun1(cat, hat):
+    """
+    This docstring is missing a blank line
+    between the body and parameter sections.
+    :param cat: A cat.
+    :param hat: A hat.
+    :return: A cat in a hat.
+    """
+
+def bad_fun2(foo):
+    """
+    This docstring has too many blank lines between
+    the body and parameter sections.
+
+
+    :param foo: foo description.
+    :return: bar
+    """
+    
