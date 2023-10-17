471a1f73e580eb09f91c1c9501dd719b10d7eac5
Josh Carmichael <carmichael@princeton.edu>
2023-10-17 11:58:37 -0400

    Initial test

---
+++ tests/test_docstring_checker.py
@@ -0,0 +1,18 @@
+import sys
+
+import pytest
+import subprocess
+
+def test_check_docstrings():
+    result = subprocess.run(
+        ["python", "docs/check_docstrings.py", "tests/saved_test_data"],
+        capture_output=True,
+        text=True,
+    )
+
+    err_1 = "sample_docstrings.py: 22"
+    err_2 = "sample_docstrings.py: 31"
+    
+    assert result.stdout == ""
+    assert err_1 in result.stderr
+    assert err_2 in result.stderr    
