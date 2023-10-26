f5812b4dc6dceba86febcb24435b6c1c6840fdbf
Josh Carmichael <carmichael@princeton.edu>
2023-10-26 14:22:30 -0400

    Rotation transformation method. Fix test_simulation.

--- src/aspire/utils/__init__.py
+++ src/aspire/utils/__init__.py
@@ -67,7 +67,11 @@ from .multiprocessing import (
     virtual_core_cpu_suggestion,
 )
 from .random import Random, choice, rand, randi, randn, random
-from .relion_interop import RelionStarFile, relion_metadata_fields
+from .relion_interop import (
+    RelionStarFile,
+    relion_metadata_fields,
+    rots_zyx_to_legacy_aspire,
+)
 from .resolution_estimation import FourierRingCorrelation, FourierShellCorrelation
 from .rotation import Rotation
 from .units import ratio_to_decibel, voltage_to_wavelength, wavelength_to_voltage
