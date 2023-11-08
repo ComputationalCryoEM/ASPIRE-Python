64d277090d3fa6fb28ce32ccf82a6acb2e30af4b
Garrett Wright <garrettwrong@gmail.com>
2023-11-08 11:38:00 -0500

    update prolate  unit tests after 'yx' internal grid change

--- tests/test_PSWFbasis2D.py
+++ tests/test_PSWFbasis2D.py
@@ -16,9 +16,7 @@ test_bases = [PSWFBasis2D(L, dtype=dtype) for L, dtype in pswf_params_2d]
 @pytest.mark.parametrize("basis", test_bases, ids=show_basis_params)
 class TestPSWFBasis2D(UniversalBasisMixin):
     def testPSWFBasis2DEvaluate_t(self, basis):
-        img_ary = np.load(
-            os.path.join(DATA_DIR, "ffbbasis2d_xcoef_in_8_8.npy")
-        ).T  # RCOPT
+        img_ary = np.load(os.path.join(DATA_DIR, "ffbbasis2d_xcoef_in_8_8.npy"))
         images = Image(img_ary)
 
         result = basis.evaluate_t(images)
@@ -37,5 +35,10 @@ class TestPSWFBasis2D(UniversalBasisMixin):
         coefs = ComplexCoef(basis, ccoefs).to_real()
 
         result = coefs.evaluate()
-        images = np.load(os.path.join(DATA_DIR, "pswf2d_xcoef_out_8_8.npy")).T  # RCOPT
-        assert np.allclose(result.asnumpy(), images)
+
+        # This hardcoded reference result requires transposing the stack axis.
+        images = np.transpose(
+            np.load(os.path.join(DATA_DIR, "pswf2d_xcoef_out_8_8.npy")), (2, 0, 1)
+        )
+
+        np.testing.assert_allclose(result.asnumpy(), images, rtol=1e-05, atol=1e-08)
