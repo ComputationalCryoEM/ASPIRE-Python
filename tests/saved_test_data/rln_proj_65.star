53bca8d244183cdffd8492f7f8a9d8cc162d1b5f
Josh Carmichael <carmichael@princeton.edu>
2023-11-21 09:15:38 -0500

    Test for even res. Use fixtures.

---
+++ tests/saved_test_data/rln_proj_65.star
@@ -0,0 +1,19 @@
+
+
+# version 30001
+
+data_particles
+
+loop_ 
+_rlnAngleRot #1 
+_rlnAngleTilt #2 
+_rlnAnglePsi #3 
+_rlnOriginXAngst #4 
+_rlnOriginYAngst #5 
+_rlnOpticsGroup #6 
+_rlnImageName #7 
+  355.858841    72.200518   240.688133     0.000000     0.000000            1 000001@rln_proj_65.mrcs 
+   63.759542    92.697219   303.796113     0.000000     0.000000            1 000002@rln_proj_65.mrcs 
+  178.751045    42.736465   137.511331     0.000000     0.000000            1 000003@rln_proj_65.mrcs 
+  248.292861    83.402259   197.749400     0.000000     0.000000            1 000004@rln_proj_65.mrcs 
+   99.680307    72.793409   270.791295     0.000000     0.000000            1 000005@rln_proj_65.mrcs 
--- tests/test_relion_interop.py
+++ tests/test_relion_interop.py
@@ -1,6 +1,7 @@
 import os
 
 import numpy as np
+import pytest
 
 from aspire.source import RelionSource, Simulation
 from aspire.volume import Volume
@@ -8,27 +9,46 @@ from aspire.volume import Volume
 DATA_DIR = os.path.join(os.path.dirname(__file__), "saved_test_data")
 
 
-def test_projections():
-    # Create RelionSource from Relion generated projection images.
-    starfile = os.path.join(DATA_DIR, "rln_proj.star")
+STARFILE = ["rln_proj_65.star", "rln_proj_64.star"]
+
+
+@pytest.fixture(params=STARFILE, ids=lambda x: f"resolution={x}", scope="module")
+def starfile(request):
+    filepath = request.param
+    return os.path.join(DATA_DIR, filepath)
+
+
+@pytest.fixture(scope="module")
+def sources(starfile):
     rln_src = RelionSource(starfile)
 
-    # Create Simulation source using same volume and angles.
-    # Note, Relion projections are shifted by 1 pixel compared to ASPIRE.
-    dtype = rln_src.dtype
+    # Generate Volume used for Relion projections.
+    # Note, `downsample` is a no-op for resolution 65.
     vol_path = os.path.join(DATA_DIR, "clean70SRibosome_vol.npy")
-    vol = Volume(np.load(vol_path), dtype=dtype)
+    vol = Volume(np.load(vol_path), dtype=rln_src.dtype).downsample(rln_src.L)
+
+    # Create Simulation source using Volume and angles from Relion projections.
+    # Note, for odd resolution Relion projections are shifted by 1 pixel in x and y.
+    offsets = 0
+    if rln_src.L % 2 == 1:
+        offsets = -np.ones((rln_src.n, 2), dtype=rln_src.dtype)
+
     sim_src = Simulation(
         n=rln_src.n,
         vols=vol,
-        offsets=-np.ones((rln_src.n, 2), dtype=dtype),
+        offsets=offsets,
         angles=rln_src.angles,
-        dtype=dtype,
+        dtype=rln_src.dtype,
     )
+    return rln_src, sim_src
+
+
+def test_projections(sources):
+    rln_src, sim_src = sources
 
     # Compute the Fourier Ring Correlation.
     res, corr = rln_src.images[:].frc(sim_src.images[:], cutoff=0.143)
 
-    # Check that estimated resolution is small and correlation is close to 1.
+    # Check that estimated resolution is high (< 2.5 pixels) and correlation is close to 1.
     np.testing.assert_array_less(res, 2.5)
-    np.testing.assert_array_less(1 - corr[:, -2], 0.0015)
+    np.testing.assert_array_less(1 - corr[:, -2], 0.02)
