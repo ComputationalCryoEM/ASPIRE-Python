3a897a56f67e8378bcfc5fd6f2216c71e7456f6e
Josh Carmichael <carmichael@princeton.edu>
2023-12-01 15:48:19 -0500

    match matlab MarkEquators

--- src/aspire/abinitio/__init__.py
+++ src/aspire/abinitio/__init__.py
@@ -8,5 +8,6 @@ from .commonline_sync3n import CLSync3N
 from .commonline_c3_c4 import CLSymmetryC3C4
 from .commonline_cn import CLSymmetryCn
 from .commonline_c2 import CLSymmetryC2
+from .commonline_d2 import CLSymmetryD2
 
 # isort: on
