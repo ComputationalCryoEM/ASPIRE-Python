614c484a89af44ebe4067cf27c459675e5ee4f9f
Garrett Wright <garrettwrong@gmail.com>
2024-04-11 15:24:23 -0400

    stashing

--- x.py
+++ x.py
@@ -1,4 +1,5 @@
 from aspire.abinitio.commonline_sync3n import _signs_times_v_cupy
+from aspire.abinitio.commonline_sync3n import _signs_times_v_host
 import numpy as np
 import cupy as cp
 
@@ -7,9 +8,15 @@ n_pairs = n*(n-1)//2
 vec = np.ones(n_pairs, dtype=np.float64)
 new_vec = np.zeros(n_pairs, dtype=np.float64)
 #Rijs = np.random.randn(n_pairs*3*3).astype(dtype=np.float64)
-Rijs = np.arange(n_pairs*3*3).astype(dtype=np.float64)
+Rijs = np.arange(n_pairs*3*3).reshape(n_pairs,3,3).astype(dtype=np.float64)
 
 
-new_vec = _signs_times_v_cupy(n, Rijs, vec, J_weighting=None, _ALTS=None, _signs_confs=None)
+new_vec = _signs_times_v_cupy(n, Rijs, vec, J_weighting=None, _ALTS=None)
 
+print("gpu\n")
 print(new_vec)
+
+new_vec_host = _signs_times_v_host(n, Rijs, vec, J_weighting=None, _ALTS=None)
+
+print("host\n",new_vec_host)
+
