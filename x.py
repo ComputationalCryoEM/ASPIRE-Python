a1b2c95e5551ef52e3fd8421ce4aad6e6ea308b8
Garrett Wright <garrettwrong@gmail.com>
2024-04-11 16:36:58 -0400

    debug

--- x.py
+++ x.py
@@ -2,20 +2,20 @@ import cupy as cp
 import numpy as np
 
 from aspire.abinitio.commonline_sync3n import _signs_times_v_cupy, _signs_times_v_host
+from aspire.utils import all_pairs
 
-n = 7
+n = 4
 n_pairs = n * (n - 1) // 2
+_, _pairs_to_linear = all_pairs(n, return_map=True)
+
 vec = np.ones(n_pairs, dtype=np.float64)
-new_vec = np.zeros(n_pairs, dtype=np.float64)
 # Rijs = np.random.randn(n_pairs*3*3).astype(dtype=np.float64)
 Rijs = np.arange(n_pairs * 3 * 3).reshape(n_pairs, 3, 3).astype(dtype=np.float64)
 
 
 new_vec = _signs_times_v_cupy(n, Rijs, vec, J_weighting=None, _ALTS=None)
+print("gpu\n", new_vec)
 
-print("gpu\n")
-print(new_vec)
 
-new_vec_host = _signs_times_v_host(n, Rijs, vec, J_weighting=None, _ALTS=None)
-
+new_vec_host = _signs_times_v_host(n, Rijs, vec, J_weighting=None, _ALTS=None, _pairs_to_linear=_pairs_to_linear)
 print("host\n", new_vec_host)
