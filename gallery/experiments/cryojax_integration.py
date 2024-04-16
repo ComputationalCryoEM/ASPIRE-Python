70e7d8f0031a5f071bab8cf426851d5811f83223
Garrett Wright <garrettwrong@gmail.com>
2024-04-16 08:27:47 -0400

    cleanup imports to tof

--- gallery/experiments/cryojax_integration.py
+++ gallery/experiments/cryojax_integration.py
@@ -6,30 +6,37 @@ conda activate cryojax-aspire-env
 python -m pip install cryojax
 # note if installing from source, will need `git-lfs pull` to download data files.
 # note that we actually ended up using the `grid-search` development branch (not the pypi package above)
-# Todo, update these notes when polishing the example.
+# Todo, update these notes when polishing the example or wait for their next release to publish...
 
 
 git clone -b develop git@github.com:ComputationalCryoEM/ASPIRE-Python
 cd ASPIRE-Python
 pip install -e ".[dev]"
-# Installing from pypi was similar, no issues.
+# Installing from pypi was similar, no issues.  For publishing post we should just use the pypi release...
 
 """
+
+# CryoJAX imports
+
+from functools import partial
+
+import cryojax as cx
+import cryojax.simulator as cxs
+import equinox as eqx
+import equinox.internal as eqxi
 
 # Jax imports
 import jax
 import jax.numpy as jnp
 import numpy as np
+from cryojax.data import read_array_with_spacing_from_mrc
+from cryojax.image.operators import FourierExp2D
+from cryojax.rotations import SO3
+from jaxtyping import PRNGKeyArray, PyTree
 
 # Plotting imports and functions
 from matplotlib import pyplot as plt
 from mpl_toolkits.axes_grid1 import make_axes_locatable
-
-
-# CryoJAX imports
-import cryojax.simulator as cxs
-from cryojax.data import read_array_with_spacing_from_mrc
-from cryojax.rotations import SO3
 
 # First, load the scattering potential and projection method
 filename = "./data/ribosome_4ug0_scattering_potential_from_cistem.mrc"
@@ -59,9 +66,6 @@ instrument_with_detector = cxs.Instrument(
 )
 
 
-from cryojax.image.operators import FourierExp2D
-
-
 # TODO: Figure out how to reduce the noise to something easier to run a demo sized recon with...
 # Then, choose a model for the solvent. The amplitude is the
 # (squared) characteristic phase shift of the ice phase shifts, and the length_scale is
@@ -76,12 +80,6 @@ pixel_size = potential.voxel_size  # Angstroms
 image_size = np.asarray(shape) * pixel_size
 config = cxs.ImageConfig(shape, pixel_size, pad_scale=1.1)
 
-from functools import partial
-
-import equinox as eqx
-import equinox.internal as eqxi
-from jaxtyping import PRNGKeyArray, PyTree
-
 
 @partial(eqx.filter_vmap, in_axes=(0, None), out_axes=eqxi.if_mapped(axis=0))
 def make_pipeline(key: PRNGKeyArray, no_vmap: tuple[PyTree, ...]) -> cxs.ImagePipeline:
@@ -95,7 +93,8 @@ def make_pipeline(key: PRNGKeyArray, no_vmap: tuple[PyTree, ...]) -> cxs.ImagePi
     in_plane_offset_in_angstroms = (
         jax.random.uniform(key, (2,), minval=-0.45, maxval=0.45)
         * jnp.asarray((nx, ny))
-        * config.pixel_size * 0 # hack
+        * config.pixel_size
+        * 0  # hack
     )
     # ... convert 2D in-plane translation to 3D, setting the out-of-plane translation to
     # zero
@@ -107,19 +106,20 @@ def make_pipeline(key: PRNGKeyArray, no_vmap: tuple[PyTree, ...]) -> cxs.ImagePi
 
     # # TODO: Ask what I'm doing wrong here ...
     # pose = cxs.QuaternionPose.from_rotation(rotation)
-    
+
     # ... build the Specimen and ImagePipeline as usual and return
     specimen = cxs.Specimen(potential, integrator, pose)
     return cxs.ImagePipeline(config, specimen, instrument, solvent=solvent)
+
 
 # Generate RNG keys
 number_of_poses = 5000
 keys = jax.random.split(jax.random.PRNGKey(12345), number_of_poses)
 
 # ... instantiate the pipeline
-pipeline = make_pipeline(keys, (config, potential, integrator, instrument_with_detector, solvent))
-
-import cryojax as cx
+pipeline = make_pipeline(
+    keys, (config, potential, integrator, instrument_with_detector, solvent)
+)
 
 
 # ... specify which leaves we would like to vmap over
@@ -127,16 +127,14 @@ where = lambda pipeline: pipeline.specimen.pose
 # ... use a cryojax wrapper to return a filter_spec
 filter_spec = cx.get_filter_spec(pipeline, where)
 
-import equinox as eqx
-
 
 @partial(cx.filter_vmap_with_spec, filter_spec=filter_spec)
 def compute_image_stack(pipeline, key):
     """Compute a batch of images at different poses,
     specified by the `filter_spec`.
     """
-    #key = jax.random.PRNGKey(77)
-    #image = pipeline.render()
+    # key = jax.random.PRNGKey(77)
+    # image = pipeline.render()
     image = pipeline.sample(key)
     return image - image.mean()
 
@@ -144,11 +142,10 @@ def compute_image_stack(pipeline, key):
 res = compute_image_stack(pipeline, keys)
 # convert to numpy array
 res_np = np.array(res).astype(np.float64)
-
-np.save('res5k_px89.npy', res)
+# save off
+np.save(f"res_{number_of_poses}_px{shape[0]}.npy", res)
 
 #### Begin ASPIRE
-
 
 import aspire
 
@@ -162,7 +159,6 @@ src.images[:10].show(colorbar=False)
 
 # Class averaging
 
-from aspire.classification import RIRClass2D
 
 # set parameters
 n_classes = 200
@@ -171,40 +167,35 @@ n_nbor = 10
 # We will customize our class averaging source. Note that the
 # ``fspca_components`` and ``bispectrum_components`` were selected for
 # this small tutorial.
-rir = RIRClass2D(
+rir = aspire.classification.RIRClass2D(
     src,
     fspca_components=40,
     bispectrum_components=30,
     n_nbor=n_nbor,
 )
 
-from aspire.denoising import DebugClassAvgSource
 
-avgs = DebugClassAvgSource(
+avgs = aspire.denoising.DebugClassAvgSource(
     src=src,
     classifier=rir,
 )
 
 # We'll continue our pipeline with the first ``n_classes`` from ``avgs``.
 avgs = avgs[:n_classes]
-avgs.images[:].save('avgs.mrcs', overwrite=True)
+avgs.images[:].save("avgs.mrcs", overwrite=True)
 
 # Show class averages
 avgs.images[0:10].show()
 
 # Orientation Estimation
-
-from aspire.source import OrientedSource
-
 # Instantiate an ``OrientedSource``.
-oriented_src = OrientedSource(avgs)
+oriented_src = aspire.source.OrientedSource(avgs)
 
-from aspire.reconstruction import MeanEstimator
 
 # Setup an estimator to perform the back projection.
-estimator = MeanEstimator(oriented_src)
+estimator = aspire.reconstruction.MeanEstimator(oriented_src)
 
 # Perform the estimation and save the volume.
 estimated_volume = estimator.estimate()
 
-estimated_volume.save('jaxvol.mrc', overwrite=True)
+estimated_volume.save("jaxvol.mrc", overwrite=True)
