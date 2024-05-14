de3cacfc34e50479accc635e640766b61970463a
Josh Carmichael <carmichael@princeton.edu>
2024-05-14 11:51:48 -0400

    Test for global_J_sync.

--- tests/test_orient_d2.py
+++ tests/test_orient_d2.py
@@ -83,7 +83,7 @@ def orient_est(source):
         grid_res=350,  # Tuned for speed
         inplane_res=15,  # Tuned for speed
         eq_min_dist=10,  # Tuned for speed
-        epsilon=0.01,
+        epsilon=0.001,
         seed=SEED,
     )
 
@@ -96,6 +96,10 @@ def orient_est(source):
 
 
 def test_estimate_rotations(orient_est):
+    """
+    This test runs through the complete D2 algorithm and compares the
+    estimated rotations to the ground truth rotations.
+    """
     # Estimate rotations.
     orient_est.estimate_rotations()
     rots_est = orient_est.rotations
@@ -112,6 +116,65 @@ def test_estimate_rotations(orient_est):
     if orient_est.src.offsets.all() != 0:
         deg_tol = 7
     mean_aligned_angular_distance(rots_est, rots_gt_sync, degree_tol=deg_tol)
+
+
+def test_global_J_sync(orient_est):
+    """
+    For this test we build a set of relative rotations, Rijs, of shape
+    (npairs, order(D2), 3, 3) and randomly J_conjugate them. We then test
+    that the J-configuration is correctly detected and that J-synchronization
+    is correct up to conjugation of the entire set.
+    """
+    # Grab set of rotations and generate a set of relative rotations, Rijs.
+    rots = orient_est.src.rotations
+    Rijs = np.zeros((orient_est.n_pairs, 4, 3, 3), dtype=orient_est.dtype)
+    for p, (i, j) in enumerate(orient_est.pairs):
+        for k, g in enumerate(orient_est.gs):
+            k = (k + p) % 4  # Mix up the ordering of symmetric Rijs
+            Rijs[p, k] = rots[i].T @ (g * rots[j])
+
+    # J-conjugate a random set of Rijs.
+    Rijs_conj = Rijs.copy()
+    inds = np.random.choice(orient_est.n_pairs, size=15, replace=False)
+    Rijs_conj[inds] = J_conjugate(Rijs[inds])
+
+    # Create J-configuration conditions for the triplet Rij, Rjk, Rik.
+    J_conds = {
+        (False, False, False): 0,
+        (True, True, True): 0,
+        (True, False, False): 1,
+        (False, True, True): 1,
+        (False, True, False): 2,
+        (True, False, True): 2,
+        (False, False, True): 3,
+        (True, True, False): 3,
+    }
+
+    # Construct ground truth J-configuration list based on `inds` of Rijs
+    # that have been conjugated above.
+    J_list_gt = np.zeros(len(orient_est.triplets), dtype=int)
+    idx = 0
+    for i, j, k in orient_est.triplets:
+        ij = orient_est.pairs_to_linear[i, j]
+        jk = orient_est.pairs_to_linear[j, k]
+        ik = orient_est.pairs_to_linear[i, k]
+
+        J_conf = (ij in inds, jk in inds, ik in inds)
+        J_list_gt[idx] = J_conds[J_conf]
+        idx += 1
+
+    # Perform J-configuration and compare to ground truth.
+    J_list = orient_est._J_configuration(Rijs_conj)
+    np.testing.assert_equal(J_list, J_list_gt)
+
+    # Perform global J-synchronization and check that
+    # Rijs_sync is equal to either Rijs or J_conjugate(Rijs).
+    Rijs_sync = orient_est._global_J_sync(Rijs_conj)
+    need_to_conj_Rijs = ~np.allclose(Rijs_sync[inds][0], Rijs[inds][0])
+    if need_to_conj_Rijs:
+        np.testing.assert_allclose(Rijs_sync, J_conjugate(Rijs))
+    else:
+        np.testing.assert_allclose(Rijs_sync, Rijs)
 
 
 ####################
