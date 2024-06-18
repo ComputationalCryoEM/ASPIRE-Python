34cd127515e7838eaff9647a619f714272dd3d46
Marc Karimi <mk3652@decaf.math.princeton.edu.private>
2024-06-18 12:33:31 -0400

    Stashing initial project with test placeholder

--- tests/test_sinogram.py
+++ tests/test_sinogram.py
@@ -1,1 +1,23 @@
 import pytest
+from skimage import data
+from skimage.transform import radon
+from aspire.image import Image
+import numpy as np
+
+#Image.project and compare results to skimage.radon
+
+def test_image_project():
+    image = Image(data.camera().astype(np.float64))
+    ny = image.resolution
+    angles = np.linspace(0, 360, ny, endpoint=False)
+    rads = angles / 180 * np.pi
+    s = image.project(rads)
+
+    # add reference skimage radon here
+
+    
+    #compare s with reference
+    
+    
+
+
