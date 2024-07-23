0dd278a3731e6b03231458a1331d2a70a2bbdd36
Marc Karimi <mk3652@decaf.math.princeton.edu.private>
2024-07-23 11:12:00 -0400

    Stashing

--- src/aspire/image/line.py
+++ src/aspire/image/line.py
@@ -2,18 +2,16 @@ import logging
 import os
 from pathlib import Path
 
-import numpy as np
-
-import aspire
 from aspire.image import Image
 from aspire.nufft import anufft, nufft
+import aspire
+import numpy as np
 
 # noticed a lot of classes had these already, might be helpful for pathing, logging info, etc. (inc. os, logging)
 logger = logging.getLogger(__name__)
 
-
 class Line:
-    def __init__(self, data, dtype=np.float64):
+    def __init__(self, data, dtype = np.float64):
         """
         Initialize a Line Object. Change later (similar intuition from Image class)
         Question: Is it a line or collection of line object?
@@ -27,11 +25,11 @@ class Line:
         if data.ndim == 2:
             data = data[np.newaxis, :, :]
         if data.ndim < 3:
-            raise ("Projection Dimensions should be more than Three-Dimensions")
+            raise('Projection Dimensions should be more than Three-Dimensions')
         self._data = data.astype(self.dtype, copy=False)
         self.stack_shape = self._data.shape[:-2]
-        self.n_images = self._data.shape[0]  # broken for higher dimensional stacks
-        self.n_lines = self._data.shape[-2]
+        self.n_images = self._data.shape[0] #broken for higher dimensional stacks
+        self.n_lines = self._data.shape[-2] 
         self.n_points = self._data.shape[-1]
         # self.n_dim = (self._data.shape[1], self._data.shape[2])
 
@@ -45,7 +43,7 @@ class Line:
     def back_project(self, angles):
         """
         Back Projection Method for a single stack of lines.
-
+        
         :param filter_name: string, optional
             Filter used in frequency domain filtering. Assign None to use no filter.
         :param angles: array
@@ -54,26 +52,25 @@ class Line:
         """
         sinogram = self._data
         n_img, n_angles, n_rad = sinogram.shape
-        assert n_angles == len(
-            angles
-        ), "Number of angles must match the number of projections"
+        assert n_angles == len(angles), "Number of angles must match the number of projections"
 
         L = n_rad
-        sinogram = np.fft.ifftshift(sinogram, axes=-1)
-        sinogram_ft = np.fft.rfft(sinogram, axis=-1)
+        sinogram = np.fft.ifftshift(sinogram, axes= -1)
+        sinogram_ft = np.fft.rfft(sinogram, axis= -1)
 
-        # grid generation with real points
+        #grid generation with real points
         y_idx = np.fft.rfftfreq(n_rad) * np.pi * 2
         n_real_points = len(y_idx)
         pts = np.empty((2, len(angles), n_real_points), dtype=self.dtype)
         pts[0] = y_idx[np.newaxis, :] * np.sin(angles)[:, np.newaxis]
         pts[1] = y_idx[np.newaxis, :] * np.cos(angles)[:, np.newaxis]
-
+        
         imgs = aspire.nufft.anufft(
             sinogram_ft.reshape(n_img, -1),
             pts.reshape(2, n_real_points * len(angles)),
             sz=(L, L),
-            real=True,
+            real=True
         ).reshape(n_img, L, L)
-
+        
         return aspire.image.Image(imgs)
+    
