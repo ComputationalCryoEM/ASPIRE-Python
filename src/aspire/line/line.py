9af2d836b12d25dff7225d39e85f2e32e1138401
Marc Karimi <mk3652@decaf.math.princeton.edu.private>
2024-07-30 05:04:39 -0400

    finished multidim test

--- src/aspire/line/line.py
+++ src/aspire/line/line.py
@@ -30,9 +30,9 @@ class Line:
         self.ndim = self._data.ndim
         self.shape = self._data.shape
         self.stack_shape = self._data.shape[:-2]
-        self.stack_n_dim = self._data.ndim - 2  # fix
-        self.n = np.product(self.stack_shape)  # stack number
-        self.n_angles = self._data.shape[-1]  # fix
+        self.stack_n_dim = self._data.ndim - 2
+        self.n = np.product(self.stack_shape)
+        self.n_angles = self._data.shape[-2]
         self.n_radial_points = self._data.shape[-1]
 
         # Numpy interop
@@ -93,50 +93,7 @@ class Line:
     def copy(self):
         return self.__class__(self._data.copy())
 
-    # fix later
-    def save(self, mrcs_filepath, overwrite=False):
-        if self.stack_ndim > 1:
-            raise NotImplementedError("`save` is currently limited to 1D image stacks.")
-
-        with mrcfile.new(mrcs_filepath, overwrite=overwrite) as mrc:
-            # original input format (the image index first)
-            mrc.set_data(self._data.astype(np.float32))
-
-    # fix later
-    @staticmethod
-    def load(filepath, dtype=None):
-        """
-        Load raw data from supported files.
-
-        Currently MRC and TIFF are supported.
-
-        :param filepath: File path (string).
-        :param dtype: Optionally force cast to `dtype`.
-             Default dtype is inferred from the file contents.
-        :return: numpy array of image data.
-        """
-
-        # Get the file extension
-        ext = os.path.splitext(filepath)[1]
-
-        # On unsupported extension, raise with suggested file types
-        if ext not in Image.extensions:
-            raise RuntimeError(
-                f"Attempting to open unsupported file extension '{ext}', try {list(Image.extensions.keys())}."
-            )
-
-        # Call the appropriate file reader
-        im = Image.extensions[ext](filepath)
-
-        # Attempt casting when user provides dtype
-        if dtype is not None:
-            im = im.astype(dtype, copy=False)
-
-        # Return as Image instance
-        return Image(im)
-
     def __str__(self):
-        # fix later
         return f"Line(n_images = {self.n}, n_angles = {self.n_points}, n_radial_points = {self.n_radial_points})"
 
     @property
@@ -153,30 +110,30 @@ class Line:
             assuming not perfectly radial angles
         :return: stack of reconstructed
         """
-        sinogram = self._data
-        n_img, n_angles, n_rad = sinogram.shape
-        assert n_angles == len(
-            angles
+        assert (
+            len(angles) == self.n_angles
         ), "Number of angles must match the number of projections"
 
-        L = n_rad
+        original_stack_shape = self.stack_shape
+        sinogram = self.stack_reshape(-1)
+        L = self.n_radial_points
         sinogram = np.fft.ifftshift(sinogram, axes=-1)
         sinogram_ft = np.fft.rfft(sinogram, axis=-1)
 
         # grid generation with real points
-        y_idx = np.fft.rfftfreq(n_rad) * np.pi * 2
+        y_idx = np.fft.rfftfreq(self.n_radial_points) * np.pi * 2
         n_real_points = len(y_idx)
         pts = np.empty((2, len(angles), n_real_points), dtype=self.dtype)
         pts[0] = y_idx[np.newaxis, :] * np.sin(angles)[:, np.newaxis]
         pts[1] = y_idx[np.newaxis, :] * np.cos(angles)[:, np.newaxis]
 
         imgs = anufft(
-            sinogram_ft.reshape(n_img, -1),
+            sinogram_ft.reshape(self.n, -1),
             pts.reshape(2, n_real_points * len(angles)),
             sz=(L, L),
             real=True,
-        ).reshape(n_img, L, L)
+        ).reshape(self.n, L, L)
 
         # normalization which gives us roughly the same error regardless of angles
         imgs = imgs / (n_real_points * len(angles))
-        return aspire.image.Image(imgs)
+        return aspire.image.Image(imgs).stack_reshape(original_stack_shape)
