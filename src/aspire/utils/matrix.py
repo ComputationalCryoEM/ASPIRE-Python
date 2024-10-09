9c8e13ebc4b66565e7b8d9c99070acb370308392
Garrett Wright <garrettwrong@gmail.com>
2024-10-09 14:19:48 -0400

    np2 breaks np.sign for complex

--- src/aspire/utils/matrix.py
+++ src/aspire/utils/matrix.py
@@ -493,7 +493,7 @@ def fix_signs(u):
     # Now we only care about the sign +1/-1.
     #  The following corrects for any numerical division noise,
     #  and also remaps 0 to +1.
-    signs = np.sign(signs * 2 + 1)
+    signs = np.sign(signs.real * 2 + 1)
 
     # Apply signs elementwise to matrix
     return u * signs
