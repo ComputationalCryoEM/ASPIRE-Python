39fcaa3c3b071935007825b1dfab05606ae6f926
Garrett Wright <garrettwrong@gmail.com>
2025-02-18 13:14:23 -0500

    Attempt bug fix enforcing pure rotation for CL methods

--- src/aspire/utils/matrix.py
+++ src/aspire/utils/matrix.py
@@ -321,10 +321,11 @@ def nearest_rotations(A, allow_reflection=False):
     U, _, V = np.linalg.svd(A)
 
     if not allow_reflection:
-        # If det(U)*det(V) = -1, we negate the third singular value to
-        # ensure we have a rotation.
+        # If det(U)*det(V) = -1, apply reflection about the origin
         neg_det_idx = np.linalg.det(U) * np.linalg.det(V) < 0
-        U[neg_det_idx] = U[neg_det_idx] @ np.diag((1, 1, -1)).astype(dtype, copy=False)
+        U[neg_det_idx] = U[neg_det_idx] @ np.diag((-1, -1, -1)).astype(
+            dtype, copy=False
+        )
 
     rots = U @ V
 
