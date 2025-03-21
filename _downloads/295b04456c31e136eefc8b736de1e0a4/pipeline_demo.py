93f0f532f324761a09323114f475dac861b2e462
garrettwrong <garrettwrong@users.noreply.github.com>
2025-03-21 00:10:24 +0000

    Updating Docs for commit d0f3d097fc51300652af186124f2576f76a46315 made on 2025-03-20T23:58:14+00:00 from refs/heads/main by garrettwrong

---
+++ _downloads/295b04456c31e136eefc8b736de1e0a4/pipeline_demo.py
@@ -0,0 +1,280 @@
+"""
+Ab-initio Pipeline Demonstration
+================================
+
+This tutorial demonstrates some key components of an ab-initio
+reconstruction pipeline using synthetic data generated with ASPIRE's
+``Simulation`` class of objects.
+"""
+
+# %%
+# Download an Example Volume
+# --------------------------
+# We begin by downloading a high resolution volume map of the 80S
+# Ribosome, sourced from EMDB: https://www.ebi.ac.uk/emdb/EMD-2660.
+# This is one of several volume maps that can be downloaded with
+# ASPIRE's data downloading utility by using the following import.
+# sphinx_gallery_start_ignore
+# flake8: noqa
+# sphinx_gallery_end_ignore
+from aspire.downloader import emdb_2660
+
+# Load 80s Ribosome as a ``Volume`` object.
+original_vol = emdb_2660()
+
+# Downsample the volume
+res = 41
+vol = original_vol.downsample(res)
+
+# %%
+# .. note::
+#     A ``Volume`` can be saved using the ``Volume.save()`` method as follows::
+#
+#         fn = f"downsampled_80s_ribosome_size{res}.mrc"
+#         vol.save(fn, overwrite=True)
+
+
+# %%
+# Create a Simulation Source
+# --------------------------
+# ASPIRE's ``Simulation`` class can be used to generate a synthetic
+# dataset of projection images.  A ``Simulation`` object produces
+# random projections of a supplied Volume and applies noise and CTF
+# filters. The resulting stack of 2D images is stored in an ``Image``
+# object.
+
+
+# %%
+# CTF Filters
+# ^^^^^^^^^^^^^^^^^^^^^
+# Let's start by creating CTF filters. The ``operators`` package
+# contains a collection of filter classes that can be supplied to a
+# ``Simulation``.  We use ``RadialCTFFilter`` to generate a set of CTF
+# filters with various defocus values.
+
+# Create CTF filters
+import numpy as np
+
+from aspire.operators import RadialCTFFilter
+
+# Radial CTF Filter
+defocus_min = 15000  # unit is angstroms
+defocus_max = 25000
+defocus_ct = 7
+
+ctf_filters = [
+    RadialCTFFilter(pixel_size=vol.pixel_size, defocus=d)
+    for d in np.linspace(defocus_min, defocus_max, defocus_ct)
+]
+
+# %%
+# Initialize Simulation Object
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+# We feed our ``Volume`` and filters into ``Simulation`` to generate
+# the dataset of images.  When controlled white Gaussian noise is
+# desired, ``WhiteNoiseAdder.from_snr()`` can be used to generate a
+# simulation data set around a specific SNR.
+#
+# Alternatively, users can bring their own images using an
+# ``ArrayImageSource``, or define their own custom noise functions via
+# ``Simulation(..., noise_adder=CustomNoiseAdder(...))``.  Examples
+# can be found in ``tutorials/class_averaging.py`` and
+# ``experiments/simulated_abinitio_pipeline.py``.
+
+from aspire.noise import WhiteNoiseAdder
+from aspire.source import Simulation
+
+# set parameters
+n_imgs = 2500
+
+# SNR target for white gaussian noise.
+snr = 0.5
+
+# %%
+# .. note::
+#   Note, the SNR value was chosen based on other parameters for this
+#   quick tutorial, and can be changed to adjust the power of the
+#   additive noise.
+
+# For this ``Simulation`` we set all 2D offset vectors to zero,
+# but by default offset vectors will be randomly distributed.
+src = Simulation(
+    n=n_imgs,  # number of projections
+    vols=vol,  # volume source
+    offsets=0,  # Default: images are randomly shifted
+    unique_filters=ctf_filters,
+    noise_adder=WhiteNoiseAdder.from_snr(snr=snr),  # desired SNR
+)
+
+
+# %%
+# Several Views of the Projection Images
+# --------------------------------------
+# We can access several views of the projection images.
+
+# with no corruption applied
+src.projections[0:10].show()
+
+# %%
+
+# with no noise corruption
+src.clean_images[0:10].show()
+
+# %%
+
+# with noise and CTF corruption
+src.images[0:10].show()
+
+
+# %%
+# CTF Correction
+# --------------
+# We apply ``phase_flip()`` to correct for CTF effects.
+
+src = src.phase_flip()
+
+# %%
+# Cache
+# -----
+# We apply ``cache`` to store the results of the ``ImageSource``
+# pipeline up to this point.  This is optional, but can provide
+# benefit when used intently on machines with adequate memory.
+
+src = src.cache()
+src.images[0:10].show()
+
+# %%
+# Class Averaging
+# ---------------
+# We use ``RIRClass2D`` object to classify the images via the
+# rotationally invariant representation (RIR) algorithm. Class
+# selection is customizable. The classification module also includes a
+# set of protocols for selecting a set of images to be used after
+# classification.  Here we're using the simplest
+# ``DebugClassAvgSource`` which internally uses the
+# ``TopClassSelector`` to select the first ``n_classes`` images from
+# the source.  In practice, the selection is done by sorting class
+# averages based on some configurable notion of quality (contrast,
+# neighbor distance etc).
+
+from aspire.classification import RIRClass2D
+
+# set parameters
+n_classes = 200
+n_nbor = 6
+
+# We will customize our class averaging source. Note that the
+# ``fspca_components`` and ``bispectrum_components`` were selected for
+# this small tutorial.
+rir = RIRClass2D(
+    src,
+    fspca_components=40,
+    bispectrum_components=30,
+    n_nbor=n_nbor,
+)
+
+from aspire.denoising import DebugClassAvgSource
+
+avgs = DebugClassAvgSource(
+    src=src,
+    classifier=rir,
+)
+
+# We'll continue our pipeline using only the first ``n_classes`` from
+# ``avgs``.  The ``cache()`` call is used here to precompute results
+# for the ``:n_classes`` slice.  This avoids recomputing the same
+# images twice when peeking in the next cell then requesting them in
+# the following ``CLSyncVoting`` algorithm.  Outside of demonstration
+# purposes, where we are repeatedly peeking at various stage results,
+# such caching can be dropped allowing for more lazy evaluation.
+avgs = avgs[:n_classes].cache()
+
+
+# %%
+# View the Class Averages
+# -----------------------
+
+# Show class averages
+avgs.images[0:10].show()
+
+# %%
+
+# Show original images corresponding to those classes. This 1:1
+# comparison is only expected to work because we used
+# ``TopClassSelector`` to classify our images.
+src.images[0:10].show()
+
+
+# %%
+# Orientation Estimation
+# ----------------------
+# We create an ``OrientedSource``, which consumes an ``ImageSource`` object, an
+# orientation estimator, and returns a new source which lazily estimates orientations.
+# In this case we supply ``avgs`` for our source and a ``CLSyncVoting``
+# class instance for our orientation estimator. The ``CLSyncVoting`` algorithm employs
+# a common-lines method with synchronization and voting.
+
+from aspire.abinitio import CLSyncVoting
+from aspire.source import OrientedSource
+
+# Stash true rotations for later comparison
+true_rotations = src.rotations[:n_classes]
+
+# For this low resolution example we will customize the ``CLSyncVoting``
+# instance to use fewer theta points ``n_theta`` then the default value of 360.
+orient_est = CLSyncVoting(avgs, n_theta=72)
+
+# Instantiate an ``OrientedSource``.
+oriented_src = OrientedSource(avgs, orient_est)
+
+
+# %%
+# Mean Error of Estimated Rotations
+# ---------------------------------
+# ASPIRE has the built-in utility function, ``mean_aligned_angular_distance``, which globally
+# aligns the estimated rotations to the true rotations and computes the mean
+# angular distance (in degrees).
+
+from aspire.utils import mean_aligned_angular_distance
+
+# Compare with known true rotations
+mean_ang_dist = mean_aligned_angular_distance(oriented_src.rotations, true_rotations)
+print(f"Mean aligned angular distance: {mean_ang_dist} degrees")
+
+
+# %%
+# Volume Reconstruction
+# ---------------------
+# Now that we have our class averages and rotation estimates, we can
+# estimate the mean volume by supplying the class averages and basis
+# for back projection.
+
+from aspire.reconstruction import MeanEstimator
+
+# Setup an estimator to perform the back projection.
+estimator = MeanEstimator(oriented_src)
+
+# Perform the estimation and save the volume.
+estimated_volume = estimator.estimate()
+
+
+# %%
+# Comparison of Estimated Volume with Source Volume
+# -------------------------------------------------
+# To get a visual confirmation that our results are sane, we rotate
+# the estimated volume by the estimated rotations and project along
+# the z-axis.  These estimated projections should align with the
+# original projection images.
+
+# Get the first 10 projections from the estimated volume using the
+# estimated orientations.  Recall that ``project`` returns an
+# ``Image`` instance, which we can peek at with ``show``.
+projections_est = estimated_volume.project(oriented_src.rotations[0:10])
+
+# We view the first 10 projections of the estimated volume.
+projections_est.show()
+
+# %%
+
+# For comparison, we view the first 10 source projections.
+src.projections[0:10].show()
---
+++ _downloads/2f2a93151a73ec4b42f2b2f3b0c91b1a/image_expansion.py
@@ -0,0 +1,269 @@
+"""
+Image Expansion
+===============
+
+This script illustrates several expansion methods for 2D images developed in ASPIRE package
+based on the basis functions of  Fourier-Bessel (FB) and prolate spheroidal wave function (PSWF).
+"""
+
+import os
+import timeit
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from aspire.basis import FBBasis2D, FFBBasis2D, FPSWFBasis2D, PSWFBasis2D
+from aspire.utils import anorm
+
+print("This script illustrates different image expansion methods in ASPIRE package.")
+
+# %%
+# Load Initial Images
+# -------------------
+
+# Load the images from NumPy array, 10 images of 70S Ribosome with size 129 x 129
+file_path = os.path.join(
+    os.path.dirname(os.getcwd()), "data", "example_data_np_array.npy"
+)
+# Here the images were saved in Fortran order. Transpose from (129,
+# 129, 10) to (10, 129, 129) so that the stack axis is the slowest
+# moving axis.
+org_images = np.load(file_path).T
+
+# Set the sizes of images (129, 129)
+img_size = org_images.shape[-1]
+
+# %%
+# Expand Images with Normal Fourier-Bessel Basis Method
+# -----------------------------------------------------
+
+# Specify the normal FB basis method for expanding the 2D image
+# Note, we'll set the Basis dtype to be the same as the `org_images` data,
+#  as good practice.
+fb_basis = FBBasis2D((img_size, img_size), dtype=org_images.dtype)
+
+# Get the expansion coefficients based on FB basis
+print("Start normal FB expansion of original images.")
+tstart = timeit.default_timer()
+fb_coefs = fb_basis.evaluate_t(org_images)
+tstop = timeit.default_timer()
+dtime = tstop - tstart
+print(f"Finish normal FB expansion of original images in {dtime:.4f} seconds.")
+
+# Reconstruct images from the expansion coefficients based on FB basis
+fb_images = fb_basis.evaluate(fb_coefs).asnumpy()
+print("Finish reconstruction of images from normal FB expansion coefficients.")
+
+# Calculate the mean value of maximum differences between the FB estimated images and the original images
+fb_meanmax = np.mean(np.max(abs(fb_images - org_images), axis=(1, 2)))
+print(
+    f"Mean value of maximum differences between FB estimated images and original images: {fb_meanmax}"
+)
+
+# Calculate the normalized RMSE of the FB estimated images
+fb_nrmse_ims = anorm(fb_images - org_images) / anorm(org_images)
+print(f"FB estimated images normalized RMSE: {fb_nrmse_ims}")
+
+# plot the first images using the normal FB method
+plt.subplot(1, 3, 1)
+plt.imshow(np.real(org_images[0]), cmap="gray")
+plt.title("Original")
+plt.subplot(1, 3, 2)
+plt.imshow(np.real(fb_images[0]), cmap="gray")
+plt.title("FB Image")
+plt.subplot(1, 3, 3)
+plt.imshow(np.real(org_images[0] - fb_images[0]), cmap="gray")
+plt.title("Differences")
+plt.tight_layout()
+
+# %%
+# Expand Images with Fast FB Basis Method
+# ---------------------------------------
+
+# Specify the fast FB basis method for expanding the 2D images
+# Note, we'll set the Basis dtype to be the same as the `org_image` data,
+#  as good practice.
+ffb_basis = FFBBasis2D((img_size, img_size), dtype=org_images.dtype)
+
+# Get the expansion coefficients based on fast FB basis
+print("start fast FB expansion of original images.")
+tstart = timeit.default_timer()
+ffb_coefs = ffb_basis.evaluate_t(org_images)
+tstop = timeit.default_timer()
+dtime = tstop - tstart
+print(f"Finish fast FB expansion of original images in {dtime:.4f} seconds.")
+
+# Reconstruct images from the expansion coefficients based on fast FB basis
+ffb_images = ffb_basis.evaluate(ffb_coefs).asnumpy()
+print("Finish reconstruction of images from fast FB expansion coefficients.")
+
+# Calculate the mean value of maximum differences between the fast FB estimated images to the original images
+diff = ffb_images - org_images
+ffb_meanmax = np.mean(np.max(abs(diff), axis=(1, 2)))
+print(
+    f"Mean value of maximum differences between FFB estimated images and original images: {ffb_meanmax}"
+)
+
+# Calculate the normalized RMSE of the estimated images
+ffb_nrmse_ims = anorm(diff) / anorm(org_images)
+print(f"FFB Estimated images normalized RMSE: {ffb_nrmse_ims}")
+
+# plot the first images using the fast FB method
+plt.subplot(1, 3, 1)
+plt.imshow(np.real(org_images[0]), cmap="gray")
+plt.title("Original")
+plt.subplot(1, 3, 2)
+plt.imshow(np.real(ffb_images[0]), cmap="gray")
+plt.title("FFB Image")
+plt.subplot(1, 3, 3)
+plt.imshow(np.real(org_images[0] - ffb_images[0]), cmap="gray")
+plt.title("Differences")
+plt.tight_layout()
+
+# %%
+# Expand Images with Prolate Spheroidal Wave Function
+# ---------------------------------------------------
+
+# Specify the direct PSWF basis method for expanding the 2D images
+# Note, we'll set the Basis dtype to be the same as the `org_images` data,
+#  as good practice.
+pswf_basis = PSWFBasis2D((img_size, img_size), dtype=org_images.dtype)
+
+# Get the expansion coefficients based on direct PSWF basis
+print("Start direct PSWF expansion of original images.")
+tstart = timeit.default_timer()
+pswf_coefs = pswf_basis.evaluate_t(org_images)
+tstop = timeit.default_timer()
+dtime = tstop - tstart
+print(f"Finish direct PSWF expansion of original images in {dtime:.4f} seconds.")
+
+# Reconstruct images from the expansion coefficients based on direct PSWF basis
+pswf_images = pswf_basis.evaluate(pswf_coefs).asnumpy()
+print("Finish reconstruction of images from direct PSWF expansion coefficients.")
+
+# Calculate the mean value of maximum differences between direct PSWF estimated images and original images
+diff = pswf_images - org_images
+pswf_meanmax = np.mean(np.max(abs(diff), axis=(1, 2)))
+print(
+    f"Mean value of maximum differences between PSWF estimated images and original images: {pswf_meanmax}"
+)
+
+# Calculate the normalized RMSE of the estimated images
+pswf_nrmse_ims = anorm(diff) / anorm(org_images)
+print(f"PSWF Estimated images normalized RMSE: {pswf_nrmse_ims}")
+
+# plot the first images using the direct PSWF method
+plt.subplot(1, 3, 1)
+plt.imshow(np.real(org_images[0]), cmap="gray")
+plt.title("Original")
+plt.subplot(1, 3, 2)
+plt.imshow(np.real(pswf_images[0]), cmap="gray")
+plt.title("PSWF Image")
+plt.subplot(1, 3, 3)
+plt.imshow(np.real(org_images[0] - pswf_images[0]), cmap="gray")
+plt.title("Differences")
+plt.tight_layout()
+
+# %%
+# Expand Images with Fast PSFW
+# ----------------------------
+
+# Specify the fast FPSWF basis method for expanding the 2D images
+# Note, we'll set the Basis dtype to be the same as the `org_images` data,
+#  as good practice.
+fpswf_basis = FPSWFBasis2D((img_size, img_size), dtype=org_images.dtype)
+
+# Get the expansion coefficients based on fast PSWF basis
+print("Start fast PSWF expansion of original images.")
+tstart = timeit.default_timer()
+fpswf_coefs = fpswf_basis.evaluate_t(org_images)
+tstop = timeit.default_timer()
+dtime = tstop - tstart
+print(f"Finish fast PSWF expansion of original images in {dtime:.4f} seconds.")
+
+# Reconstruct images from the expansion coefficients based on direct PSWF basis
+fpswf_images = fpswf_basis.evaluate(fpswf_coefs).asnumpy()
+print("Finish reconstruction of images from fast PSWF expansion coefficients.")
+
+# Calculate mean value of maximum differences between the fast PSWF estimated images and the original images
+diff = fpswf_images - org_images
+fpswf_meanmax = np.mean(np.max(abs(diff), axis=(1, 2)))
+print(
+    f"Mean value of maximum differences between FPSWF estimated images and original images: {fpswf_meanmax}"
+)
+
+# Calculate the normalized RMSE of the estimated images
+fpswf_nrmse_ims = anorm(diff) / anorm(org_images)
+print(f"FPSWF Estimated images normalized RMSE: {fpswf_nrmse_ims}")
+
+# plot the first images using the fast PSWF method
+plt.subplot(1, 3, 1)
+plt.imshow(np.real(org_images[0]), cmap="gray")
+plt.title("Original")
+plt.subplot(1, 3, 2)
+plt.imshow(np.real(fpswf_images[0]), cmap="gray")
+plt.title("FPSWF Image")
+plt.subplot(1, 3, 3)
+plt.imshow(np.real(org_images[0] - fpswf_images[0]), cmap="gray")
+plt.title("Differences")
+plt.tight_layout()
+
+# Basic Check
+assert fb_nrmse_ims < 0.025
+assert ffb_nrmse_ims < 0.025
+assert pswf_nrmse_ims < 0.025
+assert fpswf_nrmse_ims < 0.025
+
+# %%
+# Side-by-side Comparison
+# -----------------------
+#
+# Here we present the images side-by-side, column-wise, for comparison between the methods.
+# All plotting commands are copied from the above sections and concatenated to render the comparison plot.
+
+# plot the first images using the normal FB method
+plt.subplot(3, 4, 1)
+plt.imshow(np.real(org_images[0]), cmap="gray")
+plt.title("Original")
+plt.subplot(3, 4, 5)
+plt.imshow(np.real(fb_images[0]), cmap="gray")
+plt.title("FB Image")
+plt.subplot(3, 4, 9)
+plt.imshow(np.real(org_images[0] - fb_images[0]), cmap="gray")
+plt.title("Differences")
+
+# plot the first images using the fast FB method
+plt.subplot(3, 4, 2)
+plt.imshow(np.real(org_images[0]), cmap="gray")
+plt.title("Original")
+plt.subplot(3, 4, 6)
+plt.imshow(np.real(ffb_images[0]), cmap="gray")
+plt.title("FFB Image")
+plt.subplot(3, 4, 10)
+plt.imshow(np.real(org_images[0] - ffb_images[0]), cmap="gray")
+plt.title("Differences")
+
+# plot the first images using the direct PSWF method
+plt.subplot(3, 4, 3)
+plt.imshow(np.real(org_images[0]), cmap="gray")
+plt.title("Original")
+plt.subplot(3, 4, 7)
+plt.imshow(np.real(pswf_images[0]), cmap="gray")
+plt.title("PSWF Image")
+plt.subplot(3, 4, 11)
+plt.imshow(np.real(org_images[0] - pswf_images[0]), cmap="gray")
+plt.title("Differences")
+
+# plot the first images using the fast PSWF method
+plt.subplot(3, 4, 4)
+plt.imshow(np.real(org_images[0]), cmap="gray")
+plt.title("Original")
+plt.subplot(3, 4, 8)
+plt.imshow(np.real(fpswf_images[0]), cmap="gray")
+plt.title("FPSWF Image")
+plt.subplot(3, 4, 12)
+plt.imshow(np.real(org_images[0] - fpswf_images[0]), cmap="gray")
+plt.title("Differences")
+plt.tight_layout()
+
+plt.show()
