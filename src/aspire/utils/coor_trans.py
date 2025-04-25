8d3561aef0fc442bc3d37e38df81f86cc46249af
Josh Carmichael <carmichael@princeton.edu>
2025-04-25 10:09:40 -0400

    remove normalization adjustment for even/shifted/normalized grids. Adapt hard-coded reference values in test.

--- src/aspire/utils/coor_trans.py
+++ src/aspire/utils/coor_trans.py
@@ -73,14 +73,8 @@ def _mgrid_slice(n, shifted, normalized):
         end -= 1
 
     if normalized:
-        # Compute the denominator for normalization
-        denom = n / 2
-        if shifted and n % 2 == 0:
-            denom -= 1 / 2
-
-        # Apply the normalization
-        start /= denom
-        end /= denom
+        start /= n / 2
+        end /= n / 2
 
     return slice(start, end, num_points)
 
