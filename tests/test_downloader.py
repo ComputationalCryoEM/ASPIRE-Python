fd8aa2c956b6f44ea9fc19269d352fe114743d84
Josh Carmichael <carmichael@princeton.edu>
2025-05-20 10:31:04 -0400

    Use logger warning. Update test to fail on hash mismatch warning.

--- tests/test_downloader.py
+++ tests/test_downloader.py
@@ -4,6 +4,11 @@ from aspire.downloader import download_all
 
 
 @pytest.mark.scheduled
-def test_download_all():
-    """This test will throw a warning if any hashes have changed"""
-    _ = download_all()
+def test_download_all(caplog):
+    """Fail if a hash mismatch warning is logged during download."""
+    caplog.clear()
+    with caplog.at_level("WARNING"):
+        _ = download_all()
+
+    if "Hash mismatch" in caplog.text:
+        pytest.fail(f"Hash mismatch warning was logged.\nCaptured logs:\n{caplog.text}")
