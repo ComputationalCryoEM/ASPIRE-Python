e001c015c7a2b0d1b254373c897f5608fb43b2ab
Josh Carmichael <carmichael@princeton.edu>
2025-09-16 15:35:52 -0400

    fix param in gallery

--- gallery/tutorials/tutorials/cov2d_simulation.py
+++ gallery/tutorials/tutorials/cov2d_simulation.py
@@ -92,9 +92,9 @@ sim = Simulation(
     unique_filters=ctf_filters,
     offsets=0.0,
     amplitudes=1.0,
-    pixel_size=pixel_size,
     dtype=dtype,
     noise_adder=noise_adder,
+    pixel_size=pixel_size,
 )
 
 
