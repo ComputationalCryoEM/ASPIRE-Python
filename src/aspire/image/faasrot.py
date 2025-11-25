17f5c8c0c95711224965755b73ff81a641403767
Garrett Wright <garrettwrong@gmail.com>
2025-11-25 14:34:18 -0500

    add smoke test and xp
    [skip ci]

--- src/aspire/image/faasrot.py
+++ src/aspire/image/faasrot.py
@@ -1,14 +1,17 @@
 import numpy as np
 
-from aspire.numeric import xp
+from aspire.numeric import fft, xp
 
 
 def _pre_rotate(theta):
     """
-    Given angle `theta` (degrees) return nearest rotation of 90
-    degrees required to place angle within [-45,45) and residual
-    rotation in degrees.
+    Given angle `theta` (radians) return nearest rotation of 90
+    degrees required to place angle within [-45,45) degrees and residual
+    rotation (radians).
     """
+
+    # todo
+    theta = np.rad2deg(theta)
 
     theta = np.mod(theta, 360)
 
@@ -29,7 +32,7 @@ def _pre_rotate(theta):
         rot90 = 0
         residual = theta - 360
 
-    return residual, rot90
+    return np.deg2rad(residual), rot90
 
 
 def _shift_center(n):
@@ -49,10 +52,11 @@ def _shift_center(n):
 def _pre_compute(theta, nx, ny):
     """
     Retuns M = (Mx, My, rot90)
+
+    :param theta: angle in radians
     """
     theta, mult90 = _pre_rotate(theta)
 
-    theta = np.pi * theta / 180
     theta = -theta  # Yaroslavsky rotated CW
 
     cy, sy = _shift_center(ny)
@@ -62,32 +66,32 @@ def _pre_compute(theta, nx, ny):
     eps = np.finfo(np.float64).eps
 
     # Precompute Y interpolation tables
-    My = np.zeros((nx, ny), dtype=np.complex128)
-    r = np.arange(cy + 1, dtype=int)
-    u = (1 - np.cos(theta)) / np.sin(theta + eps)
-    alpha1 = 2 * np.pi * 1j * r / ny
+    My = xp.zeros((nx, ny), dtype=xp.complex128)
+    r = xp.arange(cy + 1, dtype=int)
+    u = (1 - xp.cos(theta)) / xp.sin(theta + eps)
+    alpha1 = 2 * xp.pi * 1j * r / ny
 
-    linds = np.arange(ny - 1, cy, -1, dtype=int)
-    rinds = np.arange(1, cy - 2 * sy + 1, dtype=int)
+    linds = xp.arange(ny - 1, cy, -1, dtype=int)
+    rinds = xp.arange(1, cy - 2 * sy + 1, dtype=int)
     # This can be broadcast, but leaving loop since would be close to CUDA...
     for x in range(nx):
         Ux = u * (x - cx + sx + 2)
-        My[x, r] = np.exp(alpha1 * Ux)
-        My[x, linds] = np.conj(My[x, rinds])
+        My[x, r] = xp.exp(alpha1 * Ux)
+        My[x, linds] = My[x, rinds].conj()
 
     # Precompute X interpolation tables
-    Mx = np.zeros((ny, nx), dtype=np.complex128)
-    r = np.arange(cx + 1, dtype=int)
-    u = -np.sin(theta)
-    alpha2 = 2 * np.pi * 1j * r / nx
+    Mx = xp.zeros((ny, nx), dtype=xp.complex128)
+    r = xp.arange(cx + 1, dtype=int)
+    u = -xp.sin(theta)
+    alpha2 = 2 * xp.pi * 1j * r / nx
 
-    linds = np.arange(nx - 1, cx, -1, dtype=int)
-    rinds = np.arange(1, cx - 2 * sx + 1, dtype=int)
+    linds = xp.arange(nx - 1, cx, -1, dtype=int)
+    rinds = xp.arange(1, cx - 2 * sx + 1, dtype=int)
     # This can be broadcast, but leaving loop since would be close to CUDA...
     for y in range(ny):
         Uy = u * (y - cy + sy + 2)
-        Mx[y, r] = np.exp(alpha2 * Uy)
-        Mx[y, linds] = np.conj(Mx[y, rinds])
+        Mx[y, r] = xp.exp(alpha2 * Uy)
+        Mx[y, linds] = Mx[y, rinds].conj()
 
     # After building, transpose to (nx, ny).
     Mx = Mx.T
@@ -96,15 +100,15 @@ def _pre_compute(theta, nx, ny):
 
 
 def _rot90(img):
-    return np.flipud(img.T)
+    return xp.flipud(img.T)
 
 
 def _rot180(img):
-    return np.flipud(np.fliplr(img))
+    return xp.flipud(xp.fliplr(img))
 
 
 def _rot270(img):
-    return np.fliplr(img.T)
+    return xp.fliplr(img.T)
 
 
 def faasrotate(images, theta, M=None):
@@ -116,8 +120,6 @@ def faasrotate(images, theta, M=None):
     :param M: optional precomputed shearing table
     :return: (n, px, px) array fo rotated image data
     """
-    # Convert to degrees
-    theta = np.rad2deg(theta)
 
     # Make a stack of 1
     if images.ndim == 2:
@@ -134,7 +136,7 @@ def faasrotate(images, theta, M=None):
 
     for i in range(n):
 
-        img = images[i]
+        img = xp.asarray(images[i])
 
         # Pre rotate by multiples of 90
         if Mrot90 == 1:
@@ -145,18 +147,18 @@ def faasrotate(images, theta, M=None):
             img = _rot270(img)
 
         # Shear 1
-        img_k = np.fft.fft(img, axis=-1)
+        img_k = fft.fft(img, axis=-1)
         img_k = img_k * My
-        result[i] = np.real(np.fft.ifft(img_k, axis=-1))
+        result[i] = fft.ifft(img_k, axis=-1).real
 
         # Shear 2
-        img_k = np.fft.fft(result[i], axis=-2)
+        img_k = fft.fft(result[i], axis=-2)
         img_k = img_k * Mx
-        result[i] = np.real(np.fft.ifft(img_k, axis=-2))
+        result[i] = fft.ifft(img_k, axis=-2).real
 
         # Shear 3
-        img_k = np.fft.fft(result[i], axis=-1)
+        img_k = fft.fft(result[i], axis=-1)
         img_k = img_k * My
-        result[i] = np.real(np.fft.ifft(img_k, axis=-1))
+        result[i] = fft.ifft(img_k, axis=-1).real
 
     return result
