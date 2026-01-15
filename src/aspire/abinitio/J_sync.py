75fc54385b51dbc61ff7ded5e38f1b60dcb18733
Josh Carmichael <carmichael@princeton.edu>
2026-01-15 11:41:21 -0500

    migrate optimized J_sync methods to JSync module

--- src/aspire/abinitio/J_sync.py
+++ src/aspire/abinitio/J_sync.py
@@ -1,10 +1,10 @@
 import logging
+import os.path
 
 import numpy as np
 from numpy.linalg import norm
 
-from aspire.utils import J_conjugate, all_pairs, all_triplets, tqdm
-from aspire.utils.random import randn
+from aspire.utils import J_conjugate, all_pairs, random, trange
 
 logger = logging.getLogger(__name__)
 
@@ -14,12 +14,30 @@ class JSync:
     Class for handling J-synchronization methods.
     """
 
+    # Initialize alternatives
+    #
+    # When we find the best J-configuration, we also compare it to the alternative 2nd best one.
+    # this comparison is done for every pair in the triplet independently. to make sure that the
+    # alternative is indeed different in relation to the pair, we document the differences between
+    # the configurations in advance:
+    # ALTS(:,best_conf,pair) = the two configurations in which J-sync differs from best_conf in relation to pair
+
+    _ALTS = np.array(
+        [
+            [[1, 2, 1], [0, 2, 0], [0, 0, 1], [1, 0, 0]],
+            [[2, 3, 3], [3, 3, 2], [3, 1, 3], [2, 1, 2]],
+        ],
+        dtype=int,
+    )
+
     def __init__(
         self,
         n,
         epsilon=1e-2,
         max_iters=1000,
         seed=None,
+        disable_gpu=False,
+        J_weighting=False,
     ):
         """
         Initialize JSync object for estimating global handedness synchronization for a
@@ -34,32 +52,54 @@ class JSync:
         self.epsilon = epsilon
         self.max_iters = max_iters
         self.seed = seed
+        self.J_weighting = J_weighting
 
-    def global_J_sync(self, vijs):
+        # Generate pair mappings
+        self._pairs, self._pairs_to_linear = all_pairs(n, return_map=True)
+
+        # Auto configure GPU
+        self.__gpu_module = None
+        if not disable_gpu:
+            try:
+                import cupy as cp
+
+                if cp.cuda.runtime.getDeviceCount() >= 1:
+                    gpu_id = cp.cuda.runtime.getDevice()
+                    logger.info(
+                        f"cupy and GPU {gpu_id} found by cuda runtime; enabling cupy."
+                    )
+                    self.__gpu_module = self.__init_cupy_module()
+                else:
+                    logger.info("GPU not found, defaulting to numpy.")
+
+            except ModuleNotFoundError:
+                logger.info("cupy not found, defaulting to numpy.")
+
+    def global_J_sync(self, Rijs):
         """
-        Global J-synchronization of all third row outer products. Given 3x3 matrices vijs, each
-        of which might contain a spurious J (ie. vij = J*vi*vj^T*J instead of vij = vi*vj^T),
-        we return vijs that all have either a spurious J or not.
+        Apply global J-synchronization.
 
-        :param vijs: An (n-choose-2)x3x3 array where each 3x3 slice holds an estimate for the corresponding
-        outer-product vi*vj^T between the third rows of the rotation matrices Ri and Rj. Each estimate
-        might have a spurious J independently of other estimates.
+        Given all pairs of estimated rotation matrices `Rijs` with
+        arbitrary handedness (J conjugation), attempt to detect and
+        conjugate entries of `Rijs` such that all rotations have same
+        handedness.
 
-        :return: vijs, all of which have a spurious J or not.
+        :param Rijs: Array of all pairs of rotation matrices
+        :return: Array of all pairs of J synchronized rotation matrices
         """
 
-        # Determine relative handedness of vijs.
-        sign_ij_J = self.power_method(vijs)
+        # Determine relative handedness of Rijs.
+        sign_ij_J = self.power_method(Rijs)
 
-        # Synchronize vijs
-        vijs_sync = vijs.copy()
-        for i, sign in enumerate(sign_ij_J):
-            if sign == -1:
-                vijs_sync[i] = J_conjugate(vijs[i])
+        # Synchronize Rijs
+        logger.info("Applying global handedness synchronization.")
+        Rijs_sync = Rijs.copy()
+        mask = sign_ij_J == -1
+        Rijs_sync[mask] = J_conjugate(Rijs_sync[mask])
 
-        return vijs_sync
+        return Rijs_sync
 
-    def power_method(self, vijs):
+    def power_method(self, Rijs):
         """
         Calculate the leading eigenvector of the J-synchronization matrix
         using the power method.
@@ -68,31 +108,36 @@ class JSync:
         use the power method to compute the eigenvalues and eigenvectors,
         while constructing the matrix on-the-fly.
 
-        :param vijs: (n-choose-2)x3x3 array of estimates of relative orientation matrices.
+        :param Rijs: (n-choose-2)x3x3 array of estimates of relative orientation matrices.
 
         :return: An array of length n-choose-2 consisting of 1 or -1, where the sign of the
-            i'th entry indicates whether the i'th relative orientation matrix will be J-conjugated.
+        i'th entry indicates whether the i'th relative orientation matrix will be J-conjugated.
         """
 
+        logger.info(
+            "Initiating power method to estimate J-synchronization matrix eigenvector."
+        )
         # Set power method tolerance and maximum iterations.
         epsilon = self.epsilon
         max_iters = self.max_iters
 
         # Initialize candidate eigenvectors
-        n_vijs = vijs.shape[0]
-        vec = randn(n_vijs, seed=self.seed)
+        n_Rijs = Rijs.shape[0]
+        vec = random(n_Rijs, seed=self.seed)
         vec = vec / norm(vec)
         residual = 1
         itr = 0
 
+        # Todo
+        # I don't like that epsilon>1 (residual) returns signs of random vector
+        #      maybe force to run once? or return vec as zeros in that case?
+        #      Seems unintended, but easy to do.
+
         # Power method iterations
-        logger.info(
-            "Initiating power method to estimate J-synchronization matrix eigenvector."
-        )
         while itr < max_iters and residual > epsilon:
             itr += 1
-            # Note, this appears to need double precision for accuracy in the following division.
-            vec_new = self._signs_times_v(vijs, vec).astype(np.float64, copy=False)
+            # Todo, this code code actually needs double precision for accuracy... forcing.
+            vec_new = self._signs_times_v(Rijs, vec).astype(np.float64, copy=False)
             vec_new = vec_new / norm(vec_new)
             residual = norm(vec_new - vec)
             vec = vec_new
@@ -101,7 +146,8 @@ class JSync:
             )
 
         # We need only the signs of the eigenvector
-        J_sync = np.sign(vec, dtype=vijs.dtype)
+        J_sync = np.sign(vec, dtype=Rijs.dtype)
+        J_sync = np.sign(J_sync[0]) * J_sync  # Stabilize J_sync
 
         return J_sync
 
@@ -158,90 +204,143 @@ class JSync:
                 viis[i] = vii_J
         return viis
 
-    def _signs_times_v(self, vijs, vec):
+    def _signs_times_v(self, Rijs, vec):
         """
-        Multiplication of the J-synchronization matrix by a candidate eigenvector.
+        Multiplication of the J-synchronization matrix by a candidate eigenvector `vec`
 
-        The J-synchronization matrix is a matrix representation of the handedness graph, Gamma, whose set of
-        nodes consists of the estimates vijs and whose set of edges consists of the undirected edges between
-        all triplets of estimates vij, vjk, and vik, where i<j<k. The weight of an edge is set to +1 if its
-        incident nodes agree in handednes and -1 if not.
+        Wrapper for cpu/gpu dispatch.
 
-        The J-synchronization matrix is of size (n-choose-2)x(n-choose-2), where each entry corresponds to
-        the relative handedness of vij and vjk. The entry (ij, jk), where ij and jk are retrieved from the
-        all_pairs indexing, is 1 if vij and vjk are of the same handedness and -1 if not. All other entries
-        (ij, kl) hold a zero.
-
-        Due to the large size of the J-synchronization matrix we construct it on the fly as follows.
-        For each triplet of outer products vij, vjk, and vik, the associated elements of the J-synchronization
-        matrix are populated with +1 or -1 and multiplied by the corresponding elements of
-        the current candidate eigenvector supplied by the power method. The new candidate eigenvector
-        is updated for each triplet.
-
-        :param vijs: (n-choose-2)x3x3 array, where each 3x3 slice holds the outer product of vi and vj.
-
+        :param Rijs: An n-choose-2x3x3 array of estimates of relative rotations
         :param vec: The current candidate eigenvector of length n-choose-2 from the power method.
+        :return: New candidate eigenvector.
+        """
+        # host/gpu dispatch
+        if self.__gpu_module:
+            new_vec = self._signs_times_v_cupy(Rijs, vec)
+        else:
+            new_vec = self._signs_times_v_host(Rijs, vec)
 
-        :return: New candidate eigenvector of length n-choose-2. The product of the J-sync matrix and vec.
+        return new_vec.astype(vec.dtype, copy=False)
+
+    def _signs_times_v_host(self, Rijs, vec):
+        """
+        See `_signs_times_v`.
+
+        CPU implementation.
         """
 
-        # All pairs (i,j) and triplets (i,j,k) where i<j<k
-        n_img = self.n_img
-        triplets = all_triplets(n_img)
-        pairs, pairs_to_linear = all_pairs(n_img, return_map=True)
-
-        # There are 4 possible configurations of relative handedness for each triplet (vij, vjk, vik).
-        # 'conjugate' expresses which node of the triplet must be conjugated (True) to achieve synchronization.
-        conjugate = np.empty((4, 3), bool)
-        conjugate[0] = [False, False, False]
-        conjugate[1] = [True, False, False]
-        conjugate[2] = [False, True, False]
-        conjugate[3] = [False, False, True]
-
-        # 'edges' corresponds to whether conjugation agrees between the pairs (vij, vjk), (vjk, vik),
-        # and (vik, vij). True if the pairs are in agreement, False otherwise.
-        edges = np.empty((4, 3), bool)
-        edges[:, 0] = conjugate[:, 0] == conjugate[:, 1]
-        edges[:, 1] = conjugate[:, 1] == conjugate[:, 2]
-        edges[:, 2] = conjugate[:, 2] == conjugate[:, 0]
-
-        # The corresponding entries in the J-synchronization matrix are +1 if the pair of nodes agree, -1 if not.
-        edge_signs = np.where(edges, 1, -1)
-
-        # For each triplet of nodes we apply the 4 configurations of conjugation and determine the
-        # relative handedness based on the condition that vij @ vjk - vik = 0 for synchronized nodes.
-        # We then construct the corresponding entries of the J-synchronization matrix with 'edge_signs'
-        # corresponding to the conjugation configuration producing the smallest residual for the above
-        # condition. Finally, we the multiply the 'edge_signs' by the cooresponding entries of 'vec'.
-        v = vijs
         new_vec = np.zeros_like(vec)
-        pbar = tqdm(desc="Computing signs_times_v", total=len(triplets))
-        for i, j, k in triplets:
-            ij = pairs_to_linear[i, j]
-            jk = pairs_to_linear[j, k]
-            ik = pairs_to_linear[i, k]
-            vij, vjk, vik = v[ij], v[jk], v[ik]
-            vij_J = J_conjugate(vij)
-            vjk_J = J_conjugate(vjk)
-            vik_J = J_conjugate(vik)
 
-            conjugated_pairs = np.where(
-                conjugate[..., np.newaxis, np.newaxis],
-                [vij_J, vjk_J, vik_J],
-                [vij, vjk, vik],
-            )
-            residual = np.stack([norm(x @ y - z) for x, y, z in conjugated_pairs])
+        _signs_confs = np.array(
+            [[1, 1, 1], [-1, 1, -1], [-1, -1, 1], [1, -1, -1]], dtype=int
+        )
 
-            min_residual = np.argmin(residual)
+        c = np.empty((4))
+        desc = "Computing signs_times_v"
+        if self.J_weighting:
+            desc += " with J_weighting"
+        for i in trange(self.n_img - 2, desc=desc):
+            for j in range(
+                i + 1, self.n_img - 1
+            ):  # check bound (taken from MATLAB mex)
+                ij = self._pairs_to_linear[i, j]
+                Rij = Rijs[ij]
+                for k in range(j + 1, self.n_img):
+                    ik = self._pairs_to_linear[i, k]
+                    jk = self._pairs_to_linear[j, k]
+                    Rik = Rijs[ik]
+                    Rjk = Rijs[jk]
 
-            # Assign edge weights
-            s_ij_jk, s_ik_jk, s_ij_ik = edge_signs[min_residual]
+                    # Compute conjugated rotats
+                    Rij_J = J_conjugate(Rij)
+                    Rik_J = J_conjugate(Rik)
+                    Rjk_J = J_conjugate(Rjk)
 
-            # Update multiplication of signs times vec
-            new_vec[ij] += s_ij_jk * vec[jk] + s_ij_ik * vec[ik]
-            new_vec[jk] += s_ij_jk * vec[ij] + s_ik_jk * vec[ik]
-            new_vec[ik] += s_ij_ik * vec[ij] + s_ik_jk * vec[jk]
-            pbar.update()
-        pbar.close()
+                    # Compute R muls and norms
+                    c[0] = np.sum(((Rij @ Rjk) - Rik) ** 2)
+                    c[1] = np.sum(((Rij_J @ Rjk) - Rik) ** 2)
+                    c[2] = np.sum(((Rij @ Rjk_J) - Rik) ** 2)
+                    c[3] = np.sum(((Rij @ Rjk) - Rik_J) ** 2)
+
+                    # Find best match
+                    best_i = np.argmin(c)
+                    best_val = c[best_i]
+
+                    # MATLAB: scores_as_entries == 0
+                    s_ij_jk = _signs_confs[best_i][0]
+                    s_ik_jk = _signs_confs[best_i][1]
+                    s_ij_ik = _signs_confs[best_i][2]
+
+                    # Note there was a third J_weighting option (2) in MATLAB,
+                    # but it was not exposed at top level.
+                    if self.J_weighting:
+                        # MATLAB: scores_as_entries == 1
+                        # For each triangle side, find the best alternative
+                        alt_ij_jk = c[self._ALTS[0][best_i][0]]
+                        if c[self._ALTS[1][best_i][0]] < alt_ij_jk:
+                            alt_ij_jk = c[self._ALTS[1][best_i][0]]
+
+                        alt_ik_jk = c[self._ALTS[0][best_i][1]]
+                        if c[self._ALTS[1][best_i][1]] < alt_ik_jk:
+                            alt_ik_jk = c[self._ALTS[1][best_i][1]]
+
+                        alt_ij_ik = c[self._ALTS[0][best_i][2]]
+                        if c[self._ALTS[1][best_i][2]] < alt_ij_ik:
+                            alt_ij_ik = c[self._ALTS[1][best_i][2]]
+
+                        # Compute scores
+                        s_ij_jk *= 1 - np.sqrt(best_val / alt_ij_jk)
+                        s_ik_jk *= 1 - np.sqrt(best_val / alt_ik_jk)
+                        s_ij_ik *= 1 - np.sqrt(best_val / alt_ij_ik)
+
+                    # Update vector entries
+                    new_vec[ij] += s_ij_jk * vec[jk] + s_ij_ik * vec[ik]
+                    new_vec[jk] += s_ij_jk * vec[ij] + s_ik_jk * vec[ik]
+                    new_vec[ik] += s_ij_ik * vec[ij] + s_ik_jk * vec[jk]
 
         return new_vec
+
+    def _signs_times_v_cupy(self, Rijs, vec):
+        """
+        See `_signs_times_v`.
+
+        CPU implementation.
+        """
+        import cupy as cp
+
+        signs_times_v = self.__gpu_module.get_function("signs_times_v")
+
+        Rijs_dev = cp.array(Rijs, dtype=np.float64)
+        vec_dev = cp.array(vec, dtype=np.float64)
+        new_vec_dev = cp.zeros((vec.shape[0]), dtype=np.float64)
+
+        # call the kernel
+        blkszx = 512
+        nblkx = (self.n_img + blkszx - 1) // blkszx
+        signs_times_v(
+            (nblkx,),
+            (blkszx,),
+            (self.n_img, Rijs_dev, vec_dev, new_vec_dev, self.J_weighting),
+        )
+
+        # dtoh
+        new_vec = new_vec_dev.get().astype(vec.dtype, copy=False)
+
+        return new_vec
+
+    @staticmethod
+    def __init_cupy_module():
+        """
+        Private utility method to read in CUDA source and return as
+        compiled CUPY module.
+        """
+
+        import cupy as cp
+
+        # Read in contents of file
+        fp = os.path.join(os.path.dirname(__file__), "commonline_sync3n.cu")
+        with open(fp, "r") as fh:
+            module_code = fh.read()
+
+        # CUPY compile the CUDA code
+        return cp.RawModule(code=module_code, backend="nvcc")
