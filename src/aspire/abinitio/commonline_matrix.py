8815ab0973b3b379fec0ac655356cdb772ff28a4
Josh Carmichael <carmichael@princeton.edu>
2026-02-05 11:43:37 -0500

    revert clamtrix logging flag. Fix cause of excessive logs

--- src/aspire/abinitio/commonline_matrix.py
+++ src/aspire/abinitio/commonline_matrix.py
@@ -51,7 +51,6 @@ class CLMatrixOrient3D(CLOrient3D):
 
         # Outputs
         self._clmatrix = None
-        self._clmatrix_logged = False
 
     @property
     def clmatrix(self):
@@ -64,16 +63,13 @@ class CLMatrixOrient3D(CLOrient3D):
         """
         if self._clmatrix is None:
             self.build_clmatrix()
-            self._clmatrix_logged = False  # reset flag after building
-        elif not self._clmatrix_logged:
+        else:
             logger.info("Using existing estimated `clmatrix`.")
-            self._clmatrix_logged = True
         return self._clmatrix
 
     @clmatrix.setter
     def clmatrix(self, value):
         self._clmatrix = value
-        self._clmatrix_logged = False
 
     def build_clmatrix(self):
         """
