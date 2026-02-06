39d142ca2c1f4b563eb1104e383d79baa7938e07
Garrett Wright <garrettwrong@gmail.com>
2026-02-06 09:51:40 -0500

    new black style update

--- tests/test_wbemd.py
+++ tests/test_wbemd.py
@@ -10,7 +10,7 @@ logger = logging.getLogger(__name__)
 
 
 def _smoothed_disk_image(x, y, radius, width, height):
-    (Y, X) = mgrid[:height, :width]
+    Y, X = mgrid[:height, :width]
     ratio = ((X - x) ** 2 + (Y - y) ** 2) / (radius**2)
     return 2.0 - 2 / (1 + np.exp(-ratio))  # Scaled sigmoid funciton
 
