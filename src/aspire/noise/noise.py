fd8f7041693970072acad62e31da3f4f7ccf6f07
Garrett Wright <garrettwrong@gmail.com>
2026-09-22 10:29:45 -0400

    add reminder note to NoiseAdder
    [skip ci]

--- src/aspire/noise/noise.py
+++ src/aspire/noise/noise.py
@@ -55,6 +55,8 @@ class NoiseAdder(Xform):
         _im = im.asnumpy().copy()
 
         for i, idx in enumerate(indices):
+            # Changing this code will break hardcoded tests where the reference files are dependent on noise.
+            # Pushing to a "_Legacy" implementation is under consideration.
             # Note: The following random seed behavior is directly taken from MATLAB Cov3D code.
             random_seed = self.seed + 191 * (idx + 1)
             im_s = randn(2 * im.resolution, 2 * im.resolution, seed=random_seed)
